70264c3113b9bab3c62407540225735f7d141804
Joel Rosdahl <joel@rosdahl.net>
2010-02-27 23:32:15 +0100

    Portability: Include sys/stat.h and fcntl.h to get O_RDONLY and friends

--- ccache.c
+++ ccache.c
@@ -31,8 +31,10 @@
 #include "comments.h"
 
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/mman.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
