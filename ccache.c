007445b9ff6cd66beac5ca3561e4e28b6a6df1e7
Joel Rosdahl <joel@rosdahl.net>
2010-04-22 22:25:39 +0200

    Improve ccache description in ccache.c

--- ccache.c
+++ ccache.c
@@ -1,26 +1,24 @@
 /*
-  a re-implementation of the compilercache scripts in C
-
-  The idea is based on the shell-script compilercache by Erik Thiele <erikyyy@erikyyy.de>
-
-   Copyright (C) Andrew Tridgell 2002-2007
-   Copyright (C) Martin Pool 2003
-   Copyright (C) Joel Rosdahl 2009-2010
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
+ * ccache -- a fast C/C++ compiler cache
+ *
+ * Copyright (C) Andrew Tridgell 2002-2007
+ * Copyright (C) Martin Pool 2003
+ * Copyright (C) Joel Rosdahl 2009-2010
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #include "ccache.h"
 #include "getopt_long.h"
