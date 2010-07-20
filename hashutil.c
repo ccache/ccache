d4f478176e8b446dd9899be02da0a00e75bccf3c
Joel Rosdahl <joel@rosdahl.net>
2010-07-21 00:12:02 +0200

    Code formatting cleanup

--- hashutil.c
+++ hashutil.c
@@ -24,17 +24,20 @@
 #include <fcntl.h>
 #include <time.h>
 
-unsigned int hash_from_string(void *str)
+unsigned
+hash_from_string(void *str)
 {
 	return murmurhashneutral2(str, strlen((const char *)str), 0);
 }
 
-int strings_equal(void *str1, void *str2)
+int
+strings_equal(void *str1, void *str2)
 {
 	return strcmp((const char *)str1, (const char *)str2) == 0;
 }
 
-int file_hashes_equal(struct file_hash *fh1, struct file_hash *fh2)
+int
+file_hashes_equal(struct file_hash *fh1, struct file_hash *fh2)
 {
 	return memcmp(fh1->hash, fh2->hash, 16) == 0
 		&& fh1->size == fh2->size;
--- hashutil.h
+++ hashutil.h
@@ -10,7 +10,7 @@ struct file_hash
 	uint32_t size;
 };
 
-unsigned int hash_from_string(void *str);
+unsigned hash_from_string(void *str);
 int strings_equal(void *str1, void *str2);
 int file_hashes_equal(struct file_hash *fh1, struct file_hash *fh2);
 
