41113e3087aabc68bf4379d5c101da351a5c7e67
Joel Rosdahl <joel@rosdahl.net>
2010-08-29 12:09:23 +0200

    Add autoconf check and workaround for stdbool.h

--- system.h
+++ system.h
@@ -46,4 +46,20 @@
 #include <time.h>
 #include <utime.h>
 
+#ifdef HAVE_STDBOOL_H
+#  include <stdbool.h>
+#else
+#  ifndef HAVE__BOOL
+#    ifdef __cplusplus
+typedef bool _Bool;
+#    else
+#      define _Bool signed char
+#    endif
+#  endif
+#  define bool _Bool
+#  define false 0
+#  define true 1
+#  define __bool_true_false_are_defined 1
+#endif
+
 #endif /* CCACHE_SYSTEM_H */
