47b3948c6b3233089fff3fde220438b6c859dd61
Joel Rosdahl <joel@rosdahl.net>
2011-04-09 21:46:36 +0200

    Update copyright for 2011

--- test/framework.h
+++ test/framework.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 Joel Rosdahl
+ * Copyright (C) 2010-2011 Joel Rosdahl
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
