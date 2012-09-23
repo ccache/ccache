ccache/ccache

commit 81714b62b50563055faf301f5e4faec6652a426b

Include more files in dist to comply with GPL's "preferred form" clause

dev.mk.in
@@ -53,17 +53,23 @@ source_dist_files = \
     $(test_sources) \
     AUTHORS.txt \
     GPL-3.0.txt \
+    HACKING.txt \
     INSTALL.txt \
     LICENSE.txt \
     MANUAL.txt \
     Makefile.in \
     NEWS.txt \
     README.txt \
+    autogen.sh \
     config.guess \
     config.h.in \
     config.sub \
     configure \
+    configure.ac \
+    confitems.gperf \
     confitems_lookup.c \
+    dev.mk.in \
+    envtoconfitems.gperf \
     envtoconfitems_lookup.c \
     install-sh \
     main.c \

commit 629a95286b22e8405e68385cc714160657c2c01f

Fix developer mode detection
This broke in 81714b62b50563055faf301f5e4faec6652a426b.

autogen.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+rm -f dev_mode_disabled
 autoheader
 autoconf
 echo "Now run ./configure and make"

configure.ac
@@ -144,13 +144,17 @@ if test "$ac_cv_c_extern_inline" != no ; then
 fi
 
 dnl Enable developer mode if dev.mk.in exists.
-if test -f $srcdir/dev.mk.in && test "$RUN_FROM_BUILD_FARM" != yes; then
-    AC_MSG_NOTICE(Enabling developer mode)
+if test ! -f $srcdir/dev_mode_disabled && test "$RUN_FROM_BUILD_FARM" != yes; then
+    AC_MSG_NOTICE(Developer mode enabled)
     AC_CONFIG_FILES([dev.mk])
     include_dev_mk='include dev.mk'
     version=`(git --git-dir=$srcdir/.git describe --dirty 2>/dev/null || echo vunknown) | sed -e 's/v//' -e 's/-/+/' -e 's/-/_/g'`
     echo "const char CCACHE_VERSION@<:@@:>@ = \"$version\";" >version.c
-elif test ! -f $srcdir/version.c; then
+else
+    AC_MSG_NOTICE(Developer mode disabled)
+fi
+
+if test ! -f $srcdir/version.c; then
     AC_MSG_WARN(unable to determine ccache version)
     echo "const char CCACHE_VERSION@<:@@:>@ = \"unknown\";" >version.c
 fi

dev.mk.in
@@ -105,6 +105,7 @@ $(dist_archives): $(dist_files)
 	(cd $(srcdir) && \
 	 rsync -r --relative $(source_dist_files) $$dir) && \
 	cp $(built_dist_files) $$dir && \
+	echo "Remove this file to enable developer mode." >$$dir/dev_mode_disabled && \
 	(cd $$tmpdir && \
 	 tarcompression= && \
 	 case $@ in \