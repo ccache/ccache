b562c2d34c9467eba1fc0dfd19ff80cb01e619c4
David Givone <david@givone.net>
2013-01-29 23:59:22 -0800

    Fix compile error with Xcode 4.6
    static function ... is used in an inline function with external linkage
    [-Werror,-Wstatic-in-inline] use 'static' to give inline function ... internal
    linkage

--- confitems_lookup.c
+++ confitems_lookup.c
@@ -75,6 +75,7 @@ confitems_hash (register const char *str, register unsigned int len)
   return len + asso_values[(unsigned char)str[1]] + asso_values[(unsigned char)str[0]];
 }
 
+static
 #ifdef __GNUC__
 __inline
 #ifdef __GNUC_STDC_INLINE__
