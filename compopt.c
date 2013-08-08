c600ffff330d9d44bf45e642e326d056c0dada78
Joel Rosdahl <joel@rosdahl.net>
2013-08-08 22:20:12 +0200

    Bail out on option -gsplit-dwarf which produces multiple output files

--- compopt.c
+++ compopt.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 Joel Rosdahl
+ * Copyright (C) 2010, 2013 Joel Rosdahl
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -60,6 +60,7 @@ static const struct compopt compopts[] = {
 	{"-fprofile-use",   TOO_HARD},
 	{"-frepo",          TOO_HARD},
 	{"-ftest-coverage", TOO_HARD},
+	{"-gsplit-dwarf",   TOO_HARD},
 	{"-idirafter",      AFFECTS_CPP | TAKES_ARG | TAKES_PATH},
 	{"-iframework",     AFFECTS_CPP | TAKES_ARG | TAKES_CONCAT_ARG | TAKES_PATH},
 	{"-imacros",        AFFECTS_CPP | TAKES_ARG | TAKES_PATH},
