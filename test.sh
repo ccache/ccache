5236217bb0c57a3d8fcc8bd40599d234fd620f34
Anders Björklund <anders@itension.se>
2015-09-25 19:22:44 +0200

    Count the number of cleanups performed, in stats
    When running a busy ccache, it is useful to keep track of the number
    of cleanups performed to know when you need to increase the cache size.
    
    The number shown is actually the number of subdirectories, so when
    you do a full cleanup the counter usually increases by 16 (not just 1).

--- test.sh
+++ test.sh
@@ -1854,6 +1854,7 @@ cleanup_suite() {
     checkfilecount 0 '*.d' $CCACHE_DIR
     checkfilecount 0 '*.stderr' $CCACHE_DIR
     checkstat 'files in cache' 0
+    checkstat 'cleanups performed' 1
 
     testname="forced cleanup, no limits"
     $CCACHE -C >/dev/null
@@ -1864,6 +1865,7 @@ cleanup_suite() {
     checkfilecount 10 '*.d' $CCACHE_DIR
     checkfilecount 10 '*.stderr' $CCACHE_DIR
     checkstat 'files in cache' 30
+    checkstat 'cleanups performed' 0
 
     testname="forced cleanup, file limit"
     $CCACHE -C >/dev/null
@@ -1876,6 +1878,7 @@ cleanup_suite() {
     checkfilecount 7 '*.d' $CCACHE_DIR
     checkfilecount 7 '*.stderr' $CCACHE_DIR
     checkstat 'files in cache' 21
+    checkstat 'cleanups performed' 1
     for i in 0 1 2 3 4 5 9; do
         file=$CCACHE_DIR/a/result$i-4017.o
         if [ ! -f $file ]; then
@@ -1905,6 +1908,7 @@ cleanup_suite() {
     checkfilecount 3 '*.d' $CCACHE_DIR
     checkfilecount 3 '*.stderr' $CCACHE_DIR
     checkstat 'files in cache' 9
+    checkstat 'cleanups performed' 1
     for i in 3 4 5; do
         file=$CCACHE_DIR/a/result$i-4017.o
         if [ ! -f $file ]; then
@@ -1936,6 +1940,7 @@ cleanup_suite() {
     checkfilecount 156 '*.d' $CCACHE_DIR
     checkfilecount 156 '*.stderr' $CCACHE_DIR
     checkstat 'files in cache' 469
+    checkstat 'cleanups performed' 1
 
     testname="sibling cleanup"
     $CCACHE -C >/dev/null
