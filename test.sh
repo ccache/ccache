67fc1b45f7b0526a6a42000e42c0735eeacd386e
Joel Rosdahl <joel@rosdahl.net>
2016-07-28 22:29:53 +0200

    test: Add test cases for memcached_only functionality

--- test.sh
+++ test.sh
@@ -3271,26 +3271,6 @@ SUITE_memcached() {
 
 # =============================================================================
 
-SUITE_memcached_only_PROBE() {
-    probe_memcached
-}
-
-SUITE_memcached_only_SETUP() {
-    export CCACHE_MEMCACHED_CONF=--SERVER=localhost:22122
-    export CCACHE_MEMCACHED_ONLY=1
-
-    generate_code 1 test1.c
-    start_memcached -p 22122
-}
-
-SUITE_memcached_only() {
-    base_tests
-    # TODO: Test the actual memcached_only functionality instead of just
-    # running all base tests.
-}
-
-# =============================================================================
-
 SUITE_memcached_socket_PROBE() {
     probe_memcached
 }
@@ -3305,6 +3285,58 @@ SUITE_memcached_socket_SETUP() {
 SUITE_memcached_socket() {
     base_tests
     rm /tmp/memcached.$$
+}
+
+# =============================================================================
+
+SUITE_memcached_only_PROBE() {
+    probe_memcached
+}
+
+SUITE_memcached_only_SETUP() {
+    export CCACHE_MEMCACHED_CONF=--SERVER=localhost:22122
+    export CCACHE_MEMCACHED_ONLY=1
+
+    generate_code 1 test1.c
+    start_memcached -p 22122
+}
+
+SUITE_memcached_only() {
+    # -------------------------------------------------------------------------
+    TEST "Preprocessor hit"
+
+    $UNCACHED_COMPILE -c -o reference_test1.o test1.c
+
+    $CCACHE_COMPILE -c test1.c
+    expect_stat 'cache hit (preprocessed)' 0
+    expect_stat 'cache miss' 1
+    expect_stat 'files in cache' 0 # no object file stored in filesystem cache
+    expect_equal_object_files reference_test1.o test1.o
+
+    $CCACHE_COMPILE -c test1.c
+    expect_stat 'cache hit (preprocessed)' 1
+    expect_stat 'cache miss' 1
+    expect_stat 'files in cache' 0 # no object file stored in filesystem cache
+    expect_equal_object_files reference_test1.o test1.o
+
+    # -------------------------------------------------------------------------
+    TEST "Direct hit"
+
+    unset CCACHE_NODIRECT
+
+    $UNCACHED_COMPILE -c -o reference_test1.o test1.c
+
+    $CCACHE_COMPILE -c test1.c
+    expect_stat 'cache hit (direct)' 0
+    expect_stat 'cache miss' 1
+    expect_stat 'files in cache' 1 # manifest file
+    expect_equal_object_files reference_test1.o test1.o
+
+    $CCACHE_COMPILE -c test1.c
+    expect_stat 'cache hit (direct)' 1
+    expect_stat 'cache miss' 1
+    expect_stat 'files in cache' 1 # manifest file
+    expect_equal_object_files reference_test1.o test1.o
 }
 
 # =============================================================================
@@ -3433,8 +3465,8 @@ pch
 upgrade
 input_charset
 memcached
-memcached_only
 memcached_socket
+memcached_only
 "
 
 compiler_location=$(which $COMPILER)
