4054b47cdd34a9571c405380b8ebe8ed3657f086
Ivan Vaigult <i.vaigult@gmail.com>
2016-09-01 01:27:27 +0000

    Add -arch options to hash. Update tests. Fixes "ccache no longer supports building of single -archs #127"

--- test.sh
+++ test.sh
@@ -1119,19 +1119,62 @@ SUITE_multi_arch_PROBE() {
     fi
 }
 
+SUITE_multi_arch_SETUP() {
+    generate_code 1 test1.c
+    unset CCACHE_NODIRECT
+}
+
 SUITE_multi_arch() {
-    # -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
     TEST "cache hit"
 
-    generate_code 1 test1.c
+    # Different arches shouldn't affect each other
+    $CCACHE_COMPILE -arch i386 -c test1.c
+    expect_stat 'cache hit (direct)' 0
+    expect_stat 'cache miss' 1
+    
+    $CCACHE_COMPILE -arch x86_64 -c test1.c
+    expect_stat 'cache hit (direct)' 0
+    expect_stat 'cache miss' 2
+    
+    $CCACHE_COMPILE -arch i386 -c test1.c
+    expect_stat 'cache hit (direct)' 1
+    expect_stat 'cache miss' 2
 
+    # Multiple arches should be cached too
     $CCACHE_COMPILE -arch i386 -arch x86_64 -c test1.c
+    expect_stat 'cache hit (direct)' 1
+    expect_stat 'cache miss' 3
+    
+    $CCACHE_COMPILE -arch i386 -arch x86_64 -c test1.c
+    expect_stat 'cache hit (direct)' 2
+    expect_stat 'cache miss' 3
+
+    #The same for preprocessor mode
+    clear_cache
+    export CCACHE_NODIRECT=1
+    
+    $CCACHE_COMPILE -arch i386 -c test1.c
     expect_stat 'cache hit (preprocessed)' 0
     expect_stat 'cache miss' 1
+    
+    $CCACHE_COMPILE -arch x86_64 -c test1.c
+    expect_stat 'cache hit (preprocessed)' 0
+    expect_stat 'cache miss' 2
+    
+    $CCACHE_COMPILE -arch i386 -c test1.c
+    expect_stat 'cache hit (preprocessed)' 1
+    expect_stat 'cache miss' 2
 
+    # Multiple arches should be cached too
     $CCACHE_COMPILE -arch i386 -arch x86_64 -c test1.c
     expect_stat 'cache hit (preprocessed)' 1
-    expect_stat 'cache miss' 1
+    expect_stat 'cache miss' 3
+    
+    $CCACHE_COMPILE -arch i386 -arch x86_64 -c test1.c
+    expect_stat 'cache hit (preprocessed)' 2
+    expect_stat 'cache miss' 3
+
 }
 
 # =============================================================================
@@ -2402,7 +2445,7 @@ SUITE_readonly() {
         test_failed "Read-only mode + direct mode stored files in the cache"
     fi
 }
-
+ 
 # =============================================================================
 
 SUITE_readonly_direct_SETUP() {
