ccache/ccache

commit 5908e656ef2a6493b42159acff4b1f490016d055

preprocessor mode: enable using identical header in different paths

ccache.c
@@ -887,7 +887,6 @@ process_preprocessed_file(struct mdfour *hash, const char *path)
 				has_absolute_include_headers = is_absolute_path(path);
 			}
 			path = make_relative_path(path);
-			hash_string(hash, path);
 			remember_include_file(path, hash, system);
 			p = r;
 		} else {

test.sh
@@ -1280,8 +1280,8 @@ EOF
     checkstat 'cache miss' 1
     $CCACHE $COMPILER -c `pwd`/file.c
     checkstat 'cache hit (direct)' 1
-    checkstat 'cache hit (preprocessed)' 0
-    checkstat 'cache miss' 2
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 1
 
     testname="__FILE__ in include file"
     $CCACHE -Cz >/dev/null
@@ -1304,8 +1304,8 @@ EOF
     mv file_h.c file2_h.c
     $CCACHE $COMPILER -c `pwd`/file2_h.c
     checkstat 'cache hit (direct)' 1
-    checkstat 'cache hit (preprocessed)' 0
-    checkstat 'cache miss' 2
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 1
 
     ##################################################################
     # Check that direct mode ignores __FILE__ if sloppiness is specified.
@@ -1491,12 +1491,12 @@ EOF
     checkstat 'cache miss' 1
     CPATH=subdir2 $CCACHE $COMPILER -c foo.c
     checkstat 'cache hit (direct)' 1
-    checkstat 'cache hit (preprocessed)' 0
-    checkstat 'cache miss' 2 # subdir2 is part of the preprocessor output
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 1 # subdir2 is part of the preprocessor output
     CPATH=subdir2 $CCACHE $COMPILER -c foo.c
     checkstat 'cache hit (direct)' 2
-    checkstat 'cache hit (preprocessed)' 0
-    checkstat 'cache miss' 2
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 1
 
     testname="comment in strings"
     $CCACHE -Cz >/dev/null
@@ -1615,8 +1615,8 @@ EOF
     cd dir2
     CCACHE_BASEDIR="" $CCACHE $COMPILER -I`pwd`/include -c src/test.c
     checkstat 'cache hit (direct)' 0
-    checkstat 'cache hit (preprocessed)' 0
-    checkstat 'cache miss' 2
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 1
     cd ..
 
     ##################################################################
@@ -1671,8 +1671,8 @@ EOF
     $CCACHE -z >/dev/null
     $CCACHE $COMPILER -I`pwd`/include -c src/test.c
     checkstat 'cache hit (direct)' 0
-    checkstat 'cache hit (preprocessed)' 0
-    checkstat 'cache miss' 1
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 0
     cd ..
 
     ##################################################################

commit 00758023f961bb9a1650e08a34a182f814dae3d0

Hash source path when generating dependencies to avoid false positive
5908e656ef2a6493b42159acff4b1f490016d055 introduced a regression: If a
source file is compiled in directory A and an identical source file in
directory B results in a preprocessed hit, then the dependency file from
the first compilation will be overwritten by the second compilation.
Since the source path is part of the dependency file, an incorrect
dependency file will be retrieved from the cache when recompiling the
source in directory A.

The solution is to include the source path part in the object hash if a
dependency file is being generated.

Fixes #134.

ccache.c
@@ -1536,6 +1536,13 @@ calculate_common_hash(struct args *args, struct mdfour *hash)
 		}
 	}
 
+	// Possibly hash input file location to avoid false positive cache hits since
+	// the dependency file includes the source file path.
+	if (generating_dependencies) {
+		hash_delimiter(hash, "inputfile");
+		hash_string(hash, input_file);
+	}
+
 	// Possibly hash the coverage data file path.
 	if (generating_coverage && profile_arcs) {
 		char *dir = dirname(output_obj);

test.sh
@@ -976,6 +976,20 @@ EOF
     expect_stat 'unsupported compiler option' 1
 
     # -------------------------------------------------------------------------
+    TEST "-MMD for different source files"
+
+    mkdir a b
+    touch a/source.c b/source.c
+    $CCACHE_COMPILE -MMD -c a/source.c
+    expect_file_content source.d "source.o: a/source.c"
+
+    $CCACHE_COMPILE -MMD -c b/source.c
+    expect_file_content source.d "source.o: b/source.c"
+
+    $CCACHE_COMPILE -MMD -c a/source.c
+    expect_file_content source.d "source.o: a/source.c"
+
+    # -------------------------------------------------------------------------
     TEST "-Wp,-P"
 
     # Check that -Wp,-P disables ccache. (-P removes preprocessor information
@@ -1609,6 +1623,20 @@ EOF
     expect_stat 'cache hit (direct)' 0
     expect_stat 'cache hit (preprocessed)' 1
     expect_stat 'cache miss' 1
+
+    # -------------------------------------------------------------------------
+    TEST "-MMD for different source files"
+
+    mkdir a b
+    touch a/source.c b/source.c
+    $CCACHE_COMPILE -MMD -c a/source.c
+    expect_file_content source.d "source.o: a/source.c"
+
+    $CCACHE_COMPILE -MMD -c b/source.c
+    expect_file_content source.d "source.o: b/source.c"
+
+    $CCACHE_COMPILE -MMD -c a/source.c
+    expect_file_content source.d "source.o: a/source.c"
 
     # -------------------------------------------------------------------------
     TEST "Multiple object entries in manifest"