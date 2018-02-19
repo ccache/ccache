8154ca4c202f38265f6513762a22a08d4a54acc4
Geert Kloosterman <geert.kloosterman@brightcomputing.com>
2018-02-19 15:24:55 +0100

    Allow a pre-computed checksum file for precompiled headers
    Having large precompiled header files slows down direct mode off
    CCache considerably. Allow CCache to check against a (much smaller)
    pre-computed checksum file instead of the precompiled header file
    itself. This checksum file will be used in the manifest instead of
    the actual precompiled header file.
    
    Note that to make this to work, the build system needs to keep a
    checksum file in sync with the precompiled header.
    
    The behavior can be activated by the `pch_external_checksum` option.

--- src/confitems_lookup.c
+++ src/confitems_lookup.c
@@ -54,7 +54,7 @@ confitems_hash (register const char *str, register unsigned int len)
       50, 50, 50, 50, 50, 50, 50, 50, 50, 50,
       50, 50, 50, 50, 50, 50, 50, 50, 50, 50,
       50, 50, 50, 50, 50, 50, 50, 50, 50, 50,
-      50, 50, 50, 50, 50, 50, 50,  0, 13,  0,
+      50, 50, 50, 50, 50, 50, 50,  0, 35,  0,
        5, 10, 50,  0, 30, 20, 50,  0, 10, 20,
        5,  0,  0, 50,  5,  0, 10, 15, 50, 50,
       20, 50, 50, 50, 50, 50, 50, 50, 50, 50,
@@ -87,7 +87,7 @@ confitems_get (register const char *str, register unsigned int len)
 {
   enum
     {
-      TOTAL_KEYWORDS = 31,
+      TOTAL_KEYWORDS = 32,
       MIN_WORD_LENGTH = 4,
       MAX_WORD_LENGTH = 26,
       MIN_HASH_VALUE = 4,
@@ -114,28 +114,28 @@ confitems_get (register const char *str, register unsigned int len)
       {"cpp_extension",        7, ITEM(cpp_extension, string)},
 #line 14 "src/confitems.gperf"
       {"compiler_check",       4, ITEM(compiler_check, string)},
-#line 37 "src/confitems.gperf"
-      {"stats",               27, ITEM(stats, bool)},
+#line 38 "src/confitems.gperf"
+      {"stats",               28, ITEM(stats, bool)},
 #line 12 "src/confitems.gperf"
       {"cache_dir_levels",     2, ITEM_V(cache_dir_levels, unsigned, dir_levels)},
 #line 16 "src/confitems.gperf"
       {"compression_level",    6, ITEM(compression_level, unsigned)},
 #line 26 "src/confitems.gperf"
       {"log_file",            16, ITEM(log_file, env_string)},
-#line 30 "src/confitems.gperf"
-      {"prefix_command",      20, ITEM(prefix_command, env_string)},
-#line 36 "src/confitems.gperf"
-      {"sloppiness",          26, ITEM(sloppiness, sloppiness)},
-#line 10 "src/confitems.gperf"
-      {"base_dir",             0, ITEM_V(base_dir, env_string, absolute_path)},
-#line 34 "src/confitems.gperf"
-      {"recache",             24, ITEM(recache, bool)},
 #line 31 "src/confitems.gperf"
-      {"prefix_command_cpp",  21, ITEM(prefix_command_cpp, env_string)},
+      {"prefix_command",      21, ITEM(prefix_command, env_string)},
+#line 37 "src/confitems.gperf"
+      {"sloppiness",          27, ITEM(sloppiness, sloppiness)},
+#line 30 "src/confitems.gperf"
+      {"pch_external_checksum", 20, ITEM(pch_external_checksum, bool)},
+#line 35 "src/confitems.gperf"
+      {"recache",             25, ITEM(recache, bool)},
 #line 32 "src/confitems.gperf"
-      {"read_only",           22, ITEM(read_only, bool)},
-#line 40 "src/confitems.gperf"
-      {"unify",               30, ITEM(unify, bool)},
+      {"prefix_command_cpp",  22, ITEM(prefix_command_cpp, env_string)},
+#line 33 "src/confitems.gperf"
+      {"read_only",           23, ITEM(read_only, bool)},
+#line 41 "src/confitems.gperf"
+      {"unify",               31, ITEM(unify, bool)},
       {"",0,NULL,0,NULL},
 #line 24 "src/confitems.gperf"
       {"keep_comments_cpp",   14, ITEM(keep_comments_cpp, bool)},
@@ -144,14 +144,14 @@ confitems_get (register const char *str, register unsigned int len)
 #line 27 "src/confitems.gperf"
       {"max_files",           17, ITEM(max_files, unsigned)},
       {"",0,NULL,0,NULL},
-#line 33 "src/confitems.gperf"
-      {"read_only_direct",    23, ITEM(read_only_direct, bool)},
+#line 34 "src/confitems.gperf"
+      {"read_only_direct",    24, ITEM(read_only_direct, bool)},
 #line 19 "src/confitems.gperf"
       {"disable",              9, ITEM(disable, bool)},
-#line 38 "src/confitems.gperf"
-      {"temporary_dir",       28, ITEM(temporary_dir, env_string)},
-#line 35 "src/confitems.gperf"
-      {"run_second_cpp",      25, ITEM(run_second_cpp, bool)},
+#line 39 "src/confitems.gperf"
+      {"temporary_dir",       29, ITEM(temporary_dir, env_string)},
+#line 36 "src/confitems.gperf"
+      {"run_second_cpp",      26, ITEM(run_second_cpp, bool)},
       {"",0,NULL,0,NULL},
 #line 18 "src/confitems.gperf"
       {"direct_mode",          8, ITEM(direct_mode, bool)},
@@ -160,10 +160,11 @@ confitems_get (register const char *str, register unsigned int len)
       {"hash_dir",            12, ITEM(hash_dir, bool)},
 #line 21 "src/confitems.gperf"
       {"hard_link",           11, ITEM(hard_link, bool)},
-#line 39 "src/confitems.gperf"
-      {"umask",               29, ITEM(umask, umask)},
+#line 40 "src/confitems.gperf"
+      {"umask",               30, ITEM(umask, umask)},
       {"",0,NULL,0,NULL}, {"",0,NULL,0,NULL},
-      {"",0,NULL,0,NULL},
+#line 10 "src/confitems.gperf"
+      {"base_dir",             0, ITEM_V(base_dir, env_string, absolute_path)},
 #line 25 "src/confitems.gperf"
       {"limit_multiple",      15, ITEM(limit_multiple, float)},
       {"",0,NULL,0,NULL},
@@ -188,4 +189,4 @@ confitems_get (register const char *str, register unsigned int len)
     }
   return 0;
 }
-static const size_t CONFITEMS_TOTAL_KEYWORDS = 31;
+static const size_t CONFITEMS_TOTAL_KEYWORDS = 32;
