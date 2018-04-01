447ca611f9941fa6a2644f604a1a125c32a394e6
Anders F Björklund <anders.f.bjorklund@gmail.com>
2018-04-01 22:18:48 +0200

    Skip test that require symlinks, on Windows/Cygwin

--- test/suites/base.bash
+++ test/suites/base.bash
@@ -815,6 +815,7 @@ EOF
     expect_stat 'cache miss' 1
 
     # -------------------------------------------------------------------------
+if ! $HOST_OS_WINDOWS && ! $HOST_OS_CYGWIN; then
     TEST "Symlink to source directory"
 
     mkdir dir
@@ -835,7 +836,9 @@ EOF
         test_failed "Incorrect header file used"
     fi
 
+fi
     # -------------------------------------------------------------------------
+if ! $HOST_OS_WINDOWS && ! $HOST_OS_CYGWIN; then
     TEST "Symlink to source file"
 
     mkdir dir
@@ -856,6 +859,7 @@ EOF
         test_failed "Incorrect header file used"
     fi
 
+fi
     # -------------------------------------------------------------------------
     TEST ".incbin"
 
