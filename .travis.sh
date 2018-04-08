047d387da9d4dfe8a4e5af6b47859e7e8c15be6f
Anders F Björklund <anders.f.bjorklund@gmail.com>
2018-04-08 13:51:06 +0200

    Add a 32-bit build target, using multilib (-m32)
    Use gcc-multilib rather than doing a full cross-compile.
    But set the --host, just in case something is listening.

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -ex
 make clean
-make travis CC=clang
 make travis CC=gcc
+make travis CC=clang
+make travis CC=gcc CFLAGS="-m32 -g -O2" HOST="--host=i386-linux-gnu"
 make travis CC=i686-w64-mingw32-gcc HOST="--host=i686-w64-mingw32" TEST="unittest/run.exe"
 make travis CC=clang CFLAGS="-fsanitize=undefined" ASAN_OPTIONS="detect_leaks=0"
 make travis CC=clang CFLAGS="-fsanitize=address -g" ASAN_OPTIONS="detect_leaks=0"
