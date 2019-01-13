813894ff9525de75ec9096ff7d687aeb38d218c0
Joel Rosdahl <joel@rosdahl.net>
2019-01-13 21:39:43 +0100

    Revert inadvertent copyright year updates

--- src/lockfile.c
+++ src/lockfile.c
@@ -1,4 +1,4 @@
-// Copyright (C) 2010-2019 Joel Rosdahl
+// Copyright (C) 2010-2018 Joel Rosdahl
 //
 // This program is free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by the Free
