63fdff92d438c2d631ce26bd166455498fcf48c6
Joel Rosdahl <joel@rosdahl.net>
2019-06-17 22:28:51 +0200

    const-ify parameters to confitem verifier/formatter

--- src/confitems.h
+++ src/confitems.h
@@ -4,8 +4,8 @@
 #include "system.h"
 
 typedef bool (*conf_item_parser)(const char *str, void *result, char **errmsg);
-typedef bool (*conf_item_verifier)(void *value, char **errmsg);
-typedef char *(*conf_item_formatter)(void *value);
+typedef bool (*conf_item_verifier)(const void *value, char **errmsg);
+typedef char *(*conf_item_formatter)(const void *value);
 
 struct conf_item {
 	const char *name;
@@ -17,31 +17,31 @@ struct conf_item {
 };
 
 bool confitem_parse_bool(const char *str, void *result, char **errmsg);
-char *confitem_format_bool(void *value);
+char *confitem_format_bool(const void *value);
 
 bool confitem_parse_env_string(const char *str, void *result, char **errmsg);
-char *confitem_format_env_string(void *value);
+char *confitem_format_env_string(const void *value);
 
 bool confitem_parse_double(const char *str, void *result, char **errmsg);
-char *confitem_format_double(void *value);
+char *confitem_format_double(const void *value);
 
 bool confitem_parse_size(const char *str, void *result, char **errmsg);
-char *confitem_format_size(void *value);
+char *confitem_format_size(const void *value);
 
 bool confitem_parse_sloppiness(const char *str, void *result, char **errmsg);
-char *confitem_format_sloppiness(void *value);
+char *confitem_format_sloppiness(const void *value);
 
 bool confitem_parse_string(const char *str, void *result, char **errmsg);
-char *confitem_format_string(void *value);
+char *confitem_format_string(const void *value);
 
 bool confitem_parse_umask(const char *str, void *result, char **errmsg);
-char *confitem_format_umask(void *value);
+char *confitem_format_umask(const void *value);
 
 bool confitem_parse_unsigned(const char *str, void *result, char **errmsg);
-char *confitem_format_unsigned(void *value);
+char *confitem_format_unsigned(const void *value);
 
-bool confitem_verify_absolute_path(void *value, char **errmsg);
-bool confitem_verify_dir_levels(void *value, char **errmsg);
+bool confitem_verify_absolute_path(const void *value, char **errmsg);
+bool confitem_verify_dir_levels(const void *value, char **errmsg);
 
 const struct conf_item *confitems_get(const char *str, size_t len);
 size_t confitems_count(void);
