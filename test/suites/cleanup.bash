e9f58516872270411caa5d5957b523be56f89593
ivan-volnov <ivan-volnov@users.noreply.github.com>
2019-07-21 23:49:48 +0800

    Return cache cleanup tests

--- test/suites/cleanup.bash
+++ test/suites/cleanup.bash
@@ -65,6 +65,33 @@ SUITE_cleanup() {
     done
 
     # -------------------------------------------------------------------------
+    if [ -n "$ENABLE_CACHE_CLEANUP_TESTS" ]; then
+        TEST "Forced cache cleanup, size limit"
+
+        # NOTE: This test is known to fail on filesystems that have unusual block
+        # sizes, including ecryptfs. The workaround is to place the test directory
+        # elsewhere:
+        #
+        #     cd /tmp
+        #     CCACHE=$DIR/ccache $DIR/test.sh
+
+        prepare_cleanup_test_dir $CCACHE_DIR/a
+
+        $CCACHE -F 0 -M 256K >/dev/null
+        $CCACHE -c >/dev/null
+        expect_file_count 3 '*.result' $CCACHE_DIR
+        expect_stat 'files in cache' 3
+        expect_stat 'cleanups performed' 1
+        for i in 0 1 2 3 4 5 6; do
+            file=$CCACHE_DIR/a/result$i-4017.result
+            expect_file_missing $file
+        done
+        for i in 7 8 9; do
+            file=$CCACHE_DIR/a/result$i-4017.result
+            expect_file_exists $file
+        done
+    fi
+    # -------------------------------------------------------------------------
     TEST "Automatic cache cleanup, limit_multiple 0.9"
 
     for x in 0 1 2 3 4 5 6 7 8 9 a b c d e f; do
