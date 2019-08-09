4782edfc6d01b09d37e62632c6969d0975e5f5bd
Joel Rosdahl <joel@rosdahl.net>
2019-08-09 20:34:07 +0200

    Remove snprintf replacements
    They clash with MinGW-w64 C++ headers, so let’s remove them. Any
    reasonably modern system should have them.

--- src/system.hpp
+++ src/system.hpp
@@ -69,24 +69,3 @@ extern char** environ;
 #ifndef ESTALE
 #  define ESTALE -1
 #endif
-
-extern "C" {
-
-#if !HAVE_VSNPRINTF
-int rpl_vsnprintf(char*, size_t, const char*, va_list);
-#  define vsnprintf rpl_vsnprintf
-#endif
-#if !HAVE_SNPRINTF
-int rpl_snprintf(char*, size_t, const char*, ...);
-#  define snprintf rpl_snprintf
-#endif
-#if !HAVE_VASPRINTF
-int rpl_vasprintf(char**, const char*, va_list);
-#  define vasprintf rpl_vasprintf
-#endif
-#if !HAVE_ASPRINTF
-int rpl_asprintf(char**, const char*, ...);
-#  define asprintf rpl_asprintf
-#endif
-
-} // extern "C"
