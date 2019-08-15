b820ba58b9dc1816bf51b00c9318cfff2289f13c
Joel Rosdahl <joel@rosdahl.net>
2019-08-15 20:41:14 +0200

    Bump to CUDA 10.1.243 for Ubuntu 16.04

--- .travis/install_cuda.sh
+++ .travis/install_cuda.sh
@@ -8,16 +8,16 @@
 
 if [ -n "$CUDA" ]; then
     echo "Installing CUDA support"
-    travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
-    travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
+    travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_${CUDA}_amd64.deb
+    travis_retry sudo dpkg -i cuda-repo-ubuntu1604_${CUDA}_amd64.deb
     travis_retry sudo apt-get update -qq
-    export CUDA_APT=${CUDA:0:3}
+    export CUDA_APT=${CUDA:0:4}
     export CUDA_APT=${CUDA_APT/./-}
 
-    travis_retry sudo apt-get install -y cuda-command-line-tools-${CUDA_APT}
+    travis_retry sudo apt-get install --allow-unauthenticated -y cuda-command-line-tools-${CUDA_APT}
     travis_retry sudo apt-get clean
 
-    export CUDA_HOME=/usr/local/cuda-${CUDA:0:3}
+    export CUDA_HOME=/usr/local/cuda-${CUDA:0:4}
     export LD_LIBRARY_PATH=${CUDA_HOME}/nvvm/lib64:${LD_LIBRARY_PATH}
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
     export PATH=${CUDA_HOME}/bin:${PATH}
