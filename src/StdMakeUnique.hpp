8cac5ea92e573aaa7508f5c8ff506b5d0b081978
Joel Rosdahl <joel@rosdahl.net>
2019-09-26 19:53:41 +0200

    Add std::make_unique implementation

---
+++ src/StdMakeUnique.hpp
@@ -0,0 +1,32 @@
+// Copyright (C) 2019 Joel Rosdahl and other contributors
+//
+// See doc/AUTHORS.adoc for a complete list of contributors.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc., 51
+// Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+#pragma once
+
+namespace std {
+
+#if __cplusplus < 201402L
+template<typename T, typename... TArgs>
+inline unique_ptr<T>
+make_unique(TArgs&&... args)
+{
+  return unique_ptr<T>(new T(std::forward<TArgs>(args)...));
+}
+#endif
+
+} // namespace std
