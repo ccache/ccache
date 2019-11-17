ccache/ccache

commit eb5d9bd3beb546b968f81875ee858c4a9eb1c86a

Add some opt-in sloppiness

ccache.c
@@ -133,6 +133,9 @@ static char *manifest_path;
  */
 static time_t time_of_compilation;
 
+/* Bitmask of SLOPPY_*. */
+unsigned sloppiness = 0;
+
 /*
  * Files included by the preprocessor and their hashes/sizes. Key: file path.
  * Value: struct file_hash.
@@ -330,7 +333,8 @@ static void remember_include_file(char *path, size_t path_len)
 		/* Ignore directory, typically $PWD. */
 		goto ignore;
 	}
-	if (st.st_mtime >= time_of_compilation) {
+	if (!(sloppiness & SLOPPY_INCLUDE_FILE_MTIME)
+	    && st.st_mtime >= time_of_compilation) {
 		cc_log("Include file %s too new", path);
 		goto failure;
 	}
@@ -910,13 +914,15 @@ static struct file_hash *calculate_object_hash(
 	}
 
 	if (direct_mode) {
-		/*
-		 * The source code file or an include file may contain
-		 * __FILE__, so make sure that the hash is unique for the file
-		 * name.
-		 */
-		hash_delimiter(hash, "inputfile");
-		hash_string(hash, input_file);
+		if (!(sloppiness & SLOPPY_FILE_MACRO)) {
+			/*
+			 * The source code file or an include file may contain
+			 * __FILE__, so make sure that the hash is unique for
+			 * the file name.
+			 */
+			hash_delimiter(hash, "inputfile");
+			hash_string(hash, input_file);
+		}
 
 		hash_delimiter(hash, "sourcecode");
 		result = hash_source_code_file(hash, input_file);
@@ -1617,6 +1623,35 @@ static void process_args(int argc, char **argv, ARGS **preprocessor_args,
 	*compiler_args = stripped_args;
 }
 
+static unsigned parse_sloppiness(char *p)
+{
+	unsigned result = 0;
+	char *word, *q;
+
+	if (!p) {
+		return result;
+	}
+	p = x_strdup(p);
+	q = p;
+	while ((word = strtok(q, ", "))) {
+		if (strcmp(word, "file_macro") == 0) {
+			cc_log("Being sloppy about __FILE__");
+			result |= SLOPPY_FILE_MACRO;
+		}
+		if (strcmp(word, "include_file_mtime") == 0) {
+			cc_log("Being sloppy about include file mtime");
+			result |= SLOPPY_INCLUDE_FILE_MTIME;
+		}
+		if (strcmp(word, "time_macros") == 0) {
+			cc_log("Being sloppy about __DATE__ and __TIME__");
+			result |= SLOPPY_TIME_MACROS;
+		}
+		q = NULL;
+	}
+	free(p);
+	return result;
+}
+
 /* the main ccache driver function */
 static void ccache(int argc, char *argv[])
 {
@@ -1635,6 +1670,8 @@ static void ccache(int argc, char *argv[])
 	ARGS *compiler_args;
 
 	cc_log("=== CCACHE STARTED =========================================");
+
+	sloppiness = parse_sloppiness(getenv("CCACHE_SLOPPINESS"));
 
 	if (base_dir) {
 		cc_log("Base directory: %s", base_dir);

ccache.h
@@ -55,6 +55,10 @@ enum stats {
 	STATS_END
 };
 
+#define SLOPPY_INCLUDE_FILE_MTIME 1
+#define SLOPPY_FILE_MACRO 2
+#define SLOPPY_TIME_MACROS 4
+
 void hash_start(struct mdfour *md);
 void hash_delimiter(struct mdfour *md, const char* type);
 void hash_string(struct mdfour *md, const char *s);

hashutil.c
@@ -64,6 +64,7 @@ hash_source_code_string(
 	char hashbuf[64];
 	size_t hashbuflen = 0;
 	int result = HASH_SOURCE_CODE_OK;
+	extern unsigned sloppiness;
 
 	p = str;
 	end = str + len;
@@ -159,6 +160,9 @@ hash_source_code_string(
 end:
 	hash_buffer(hash, hashbuf, hashbuflen);
 
+	if (sloppiness & SLOPPY_TIME_MACROS) {
+		return 0;
+	}
 	if (result & HASH_SOURCE_CODE_FOUND_DATE) {
 		/*
 		 * Make sure that the hash sum changes if the (potential)

test.sh
@@ -53,6 +53,7 @@ unset CCACHE_PATH
 unset CCACHE_PREFIX
 unset CCACHE_READONLY
 unset CCACHE_RECACHE
+unset CCACHE_SLOPPINESS
 unset CCACHE_TEMPDIR
 unset CCACHE_UMASK
 unset CCACHE_UNIFY
@@ -838,6 +839,51 @@ EOF
     checkstat 'cache miss' 2
 
     ##################################################################
+    # Check that direct mode ignores __FILE__ if sloppiness is specified.
+    testname="__FILE__ in source file, sloppy"
+    $CCACHE -Cz >/dev/null
+    cat <<EOF >file.c
+#define file __FILE__
+int test;
+EOF
+    CCACHE_SLOPPINESS=file_macro $CCACHE $COMPILER -c file.c
+    checkstat 'cache hit (direct)' 0
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+    CCACHE_SLOPPINESS=file_macro $CCACHE $COMPILER -c file.c
+    checkstat 'cache hit (direct)' 1
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+    CCACHE_SLOPPINESS=file_macro $CCACHE $COMPILER -c $PWD/file.c
+    checkstat 'cache hit (direct)' 2
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+
+    testname="__FILE__ in include file, sloppy"
+    $CCACHE -Cz >/dev/null
+    cat <<EOF >file.h
+#define file __FILE__
+int test;
+EOF
+    backdate file.h
+    cat <<EOF >file_h.c
+#include "file.h"
+EOF
+    CCACHE_SLOPPINESS=file_macro $CCACHE $COMPILER -c file_h.c
+    checkstat 'cache hit (direct)' 0
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+    CCACHE_SLOPPINESS=file_macro $CCACHE $COMPILER -c file_h.c
+    checkstat 'cache hit (direct)' 1
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+    mv file_h.c file2_h.c
+    CCACHE_SLOPPINESS=file_macro $CCACHE $COMPILER -c $PWD/file2_h.c
+    checkstat 'cache hit (direct)' 2
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+
+    ##################################################################
     # Check that we never get direct hits when __TIME__ is used.
     testname="__TIME__ in source file"
     $CCACHE -Cz >/dev/null
@@ -871,6 +917,100 @@ EOF
     $CCACHE $COMPILER -c time_h.c
     checkstat 'cache hit (direct)' 0
     checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 1
+
+    ##################################################################
+    # Check that direct mode ignores __TIME__ when sloppiness is specified.
+    testname="__TIME__ in source file, sloppy"
+    $CCACHE -Cz >/dev/null
+    cat <<EOF >time.c
+#define time __TIME__
+int test;
+EOF
+    CCACHE_SLOPPINESS=time_macros $CCACHE $COMPILER -c time.c
+    checkstat 'cache hit (direct)' 0
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+    CCACHE_SLOPPINESS=time_macros $CCACHE $COMPILER -c time.c
+    checkstat 'cache hit (direct)' 1
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+
+    testname="__TIME__ in include time, sloppy"
+    $CCACHE -Cz >/dev/null
+    cat <<EOF >time.h
+#define time __TIME__
+int test;
+EOF
+    backdate time.h
+    cat <<EOF >time_h.c
+#include "time.h"
+EOF
+    CCACHE_SLOPPINESS=time_macros $CCACHE $COMPILER -c time_h.c
+    checkstat 'cache hit (direct)' 0
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+    CCACHE_SLOPPINESS=time_macros $CCACHE $COMPILER -c time_h.c
+    checkstat 'cache hit (direct)' 1
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+
+    ##################################################################
+    # Check that a too new include file turns off direct mode.
+    testname="too new include file"
+    $CCACHE -Cz >/dev/null
+    cat <<EOF >new.c
+#include "new.h"
+EOF
+    cat <<EOF >new.h
+int test;
+EOF
+    touchy_compiler=touchy-compiler.sh
+    cat <<EOF >$touchy_compiler
+#!/bin/sh
+CCACHE_DISABLE=1 # If $COMPILER happens to be a ccache symlink...
+export CCACHE_DISABLE
+[ x\$2 = "x-E" ] && touch \$3 # Be sure that mtime >= time_of_compilation
+exec $COMPILER "\$@"
+EOF
+    chmod +x $touchy_compiler
+
+    $CCACHE ./$touchy_compiler -c new.c
+    checkstat 'cache hit (direct)' 0
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+    $CCACHE ./$touchy_compiler -c new.c
+    checkstat 'cache hit (direct)' 0
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 1
+
+    ##################################################################
+    # Check that include file mtime is ignored when sloppiness is specified.
+    testname="too new include file, sloppy"
+    $CCACHE -Cz >/dev/null
+    cat <<EOF >new.c
+#include "new.h"
+EOF
+    cat <<EOF >new.h
+int test;
+EOF
+    touchy_compiler=touchy-compiler.sh
+    cat <<EOF >$touchy_compiler
+#!/bin/sh
+CCACHE_DISABLE=1 # If $COMPILER happens to be a ccache symlink...
+export CCACHE_DISABLE
+[ x\$2 = "x-E" ] && touch \$3 # Be sure that mtime >= time_of_compilation
+exec $COMPILER "\$@"
+EOF
+    chmod +x $touchy_compiler
+
+    CCACHE_SLOPPINESS=include_file_mtime $CCACHE ./$touchy_compiler -c new.c
+    checkstat 'cache hit (direct)' 0
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+    CCACHE_SLOPPINESS=include_file_mtime $CCACHE ./$touchy_compiler -c new.c
+    checkstat 'cache hit (direct)' 1
+    checkstat 'cache hit (preprocessed)' 0
     checkstat 'cache miss' 1
 
     ##################################################################

commit 404eedde2b8c7a048ea1f68a8e70e49c5ea74df2

Always include input file path in direct mode hash
The “file_macro sloppiness” mode is a way of opting out of inclusion of
the input file path in the direct mode hash. This can produce a false
cache hit in the following scenario:

- a/r.h exists.
- a/x.c has #include "r.h".
- b/x.c is identical to a/x.c.
- Compiling a/x.c records a/r.h in the manifest.
- Compiling b/x.c results in a false cache hit since a/x.c and b/x.c
  share manifests and a/r.h exists.

Therefore, ditch the file_macro sloppiness mode so that the input file
path is always included in the direct mode hash.

This bug has existed ever since the file_macro sloppiness was introduced
in eb5d9bd3beb5 (ccache 3.0). It has remained undetected since
compilations tend to use .d files and ccache before 3.7.5 added an
implicit “-MQ <output_file_path>” argument, thus in practice including
the output file path in the hash and therefore making manifests unique
when the object file path mirrors the source file path. However, ccache
3.7.5 no longer adds the implicit -MQ option, thus exposing the bug.

Fixes #489.

src/ccache.c
@@ -2234,12 +2234,19 @@ calculate_object_hash(struct args *args, struct hash *hash, int direct_mode)
 			}
 		}
 
-		if (!(conf->sloppiness & SLOPPY_FILE_MACRO)) {
-			// The source code file or an include file may contain __FILE__, so make
-			// sure that the hash is unique for the file name.
-			hash_delimiter(hash, "inputfile");
-			hash_string(hash, input_file);
-		}
+		// Make sure that the direct mode hash is unique for the input file path.
+		// If this would not be the case:
+		//
+		// * An false cache hit may be produced. Scenario:
+		//   - a/r.h exists.
+		//   - a/x.c has #include "r.h".
+		//   - b/x.c is identical to a/x.c.
+		//   - Compiling a/x.c records a/r.h in the manifest.
+		//   - Compiling b/x.c results in a false cache hit since a/x.c and b/x.c
+		//     share manifests and a/r.h exists.
+		// * The expansion of __FILE__ may be incorrect.
+		hash_delimiter(hash, "inputfile");
+		hash_string(hash, input_file);
 
 		hash_delimiter(hash, "sourcecode");
 		int result = hash_source_code_file(conf, hash, input_file);

src/ccache.h
@@ -87,22 +87,21 @@ extern enum guessed_compiler guessed_compiler;
 
 #define SLOPPY_INCLUDE_FILE_MTIME (1U << 0)
 #define SLOPPY_INCLUDE_FILE_CTIME (1U << 1)
-#define SLOPPY_FILE_MACRO (1U << 2)
-#define SLOPPY_TIME_MACROS (1U << 3)
-#define SLOPPY_PCH_DEFINES (1U << 4)
+#define SLOPPY_TIME_MACROS (1U << 2)
+#define SLOPPY_PCH_DEFINES (1U << 3)
 // Allow us to match files based on their stats (size, mtime, ctime), without
 // looking at their contents.
-#define SLOPPY_FILE_STAT_MATCHES (1U << 5)
+#define SLOPPY_FILE_STAT_MATCHES (1U << 4)
 // Allow us to not include any system headers in the manifest include files,
 // similar to -MM versus -M for dependencies.
-#define SLOPPY_SYSTEM_HEADERS (1U << 6)
+#define SLOPPY_SYSTEM_HEADERS (1U << 5)
 // Allow us to ignore ctimes when comparing file stats, so we can fake mtimes
 // if we want to (it is much harder to fake ctimes, requires changing clock)
-#define SLOPPY_FILE_STAT_MATCHES_CTIME (1U << 7)
+#define SLOPPY_FILE_STAT_MATCHES_CTIME (1U << 6)
 // Allow us to not include the -index-store-path option in the manifest hash.
-#define SLOPPY_CLANG_INDEX_STORE (1U << 8)
+#define SLOPPY_CLANG_INDEX_STORE (1U << 7)
 // Ignore locale settings.
-#define SLOPPY_LOCALE (1U << 9)
+#define SLOPPY_LOCALE (1U << 8)
 
 #define str_eq(s1, s2) (strcmp((s1), (s2)) == 0)
 #define str_startswith(s, prefix) \

src/confitems.c
@@ -122,9 +122,7 @@ confitem_parse_sloppiness(const char *str, void *result, char **errmsg)
 	char *word;
 	char *saveptr = NULL;
 	while ((word = strtok_r(q, ", ", &saveptr))) {
-		if (str_eq(word, "file_macro")) {
-			*value |= SLOPPY_FILE_MACRO;
-		} else if (str_eq(word, "file_stat_matches")) {
+		if (str_eq(word, "file_stat_matches")) {
 			*value |= SLOPPY_FILE_STAT_MATCHES;
 		} else if (str_eq(word, "file_stat_matches_ctime")) {
 			*value |= SLOPPY_FILE_STAT_MATCHES_CTIME;
@@ -156,9 +154,6 @@ confitem_format_sloppiness(const void *value)
 {
 	const unsigned *sloppiness = (const unsigned *)value;
 	char *s = x_strdup("");
-	if (*sloppiness & SLOPPY_FILE_MACRO) {
-		reformat(&s, "%sfile_macro, ", s);
-	}
 	if (*sloppiness & SLOPPY_INCLUDE_FILE_MTIME) {
 		reformat(&s, "%sinclude_file_mtime, ", s);
 	}

test/suites/direct.bash
@@ -554,12 +554,13 @@ EOF
     expect_stat 'cache miss' 1
 
     # -------------------------------------------------------------------------
-    TEST "__FILE__ in source file disables direct mode"
+    TEST "The source file path is included in the hash"
 
     cat <<EOF >file.c
 #define file __FILE__
 int test;
 EOF
+    cp file.c file2.c
 
     $CCACHE_COMPILE -c file.c
     expect_stat 'cache hit (direct)' 0
@@ -571,47 +572,34 @@ EOF
     expect_stat 'cache hit (preprocessed)' 0
     expect_stat 'cache miss' 1
 
-    $CCACHE_COMPILE -c `pwd`/file.c
+    $CCACHE_COMPILE -c file2.c
     expect_stat 'cache hit (direct)' 1
     expect_stat 'cache hit (preprocessed)' 0
     expect_stat 'cache miss' 2
 
-    # -------------------------------------------------------------------------
-    TEST "__FILE__ in include file disables direct mode"
-
-    cat <<EOF >file.h
-#define file __FILE__
-int test;
-EOF
-    backdate file.h
-    cat <<EOF >file_h.c
-#include "file.h"
-EOF
-
-    $CCACHE_COMPILE -c file_h.c
-    expect_stat 'cache hit (direct)' 0
-    expect_stat 'cache hit (preprocessed)' 0
-    expect_stat 'cache miss' 1
-
-    $CCACHE_COMPILE -c file_h.c
-    expect_stat 'cache hit (direct)' 1
-    expect_stat 'cache hit (preprocessed)' 0
-    expect_stat 'cache miss' 1
-
-    mv file_h.c file2_h.c
-
-    $CCACHE_COMPILE -c `pwd`/file2_h.c
-    expect_stat 'cache hit (direct)' 1
+    $CCACHE_COMPILE -c file2.c
+    expect_stat 'cache hit (direct)' 2
     expect_stat 'cache hit (preprocessed)' 0
     expect_stat 'cache miss' 2
 
+    $CCACHE_COMPILE -c $(pwd)/file.c
+    expect_stat 'cache hit (direct)' 2
+    expect_stat 'cache hit (preprocessed)' 0
+    expect_stat 'cache miss' 3
+
+    $CCACHE_COMPILE -c $(pwd)/file.c
+    expect_stat 'cache hit (direct)' 3
+    expect_stat 'cache hit (preprocessed)' 0
+    expect_stat 'cache miss' 3
+
     # -------------------------------------------------------------------------
-    TEST "__FILE__ in source file ignored if sloppy"
+    TEST "The source file path is included even if sloppiness = file_macro"
 
     cat <<EOF >file.c
 #define file __FILE__
 int test;
 EOF
+    cp file.c file2.c
 
     CCACHE_SLOPPINESS="$DEFAULT_SLOPPINESS file_macro" $CCACHE_COMPILE -c file.c
     expect_stat 'cache hit (direct)' 0
@@ -623,39 +611,66 @@ EOF
     expect_stat 'cache hit (preprocessed)' 0
     expect_stat 'cache miss' 1
 
-    CCACHE_SLOPPINESS="$DEFAULT_SLOPPINESS file_macro" $CCACHE_COMPILE -c `pwd`/file.c
+    CCACHE_SLOPPINESS="$DEFAULT_SLOPPINESS file_macro" $CCACHE_COMPILE -c file2.c
+    expect_stat 'cache hit (direct)' 1
+    expect_stat 'cache hit (preprocessed)' 0
+    expect_stat 'cache miss' 2
+
+    CCACHE_SLOPPINESS="$DEFAULT_SLOPPINESS file_macro" $CCACHE_COMPILE -c file2.c
     expect_stat 'cache hit (direct)' 2
     expect_stat 'cache hit (preprocessed)' 0
-    expect_stat 'cache miss' 1
+    expect_stat 'cache miss' 2
+
+    CCACHE_SLOPPINESS="$DEFAULT_SLOPPINESS file_macro" $CCACHE_COMPILE -c $(pwd)/file.c
+    expect_stat 'cache hit (direct)' 2
+    expect_stat 'cache hit (preprocessed)' 0
+    expect_stat 'cache miss' 3
+
+    CCACHE_SLOPPINESS="$DEFAULT_SLOPPINESS file_macro" $CCACHE_COMPILE -c $(pwd)/file.c
+    expect_stat 'cache hit (direct)' 3
+    expect_stat 'cache hit (preprocessed)' 0
+    expect_stat 'cache miss' 3
 
     # -------------------------------------------------------------------------
-    TEST "__FILE__ in include file ignored if sloppy"
+    TEST "Relative includes for identical source code in different directories"
 
-    cat <<EOF >file.h
-#define file __FILE__
-int test;
-EOF
-    backdate file.h
-    cat <<EOF >file_h.c
+    mkdir a
+    cat <<EOF >a/file.c
 #include "file.h"
 EOF
+    cat <<EOF >a/file.h
+int x = 1;
+EOF
+    backdate a/file.h
 
-    CCACHE_SLOPPINESS="$DEFAULT_SLOPPINESS file_macro" $CCACHE_COMPILE -c file_h.c
+    mkdir b
+    cat <<EOF >b/file.c
+#include "file.h"
+EOF
+    cat <<EOF >b/file.h
+int x = 2;
+EOF
+    backdate b/file.h
+
+    $CCACHE_COMPILE -c a/file.c
     expect_stat 'cache hit (direct)' 0
     expect_stat 'cache hit (preprocessed)' 0
     expect_stat 'cache miss' 1
 
-    CCACHE_SLOPPINESS="$DEFAULT_SLOPPINESS file_macro" $CCACHE_COMPILE -c file_h.c
+    $CCACHE_COMPILE -c a/file.c
     expect_stat 'cache hit (direct)' 1
     expect_stat 'cache hit (preprocessed)' 0
     expect_stat 'cache miss' 1
 
-    mv file_h.c file2_h.c
+    $CCACHE_COMPILE -c b/file.c
+    expect_stat 'cache hit (direct)' 1
+    expect_stat 'cache hit (preprocessed)' 0
+    expect_stat 'cache miss' 2
 
-    CCACHE_SLOPPINESS="$DEFAULT_SLOPPINESS file_macro" $CCACHE_COMPILE -c `pwd`/file2_h.c
+    $CCACHE_COMPILE -c b/file.c
     expect_stat 'cache hit (direct)' 2
     expect_stat 'cache hit (preprocessed)' 0
-    expect_stat 'cache miss' 1
+    expect_stat 'cache miss' 2
 
     # -------------------------------------------------------------------------
     TEST "__TIME__ in source file disables direct mode"

unittest/test_conf.c
@@ -132,7 +132,7 @@ TEST(conf_read_valid_config)
 		"read_only_direct = true\n"
 		"recache = true\n"
 		"run_second_cpp = false\n"
-		"sloppiness =     file_macro   ,time_macros,  include_file_mtime,include_file_ctime,file_stat_matches,file_stat_matches_ctime,pch_defines ,  no_system_headers,system_headers,clang_index_store\n"
+		"sloppiness =     time_macros   ,include_file_mtime  include_file_ctime,file_stat_matches,file_stat_matches_ctime,pch_defines ,  no_system_headers,system_headers,clang_index_store\n"
 		"stats = false\n"
 		"temporary_dir = ${USER}_foo\n"
 		"umask = 777\n"
@@ -175,7 +175,6 @@ TEST(conf_read_valid_config)
 	CHECK_INT_EQ(
 		SLOPPY_INCLUDE_FILE_MTIME
 		|SLOPPY_INCLUDE_FILE_CTIME
-		|SLOPPY_FILE_MACRO
 		|SLOPPY_TIME_MACROS
 		|SLOPPY_FILE_STAT_MATCHES
 		|SLOPPY_FILE_STAT_MATCHES_CTIME
@@ -493,7 +492,7 @@ TEST(conf_print_items)
 		true,
 		true,
 		.run_second_cpp = false,
-		SLOPPY_FILE_MACRO|SLOPPY_INCLUDE_FILE_MTIME|
+		SLOPPY_INCLUDE_FILE_MTIME|
 		SLOPPY_INCLUDE_FILE_CTIME|SLOPPY_TIME_MACROS|
 		SLOPPY_FILE_STAT_MATCHES|SLOPPY_FILE_STAT_MATCHES_CTIME|
 		SLOPPY_PCH_DEFINES|SLOPPY_SYSTEM_HEADERS|SLOPPY_CLANG_INDEX_STORE,
@@ -546,7 +545,7 @@ TEST(conf_print_items)
 	CHECK_STR_EQ("read_only_direct = true", received_conf_items[n++].descr);
 	CHECK_STR_EQ("recache = true", received_conf_items[n++].descr);
 	CHECK_STR_EQ("run_second_cpp = false", received_conf_items[n++].descr);
-	CHECK_STR_EQ("sloppiness = file_macro, include_file_mtime,"
+	CHECK_STR_EQ("sloppiness = include_file_mtime,"
 	             " include_file_ctime, time_macros, pch_defines,"
 	             " file_stat_matches, file_stat_matches_ctime, system_headers,"
 	             " clang_index_store",