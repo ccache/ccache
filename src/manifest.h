6341a2abd47eb43123ce9fc92716d9768300795e
MizuhaHimuraki <mocha.java.cchip@gmail.com>
2019-12-03 04:54:22 +0800

    Store mtime/ctime as -1 if sloppy_file_stat is not set (#495)
    As discussed in #493.

--- src/manifest.h
+++ src/manifest.h
@@ -9,7 +9,7 @@
 
 struct file_hash *manifest_get(struct conf *conf, const char *manifest_path);
 bool manifest_put(const char *manifest_path, struct file_hash *object_hash,
-                  struct hashtable *included_files);
+                  struct hashtable *included_files, bool save_timestamp);
 bool manifest_dump(const char *manifest_path, FILE *stream);
 
 #endif
