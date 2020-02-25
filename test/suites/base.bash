ccache/ccache

commit dcf6ab3f349a33974b31ce2e7c06a2085149ab4a

Use Config for init_log()

src/logging.cpp
@@ -45,6 +45,9 @@
 // Destination for g_config.log_file.
 static FILE* logfile;
 
+// Path to the logfile.
+static std::string logfile_path;
+
 // Whether to use syslog() instead.
 static bool use_syslog;
 
@@ -59,28 +62,32 @@ static size_t debug_log_size;
 
 #define DEBUG_LOG_BUFFER_MARGIN 1024
 
-static bool
-init_log(void)
+// Initialize logging. Call only once.
+bool
+init_log(const Config& config)
 {
-  if (debug_log_buffer || logfile || use_syslog) {
-    return true;
+  logfile_path = config.log_file();
+
+  if (logfile_path.empty()) {
+    return false;
   }
-  if (g_config.debug()) {
+
+  if (config.debug()) {
     debug_log_buffer_capacity = DEBUG_LOG_BUFFER_MARGIN;
     debug_log_buffer = static_cast<char*>(x_malloc(debug_log_buffer_capacity));
     debug_log_size = 0;
   }
-  if (g_config.log_file().empty()) {
-    return g_config.debug();
+  if (config.log_file().empty()) {
+    return config.debug();
   }
 #ifdef HAVE_SYSLOG
-  if (g_config.log_file() == "syslog") {
+  if (config.log_file() == "syslog") {
     use_syslog = true;
     openlog("ccache", LOG_PID, LOG_USER);
     return true;
   }
 #endif
-  logfile = fopen(g_config.log_file().c_str(), "a");
+  logfile = fopen(logfile_path.c_str(), "a");
   if (logfile) {
 #ifndef _WIN32
     set_cloexec_flag(fileno(logfile));
@@ -152,7 +159,7 @@ warn_log_fail(void)
   // Note: Can't call fatal() since that would lead to recursion.
   fprintf(stderr,
           "ccache: error: Failed to write to %s: %s\n",
-          g_config.log_file().c_str(),
+          logfile_path.c_str(),
           strerror(errno));
   x_exit(EXIT_FAILURE);
 }
@@ -160,7 +167,7 @@ warn_log_fail(void)
 static void
 vlog(const char* format, va_list ap, bool log_updated_time)
 {
-  if (!init_log()) {
+  if (!(debug_log_buffer || logfile || use_syslog)) {
     return;
   }
 
@@ -218,7 +225,7 @@ cc_bulklog(const char* format, ...)
 void
 cc_log_argv(const char* prefix, char** argv)
 {
-  if (!init_log()) {
+  if (!(debug_log_buffer || logfile || use_syslog)) {
     return;
   }
 

src/logging.hpp
@@ -22,6 +22,9 @@
 
 #include <string>
 
+class Config;
+
+bool init_log(const Config& config);
 void cc_log(const char* format, ...) ATTR_FORMAT(printf, 1, 2);
 void cc_bulklog(const char* format, ...) ATTR_FORMAT(printf, 1, 2);
 void cc_log_argv(const char* prefix, char** argv);

src/ccache.cpp
@@ -3487,11 +3487,14 @@ initialize()
 #endif
   }
 
+  set_up_config(g_config);
+
+  init_log(g_config);
+
   exitfn_init();
   exitfn_add_nullary(stats_flush);
   exitfn_add_nullary(clean_up_pending_tmp_files);
 
-  set_up_config(g_config);
 
   cc_log("=== CCACHE %s STARTED =========================================",
          CCACHE_VERSION);

commit e0f5063b159de97ca9cc3dae8dbab67628ccb33d

Fix empty <obj>.ccache-log in debug mode without log_file set
Regression in dcf6ab3f349a33974b31ce2e7c06a2085149ab4a.

src/logging.cpp
@@ -63,39 +63,30 @@ static size_t debug_log_size;
 #define DEBUG_LOG_BUFFER_MARGIN 1024
 
 // Initialize logging. Call only once.
-bool
+void
 init_log(const Config& config)
 {
-  logfile_path = config.log_file();
-
-  if (logfile_path.empty()) {
-    return false;
-  }
-
   if (config.debug()) {
     debug_log_buffer_capacity = DEBUG_LOG_BUFFER_MARGIN;
     debug_log_buffer = static_cast<char*>(x_malloc(debug_log_buffer_capacity));
     debug_log_size = 0;
   }
-  if (config.log_file().empty()) {
-    return config.debug();
-  }
+
 #ifdef HAVE_SYSLOG
   if (config.log_file() == "syslog") {
     use_syslog = true;
     openlog("ccache", LOG_PID, LOG_USER);
-    return true;
+    return; // Don't open logfile
   }
 #endif
+
+  logfile_path = config.log_file();
   logfile = fopen(logfile_path.c_str(), "a");
-  if (logfile) {
 #ifndef _WIN32
+  if (logfile) {
     set_cloexec_flag(fileno(logfile));
-#endif
-    return true;
-  } else {
-    return false;
   }
+#endif
 }
 
 static void

src/logging.hpp
@@ -24,7 +24,7 @@
 
 class Config;
 
-bool init_log(const Config& config);
+void init_log(const Config& config);
 void cc_log(const char* format, ...) ATTR_FORMAT(printf, 1, 2);
 void cc_bulklog(const char* format, ...) ATTR_FORMAT(printf, 1, 2);
 void cc_log_argv(const char* prefix, char** argv);

test/suites/base.bash
@@ -297,6 +297,24 @@ base_tests() {
     expect_stat 'files in cache' 1
 
     # -------------------------------------------------------------------------
+    TEST "CCACHE_DEBUG"
+
+    unset CCACHE_LOGFILE
+    unset CCACHE_NODIRECT
+    CCACHE_DEBUG=1 $CCACHE_COMPILE -c test1.c
+    if ! grep -q Result: test1.o.ccache-log; then
+        test_failed "Unexpected data in <obj>.ccache-log"
+    fi
+    if ! grep -q "PREPROCESSOR MODE" test1.o.ccache-input-text; then
+        test_failed "Unexpected data in <obj>.ccache-input-text"
+    fi
+    for ext in c p d; do
+        if ! [ -f test1.o.ccache-input-$ext ]; then
+            test_failed "<obj>.ccache-input-$ext missing"
+        fi
+    done
+
+    # -------------------------------------------------------------------------
     TEST "CCACHE_DISABLE"
 
     CCACHE_DISABLE=1 $CCACHE_COMPILE -c test1.c 2>/dev/null