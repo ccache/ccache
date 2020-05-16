b91a8d6ffc265c1d9e7fa1f2ac564d4298eeaa9b
Alexander Lanin <alex@lanin.de>
2020-05-16 08:20:35 +0200

    Make cmake-format optional

--- misc/check_format.sh
+++ misc/check_format.sh
@@ -1,4 +1,8 @@
-#!/bin/sh -ex
+#!/bin/sh -e
+
+if [ -n "${VERBOSE}" ]; then
+  set -x
+fi
 
 # xarg returns 1 if any run-clang-format call returns 1.
 clang-format --version
@@ -6,8 +10,15 @@ find src unittest -path src/third_party -prune -o -regex ".*\.[ch]p?p?" -print0 
 
 # Top level CMakeLists.txt + subidrectories.
 # This avoids running the check on any build directories.
-printf "cmake-format version "
-cmake-format --version
-CLANG_FORMAT=cmake-format misc/run-clang-format --check CMakeLists.txt
-find cmake -name "*.cmake" -print0 | CLANG_FORMAT=cmake-format xargs -0 -n1 misc/run-clang-format --check
-find src unittest -name "CMakeLists.txt" -print0 | CLANG_FORMAT=cmake-format xargs -0 -n1 misc/run-clang-format --check
+if hash cmake-format 2>/dev/null; then
+  printf "cmake-format version "
+  cmake-format --version
+  CLANG_FORMAT=cmake-format misc/run-clang-format --check CMakeLists.txt
+  find cmake -name "*.cmake" -print0 | CLANG_FORMAT=cmake-format xargs -0 -n1 misc/run-clang-format --check
+  find src unittest -name "CMakeLists.txt" -print0 | CLANG_FORMAT=cmake-format xargs -0 -n1 misc/run-clang-format --check
+else
+  echo "Note: cmake-format not installed. CMake files will not be checked for correct formatting."
+  echo "You can install it via pip3 install cmake-format"
+fi
+
+echo "Format is ok"
