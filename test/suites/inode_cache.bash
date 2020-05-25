5a557397996ced3df5ad86bd7d3a8852c3103222
Olle Liljenzin <olle@liljenzin.se>
2020-05-25 20:07:43 +0200

    Put inode cache file in /tmp when running tests to avoid nfs issues

--- test/suites/inode_cache.bash
+++ test/suites/inode_cache.bash
@@ -2,10 +2,17 @@ SUITE_inode_cache_SETUP() {
     export CCACHE_INODECACHE=1
     unset CCACHE_NODIRECT
     cat /dev/null > $CCACHE_LOGFILE
+    export CCACHE_INODECACHEFILE=`mktemp -u -t inode-cache-file-XXXXXX`
 }
 
 SUITE_inode_cache() {
     inode_cache_tests
+}
+
+inode_cache_cleanup() {
+    if [ -f ${CCACHE_INODECACHEFILE}.v* ]; then
+        rm -f ${CCACHE_INODECACHEFILE}.v*
+    fi
 }
 
 expect_inode_cache_type() {
@@ -14,6 +21,7 @@ expect_inode_cache_type() {
     local type=$3
     local actual=`grep "inode cache $type: $source_file" $CCACHE_LOGFILE|wc -l`
     if [ $actual -ne $expected ]; then
+        inode_cache_cleanup
         test_failed "Found $actual (expected $expected) $type for $source_file"
     fi
 }
@@ -22,6 +30,7 @@ expect_inode_cache() {
     expect_inode_cache_type $1 $4 hit
     expect_inode_cache_type $2 $4 miss
     expect_inode_cache_type $3 $4 insert
+    inode_cache_cleanup
 }
 
 inode_cache_tests() {
@@ -31,6 +40,7 @@ inode_cache_tests() {
     echo "// compile once" > test1.c
     $CCACHE_COMPILE -c test1.c
     expect_inode_cache 0 1 1 test1.c
+    inode_cache_cleanup
 
     # -------------------------------------------------------------------------
     TEST "Recompile"
@@ -39,6 +49,7 @@ inode_cache_tests() {
     $CCACHE_COMPILE -c test1.c
     $CCACHE_COMPILE -c test1.c
     expect_inode_cache 1 1 1 test1.c
+    inode_cache_cleanup
 
     # -------------------------------------------------------------------------
     TEST "Backdate"
@@ -48,6 +59,7 @@ inode_cache_tests() {
     backdate test1.c
     $CCACHE_COMPILE -c test1.c
     expect_inode_cache 0 2 2 test1.c
+    inode_cache_cleanup
 
     # -------------------------------------------------------------------------
     TEST "Hard link"
@@ -58,6 +70,7 @@ inode_cache_tests() {
     $CCACHE_COMPILE -c test2.c
     expect_inode_cache 0 1 1 test1.c
     expect_inode_cache 1 0 0 test2.c
+    inode_cache_cleanup
 
     # -------------------------------------------------------------------------
     TEST "Soft link"
@@ -68,6 +81,7 @@ inode_cache_tests() {
     $CCACHE_COMPILE -c test2.c
     expect_inode_cache 0 1 1 test1.c
     expect_inode_cache 1 0 0 test2.c
+    inode_cache_cleanup
 
     # -------------------------------------------------------------------------
     TEST "Replace"
@@ -78,19 +92,5 @@ inode_cache_tests() {
     echo "// replace" > test1.c
     $CCACHE_COMPILE -c test1.c
     expect_inode_cache 0 2 2 test1.c
-
-    # -------------------------------------------------------------------------
-    TEST "Profile file"
-
-return
-    echo "// replace" > test1.c
-    echo 'main:1:1' > sample.prof
-    $CCACHE_COMPILE -c test1.c
-    expect_inode_cache 0 2 2 test1.c
-    echo 'main:2:2' > sample.prof
-    $CCACHE_COMPILE -c test1.c
-    expect_inode_cache 1 3 3 test1.c
-    echo 'main:1:1' > sample.prof
-    $CCACHE_COMPILE -c test1.c
-    expect_inode_cache 2 3 3 test1.c
+    inode_cache_cleanup
 }
