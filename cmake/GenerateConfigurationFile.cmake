c753b8f37ab19904ce1196d4183433ebb3523eaf
Joel Rosdahl <joel@rosdahl.net>
2020-06-01 19:53:28 +0200

    Add configure checks for struct members

--- cmake/GenerateConfigurationFile.cmake
+++ cmake/GenerateConfigurationFile.cmake
@@ -27,6 +27,14 @@ foreach(
   check_function_exists(${func} ${func_var})
 endforeach()
 
+include(CheckStructHasMember)
+check_struct_has_member("struct stat" st_ctim sys/stat.h
+                        HAVE_STRUCT_STAT_ST_CTIM)
+check_struct_has_member("struct stat" st_mtim sys/stat.h
+                        HAVE_STRUCT_STAT_ST_MTIM)
+check_struct_has_member("struct statfs" f_fstypename sys/mount.h
+                        HAVE_STRUCT_STATFS_F_FSTYPENAME)
+
 list(APPEND CMAKE_REQUIRED_LIBRARIES ws2_32)
 list(REMOVE_ITEM CMAKE_REQUIRED_LIBRARIES ws2_32)
 
