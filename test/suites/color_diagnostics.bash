163bf67ab4d279f5b008a9d2ca24f0caaab27a5c
Matt Whitlock <matt@whitlock.name>
2020-06-05 17:20:39 -0400

    test/run: expect_file_{,not_}contains: do not support regexps
    According to Joel Rosdahl, the intention in
    3eb2483f66fc482df3bf31616d35daedc5ac49cc was not to support regular
    expressions. This commit makes expect_file_{,not_}contains perform
    literal substring matching only.
    
    See: https://github.com/ccache/ccache/pull/596#discussion_r434820232

--- test/suites/color_diagnostics.bash
+++ test/suites/color_diagnostics.bash
@@ -26,13 +26,13 @@ SUITE_color_diagnostics_SETUP() {
 }
 
 color_diagnostics_expect_color() {
-    expect_file_contains "${1:?}" $'\033\\['
-    expect_file_contains <(fgrep 'previous prototype' "$1") $'\033\\['
-    expect_file_contains <(fgrep 'from preprocessor' "$1") $'\033\\['
+    expect_file_contains "${1:?}" $'\033['
+    expect_file_contains <(fgrep 'previous prototype' "$1") $'\033['
+    expect_file_contains <(fgrep 'from preprocessor' "$1") $'\033['
 }
 
 color_diagnostics_expect_no_color() {
-    expect_file_not_contains "${1:?}" $'\033\\['
+    expect_file_not_contains "${1:?}" $'\033['
 }
 
 color_diagnostics_generate_code() {
