ccache/ccache

commit 213d9883a0c3944749f8512eeb784d8572ad8d91

Add inode cache for file hashes (#577)
The inode cache is a process shared cache that maps from device, inode,
size, mtime and ctime to saved hash results. The cache is stored
persistently in a single file that is mapped into shared memory by
running processes, allowing computed hash values to be reused both
within and between builds.

The chosen technical solution works for Linux and might work on other POSIX
platforms, but is not meant to be supported on non-POSIX platforms
such as Windows.

Use 'ccache -o inode_cache=true/false' to activate/deactivate the cache.

src/hashutil.cpp
@@ -21,6 +21,7 @@
 #include "Args.hpp"
 #include "Config.hpp"
 #include "Context.hpp"
+#include "InodeCache.hpp"
 #include "Stat.hpp"
 #include "ccache.hpp"
 #include "execute.hpp"
@@ -188,7 +189,7 @@ check_for_temporal_macros(const char* str, size_t len)
 
 // Hash a string. Returns a bitmask of HASH_SOURCE_CODE_* results.
 int
-hash_source_code_string(const Config& config,
+hash_source_code_string(const Context& ctx,
                         struct hash* hash,
                         const char* str,
                         size_t len,
@@ -198,7 +199,7 @@ hash_source_code_string(const Config& config,
 
   // Check for __DATE__, __TIME__ and __TIMESTAMP__if the sloppiness
   // configuration tells us we should.
-  if (!(config.sloppiness() & SLOPPY_TIME_MACROS)) {
+  if (!(ctx.config.sloppiness() & SLOPPY_TIME_MACROS)) {
     result |= check_for_temporal_macros(str, len);
   }
 
@@ -261,15 +262,14 @@ hash_source_code_string(const Config& config,
   return result;
 }
 
-// Hash a file ignoring comments. Returns a bitmask of HASH_SOURCE_CODE_*
-// results.
-int
-hash_source_code_file(const Config& config,
-                      struct hash* hash,
-                      const char* path,
-                      size_t size_hint)
+static int
+hash_source_code_file_nocache(const Context& ctx,
+                              struct hash* hash,
+                              const char* path,
+                              size_t size_hint,
+                              bool is_precompiled)
 {
-  if (is_precompiled_header(path)) {
+  if (is_precompiled) {
     if (hash_file(hash, path)) {
       return HASH_SOURCE_CODE_OK;
     } else {
@@ -281,10 +281,98 @@ hash_source_code_file(const Config& config,
     if (!read_file(path, size_hint, &data, &size)) {
       return HASH_SOURCE_CODE_ERROR;
     }
-    int result = hash_source_code_string(config, hash, data, size, path);
+    int result = hash_source_code_string(ctx, hash, data, size, path);
     free(data);
     return result;
   }
+}
+
+#ifdef INODE_CACHE_SUPPORTED
+static InodeCache::ContentType
+get_content_type(const Config& config, const char* path)
+{
+  if (is_precompiled_header(path)) {
+    return InodeCache::ContentType::precompiled_header;
+  }
+  if (config.sloppiness() & SLOPPY_TIME_MACROS) {
+    return InodeCache::ContentType::code_with_sloppy_time_macros;
+  }
+  return InodeCache::ContentType::code;
+}
+#endif
+
+// Hash a file ignoring comments. Returns a bitmask of HASH_SOURCE_CODE_*
+// results.
+int
+hash_source_code_file(const Context& ctx,
+                      struct hash* hash,
+                      const char* path,
+                      size_t size_hint)
+{
+#ifdef INODE_CACHE_SUPPORTED
+  if (!ctx.config.inode_cache()) {
+#endif
+    return hash_source_code_file_nocache(
+      ctx, hash, path, size_hint, is_precompiled_header(path));
+
+#ifdef INODE_CACHE_SUPPORTED
+  }
+
+  // Reusable file hashes must be independent of the outer context. Thus hash
+  // files separately so that digests based on file contents can be reused. Then
+  // add the digest into the outer hash instead.
+  InodeCache::ContentType content_type = get_content_type(ctx.config, path);
+  struct digest digest;
+  int return_value;
+  if (!ctx.inode_cache.get(path, content_type, &digest, &return_value)) {
+    struct hash* file_hash = hash_init();
+    return_value = hash_source_code_file_nocache(
+      ctx,
+      file_hash,
+      path,
+      size_hint,
+      content_type == InodeCache::ContentType::precompiled_header);
+    if (return_value == HASH_SOURCE_CODE_ERROR) {
+      return HASH_SOURCE_CODE_ERROR;
+    }
+    hash_result_as_bytes(file_hash, &digest);
+    hash_free(file_hash);
+    ctx.inode_cache.put(path, content_type, digest, return_value);
+  }
+  hash_buffer(hash, &digest.bytes, sizeof(digest::bytes));
+  return return_value;
+#endif
+}
+
+// Hash a binary file using the inode cache if enabled.
+//
+// Returns true on success, otherwise false.
+bool
+hash_binary_file(const Context& ctx, struct hash* hash, const char* path)
+{
+  if (!ctx.config.inode_cache()) {
+    return hash_file(hash, path);
+  }
+
+#ifdef INODE_CACHE_SUPPORTED
+  // Reusable file hashes must be independent of the outer context. Thus hash
+  // files separately so that digests based on file contents can be reused. Then
+  // add the digest into the outer hash instead.
+  struct digest digest;
+  if (!ctx.inode_cache.get(path, InodeCache::ContentType::binary, &digest)) {
+    struct hash* file_hash = hash_init();
+    if (!hash_file(hash, path)) {
+      return false;
+    }
+    hash_result_as_bytes(file_hash, &digest);
+    hash_free(file_hash);
+    ctx.inode_cache.put(path, InodeCache::ContentType::binary, digest);
+  }
+  hash_buffer(hash, &digest.bytes, sizeof(digest::bytes));
+  return true;
+#else
+  return hash_file(hash, path);
+#endif
 }
 
 bool

Makefile.in
@@ -42,6 +42,7 @@ non_third_party_sources = \
     src/Context.cpp \
     src/Counters.cpp \
     src/Decompressor.cpp \
+    src/InodeCache.cpp \
     src/Lockfile.cpp \
     src/MiniTrace.cpp \
     src/NullCompressor.cpp \
@@ -89,6 +90,7 @@ test_suites += unittest/test_Checksum.cpp
 test_suites += unittest/test_Compression.cpp
 test_suites += unittest/test_Config.cpp
 test_suites += unittest/test_FormatNonstdStringView.cpp
+test_suites += unittest/test_InodeCache.cpp
 test_suites += unittest/test_Lockfile.cpp
 test_suites += unittest/test_NullCompression.cpp
 test_suites += unittest/test_Stat.cpp

configure.ac
@@ -120,17 +120,22 @@ AC_CHECK_HEADERS(linux/fs.h)
 AC_CHECK_HEADERS(sys/clonefile.h)
 
 AC_CHECK_FUNCS(asctime_r)
+AC_CHECK_FUNCS(geteuid)
 AC_CHECK_FUNCS(getopt_long)
 AC_CHECK_FUNCS(getpwuid)
 AC_CHECK_FUNCS(gettimeofday)
 AC_CHECK_FUNCS(localtime_r)
 AC_CHECK_FUNCS(mkstemp)
+AC_CHECK_FUNCS(posix_fallocate)
 AC_CHECK_FUNCS(realpath)
 AC_CHECK_FUNCS(setenv)
 AC_CHECK_FUNCS(strndup)
 AC_CHECK_FUNCS(syslog)
 AC_CHECK_FUNCS(unsetenv)
 AC_CHECK_FUNCS(utimes)
+
+AC_CHECK_MEMBERS([struct stat.st_ctim, struct stat.st_mtim])
+AC_CHECK_MEMBERS([struct statfs.f_fstypename], [], [], [#include <sys/mount.h>])
 
 dnl Check if -lm is needed.
 AC_SEARCH_LIBS(cos, m)

src/Config.cpp
@@ -54,6 +54,7 @@ enum class ConfigItem {
   hard_link,
   hash_dir,
   ignore_headers_in_manifest,
+  inode_cache,
   keep_comments_cpp,
   limit_multiple,
   log_file,
@@ -91,6 +92,7 @@ const std::unordered_map<std::string, ConfigItem> k_config_key_table = {
   {"hard_link", ConfigItem::hard_link},
   {"hash_dir", ConfigItem::hash_dir},
   {"ignore_headers_in_manifest", ConfigItem::ignore_headers_in_manifest},
+  {"inode_cache", ConfigItem::inode_cache},
   {"keep_comments_cpp", ConfigItem::keep_comments_cpp},
   {"limit_multiple", ConfigItem::limit_multiple},
   {"log_file", ConfigItem::log_file},
@@ -130,6 +132,7 @@ const std::unordered_map<std::string, std::string> k_env_variable_table = {
   {"HARDLINK", "hard_link"},
   {"HASHDIR", "hash_dir"},
   {"IGNOREHEADERS", "ignore_headers_in_manifest"},
+  {"INODECACHE", "inode_cache"},
   {"LIMIT_MULTIPLE", "limit_multiple"},
   {"LOGFILE", "log_file"},
   {"MAXFILES", "max_files"},
@@ -556,6 +559,9 @@ Config::get_string_value(const std::string& key) const
   case ConfigItem::ignore_headers_in_manifest:
     return m_ignore_headers_in_manifest;
 
+  case ConfigItem::inode_cache:
+    return format_bool(m_inode_cache);
+
   case ConfigItem::keep_comments_cpp:
     return format_bool(m_keep_comments_cpp);
 
@@ -760,6 +766,10 @@ Config::set_item(const std::string& key,
     m_ignore_headers_in_manifest = parse_env_string(value);
     break;
 
+  case ConfigItem::inode_cache:
+    m_inode_cache = parse_bool(value, env_var_key, negate);
+    break;
+
   case ConfigItem::keep_comments_cpp:
     m_keep_comments_cpp = parse_bool(value, env_var_key, negate);
     break;
@@ -844,4 +854,15 @@ Config::check_key_tables_consistency()
         item.second));
     }
   }
+}
+
+std::string
+Config::default_temporary_dir(const std::string& cache_dir)
+{
+#ifdef HAVE_GETEUID
+  if (Stat::stat(fmt::format("/run/user/{}", geteuid())).is_directory()) {
+    return fmt::format("/run/user/{}/ccache-tmp", geteuid());
+  }
+#endif
+  return cache_dir + "/tmp";
 }

src/Config.hpp
@@ -55,6 +55,7 @@ public:
   bool hard_link() const;
   bool hash_dir() const;
   const std::string& ignore_headers_in_manifest() const;
+  bool inode_cache() const;
   bool keep_comments_cpp() const;
   double limit_multiple() const;
   const std::string& log_file() const;
@@ -77,7 +78,9 @@ public:
   void set_cache_dir(const std::string& value);
   void set_cpp_extension(const std::string& value);
   void set_depend_mode(bool value);
+  void set_debug(bool value);
   void set_direct_mode(bool value);
+  void set_inode_cache(bool value);
   void set_limit_multiple(double value);
   void set_max_files(uint32_t value);
   void set_max_size(uint64_t value);
@@ -139,6 +142,7 @@ private:
   bool m_hard_link = false;
   bool m_hash_dir = true;
   std::string m_ignore_headers_in_manifest = "";
+  bool m_inode_cache = false;
   bool m_keep_comments_cpp = false;
   double m_limit_multiple = 0.8;
   std::string m_log_file = "";
@@ -154,7 +158,7 @@ private:
   bool m_run_second_cpp = true;
   uint32_t m_sloppiness = 0;
   bool m_stats = true;
-  std::string m_temporary_dir = fmt::format(m_cache_dir + "/tmp");
+  std::string m_temporary_dir = default_temporary_dir(m_cache_dir);
   uint32_t m_umask = std::numeric_limits<uint32_t>::max(); // Don't set umask
 
   bool m_temporary_dir_configured_explicitly = false;
@@ -166,6 +170,8 @@ private:
                 const nonstd::optional<std::string>& env_var_key,
                 bool negate,
                 const std::string& origin);
+
+  static std::string default_temporary_dir(const std::string& cache_dir);
 };
 
 inline const std::string&
@@ -268,6 +274,12 @@ inline const std::string&
 Config::ignore_headers_in_manifest() const
 {
   return m_ignore_headers_in_manifest;
+}
+
+inline bool
+Config::inode_cache() const
+{
+  return m_inode_cache;
 }
 
 inline bool
@@ -383,7 +395,7 @@ Config::set_cache_dir(const std::string& value)
 {
   m_cache_dir = value;
   if (!m_temporary_dir_configured_explicitly) {
-    m_temporary_dir = m_cache_dir + "/tmp";
+    m_temporary_dir = default_temporary_dir(m_cache_dir);
   }
 }
 
@@ -400,9 +412,21 @@ Config::set_depend_mode(bool value)
 }
 
 inline void
+Config::set_debug(bool value)
+{
+  m_debug = value;
+}
+
+inline void
 Config::set_direct_mode(bool value)
 {
   m_direct_mode = value;
+}
+
+inline void
+Config::set_inode_cache(bool value)
+{
+  m_inode_cache = value;
 }
 
 inline void

src/Context.cpp
@@ -30,6 +30,10 @@ using nonstd::string_view;
 Context::Context()
   : actual_cwd(Util::get_actual_cwd()),
     apparent_cwd(Util::get_apparent_cwd(actual_cwd))
+#ifdef INODE_CACHE_SUPPORTED
+    ,
+    inode_cache(config)
+#endif
 {
 }
 

src/Context.hpp
@@ -24,6 +24,7 @@
 #include "ArgsInfo.hpp"
 #include "Config.hpp"
 #include "File.hpp"
+#include "InodeCache.hpp"
 #include "MiniTrace.hpp"
 #include "NonCopyable.hpp"
 #include "ccache.hpp"
@@ -101,6 +102,11 @@ public:
 
   // Headers (or directories with headers) to ignore in manifest mode.
   std::vector<std::string> ignore_header_paths;
+
+#ifdef INODE_CACHE_SUPPORTED
+  // InodeCache that caches source file hashes when enabled.
+  mutable InodeCache inode_cache;
+#endif
 
   // Full path to the statistics file in the subdirectory where the cached
   // result belongs (<cache_dir>/<x>/stats).

src/InodeCache.cpp
@@ -1,0 +1,494 @@
+// Copyright (C) 2020 Joel Rosdahl and other contributors
+//
+// See doc/AUTHORS.adoc for a complete list of contributors.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc., 51
+// Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+#include "InodeCache.hpp"
+
+#ifdef INODE_CACHE_SUPPORTED
+
+#  include "Config.hpp"
+#  include "Stat.hpp"
+#  include "Util.hpp"
+#  include "ccache.hpp"
+#  include "hash.hpp"
+#  include "logging.hpp"
+
+#  include <atomic>
+#  include <errno.h>
+#  include <fcntl.h>
+#  include <libgen.h>
+#  include <stdio.h>
+#  include <stdlib.h>
+#  include <string.h>
+#  include <sys/mman.h>
+#  include <sys/stat.h>
+#  include <sys/types.h>
+#  include <time.h>
+#  include <unistd.h>
+
+namespace {
+
+// The inode cache resides on a file that is mapped into shared memory by
+// running processes. It is implemented as a two level structure, where the
+// top level is a hash table consisting of buckets. Each bucket contains entries
+// that are sorted in LRU order. Entries map from keys representing files to
+// cached hash results.
+//
+// Concurrent access is guarded by a mutex in each bucket.
+//
+// Current cache size is fixed and the given constants are considered large
+// enough for most projects. The size could be made configurable if there is a
+// demand for it.
+const uint32_t k_version = 1;
+
+// Increment version number if constants affecting storage size are changed.
+const uint32_t k_num_buckets = 32 * 1024;
+const uint32_t k_num_entries = 4;
+
+static_assert(sizeof(digest::bytes) == 20,
+              "Increment version number if size of digest is changed.");
+
+static_assert(
+  static_cast<int>(InodeCache::ContentType::binary) == 0,
+  "Numeric value is part of key, increment version number if changed.");
+static_assert(
+  static_cast<int>(InodeCache::ContentType::code) == 1,
+  "Numeric value is part of key, increment version number if changed.");
+static_assert(
+  static_cast<int>(InodeCache::ContentType::code_with_sloppy_time_macros) == 2,
+  "Numeric value is part of key, increment version number if changed.");
+static_assert(
+  static_cast<int>(InodeCache::ContentType::precompiled_header) == 3,
+  "Numeric value is part of key, increment version number if changed.");
+
+} // namespace
+
+struct InodeCache::Key
+{
+  ContentType type;
+  dev_t st_dev;
+  ino_t st_ino;
+  mode_t st_mode;
+#  ifdef HAVE_STRUCT_STAT_ST_MTIM
+  timespec st_mtim;
+#  else
+  time_t st_mtim;
+#  endif
+#  ifdef HAVE_STRUCT_STAT_ST_CTIM
+  timespec st_ctim; // Included for sanity checking.
+#  else
+  time_t st_ctim; // Included for sanity checking.
+#  endif
+  off_t st_size; // Included for sanity checking.
+  bool sloppy_time_macros;
+};
+
+struct InodeCache::Entry
+{
+  digest key_digest;  // Hashed key
+  digest file_digest; // Cached file hash
+  int return_value;   // Cached return value
+};
+
+struct InodeCache::Bucket
+{
+  pthread_mutex_t mt;
+  Entry entries[k_num_entries];
+};
+
+struct InodeCache::SharedRegion
+{
+  uint32_t version;
+  std::atomic<int64_t> hits;
+  std::atomic<int64_t> misses;
+  std::atomic<int64_t> errors;
+  Bucket buckets[k_num_buckets];
+};
+
+bool
+InodeCache::mmap_file(const std::string& inode_cache_file)
+{
+  if (m_sr) {
+    munmap(m_sr, sizeof(SharedRegion));
+    m_sr = nullptr;
+  }
+  int fd = open(inode_cache_file.c_str(), O_RDWR);
+  if (fd < 0) {
+    cc_log("Failed to open inode cache %s: %s",
+           inode_cache_file.c_str(),
+           strerror(errno));
+    return false;
+  }
+  bool is_nfs;
+  if (Util::is_nfs_fd(fd, &is_nfs) == 0 && is_nfs) {
+    cc_log(
+      "Inode cache not supported because the cache file is located on nfs: %s",
+      inode_cache_file.c_str());
+    close(fd);
+    return false;
+  }
+  SharedRegion* sr = reinterpret_cast<SharedRegion*>(mmap(
+    nullptr, sizeof(SharedRegion), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+  close(fd);
+  if (sr == reinterpret_cast<void*>(-1)) {
+    cc_log("Failed to mmap %s: %s", inode_cache_file.c_str(), strerror(errno));
+    return false;
+  }
+  // Drop the file from disk if the found version is not matching. This will
+  // allow a new file to be generated.
+  if (sr->version != k_version) {
+    cc_log(
+      "Dropping inode cache because found version %u does not match expected "
+      "version %u",
+      sr->version,
+      k_version);
+    munmap(sr, sizeof(SharedRegion));
+    unlink(inode_cache_file.c_str());
+    return false;
+  }
+  m_sr = sr;
+  if (m_config.debug()) {
+    cc_log("inode cache file loaded: %s", inode_cache_file.c_str());
+  }
+  return true;
+}
+
+bool
+InodeCache::hash_inode(const char* path, ContentType type, digest* digest)
+{
+  Stat stat = Stat::stat(path);
+  if (!stat) {
+    cc_log("Could not stat %s: %s", path, strerror(stat.error_number()));
+    return false;
+  }
+
+  Key key;
+  memset(&key, 0, sizeof(Key));
+  key.type = type;
+  key.st_dev = stat.device();
+  key.st_ino = stat.inode();
+  key.st_mode = stat.mode();
+#  ifdef HAVE_STRUCT_STAT_ST_MTIM
+  key.st_mtim = stat.mtim();
+#  else
+  key.st_mtim = stat.mtime();
+#  endif
+#  ifdef HAVE_STRUCT_STAT_ST_CTIM
+  key.st_ctim = stat.ctim();
+#  else
+  key.st_ctim = stat.ctime();
+#  endif
+  key.st_size = stat.size();
+
+  struct hash* hash = hash_init();
+  hash_buffer(hash, &key, sizeof(Key));
+  hash_result_as_bytes(hash, digest);
+  hash_free(hash);
+  return true;
+}
+
+InodeCache::Bucket*
+InodeCache::acquire_bucket(uint32_t index)
+{
+  Bucket* bucket = &m_sr->buckets[index];
+  int err = pthread_mutex_lock(&bucket->mt);
+#  ifdef PTHREAD_MUTEX_ROBUST
+  if (err == EOWNERDEAD) {
+    if (m_config.debug())
+      ++m_sr->errors;
+    err = pthread_mutex_consistent(&bucket->mt);
+    if (err) {
+      cc_log(
+        "Can't consolidate stale mutex at index %u: %s", index, strerror(err));
+      cc_log("Consider removing the inode cache file if the problem persists");
+      return nullptr;
+    }
+    cc_log("Wiping bucket at index %u because of stale mutex", index);
+    memset(bucket->entries, 0, sizeof(Bucket::entries));
+  } else {
+#  endif
+    if (err) {
+      cc_log("Failed to lock mutex at index %u: %s", index, strerror(err));
+      cc_log("Consider removing the inode cache file if problem persists");
+      ++m_sr->errors;
+      return nullptr;
+    }
+#  ifdef PTHREAD_MUTEX_ROBUST
+  }
+#  endif
+  return bucket;
+}
+
+InodeCache::Bucket*
+InodeCache::acquire_bucket(const digest& key_digest)
+{
+  uint32_t hash;
+  Util::big_endian_to_int(key_digest.bytes, hash);
+  return acquire_bucket(hash % k_num_buckets);
+}
+
+void
+InodeCache::release_bucket(Bucket* bucket)
+{
+  pthread_mutex_unlock(&bucket->mt);
+}
+
+bool
+InodeCache::create_new_file(const std::string& filename)
+{
+  cc_log("Creating a new inode cache");
+
+  // Create the new file to a temporary name to prevent other processes from
+  // mapping it before it is fully initialized.
+  auto temp_fd = Util::create_temp_fd(filename);
+  bool is_nfs;
+  if (Util::is_nfs_fd(temp_fd.first, &is_nfs) == 0 && is_nfs) {
+    cc_log(
+      "Inode cache not supported because the cache file would be located on "
+      "nfs: %s",
+      filename.c_str());
+    unlink(temp_fd.second.c_str());
+    close(temp_fd.first);
+    return false;
+  }
+  int err = Util::fallocate(temp_fd.first, sizeof(SharedRegion));
+  if (err) {
+    cc_log("Failed to allocate file space for inode cache: %s", strerror(err));
+    unlink(temp_fd.second.c_str());
+    close(temp_fd.first);
+    return false;
+  }
+  SharedRegion* sr =
+    reinterpret_cast<SharedRegion*>(mmap(nullptr,
+                                         sizeof(SharedRegion),
+                                         PROT_READ | PROT_WRITE,
+                                         MAP_SHARED,
+                                         temp_fd.first,
+                                         0));
+  if (sr == reinterpret_cast<void*>(-1)) {
+    cc_log("Failed to mmap new inode cache: %s", strerror(errno));
+    unlink(temp_fd.second.c_str());
+    close(temp_fd.first);
+    return false;
+  }
+
+  // Initialize new shared region.
+  sr->version = k_version;
+  pthread_mutexattr_t mattr;
+  pthread_mutexattr_init(&mattr);
+  pthread_mutexattr_setpshared(&mattr, PTHREAD_PROCESS_SHARED);
+#  ifdef PTHREAD_MUTEX_ROBUST
+  pthread_mutexattr_setrobust(&mattr, PTHREAD_MUTEX_ROBUST);
+#  endif
+  for (uint32_t i = 0; i < k_num_buckets; ++i) {
+    pthread_mutex_init(&sr->buckets[i].mt, &mattr);
+  }
+
+  munmap(sr, sizeof(SharedRegion));
+  close(temp_fd.first);
+
+  // link() will fail silently if a file with the same name already exists.
+  // This will be the case if two processes try to create a new file
+  // simultaneously. Thus close the current file handle and reopen a new one,
+  // which will make us use the first created file even if we didn't win the
+  // race.
+  if (link(temp_fd.second.c_str(), filename.c_str()) != 0) {
+    cc_log("Failed to link new inode cache: %s", strerror(errno));
+    unlink(temp_fd.second.c_str());
+    return false;
+  }
+
+  unlink(temp_fd.second.c_str());
+  return true;
+}
+
+bool
+InodeCache::initialize()
+{
+  if (m_failed || !m_config.inode_cache()) {
+    return false;
+  }
+
+  if (m_sr) {
+    return true;
+  }
+
+  std::string filename = get_file();
+  if (m_sr || mmap_file(filename)) {
+    return true;
+  }
+
+  // Try to create a new cache if we failed to map an existing file.
+  create_new_file(filename);
+
+  // Concurrent processes could try to create new files simultaneously and the
+  // file that actually landed on disk will be from the process that won the
+  // race. Thus we try to open the file from disk instead of reusing the file
+  // handle to the file we just created.
+  if (mmap_file(filename)) {
+    return true;
+  }
+
+  m_failed = true;
+  return false;
+}
+
+InodeCache::InodeCache(const Config& config)
+  : m_config(config), m_sr(nullptr), m_failed(false)
+{
+}
+
+InodeCache::~InodeCache()
+{
+  if (m_sr) {
+    munmap(m_sr, sizeof(SharedRegion));
+  }
+}
+
+bool
+InodeCache::get(const char* path,
+                ContentType type,
+                digest* file_digest,
+                int* return_value)
+{
+  if (!initialize()) {
+    return false;
+  }
+
+  digest key_digest;
+  if (!hash_inode(path, type, &key_digest)) {
+    return false;
+  }
+
+  Bucket* bucket = acquire_bucket(key_digest);
+
+  if (!bucket) {
+    return false;
+  }
+
+  bool found = false;
+
+  for (uint32_t i = 0; i < k_num_entries; ++i) {
+    if (digests_equal(&bucket->entries[i].key_digest, &key_digest)) {
+      if (i > 0) {
+        Entry tmp = bucket->entries[i];
+        memmove(&bucket->entries[1], &bucket->entries[0], sizeof(Entry) * i);
+        bucket->entries[0] = tmp;
+      }
+
+      *file_digest = bucket->entries[0].file_digest;
+      if (return_value) {
+        *return_value = bucket->entries[0].return_value;
+      }
+      found = true;
+      break;
+    }
+  }
+  release_bucket(bucket);
+
+  cc_log("inode cache %s: %s", found ? "hit" : "miss", path);
+
+  if (m_config.debug()) {
+    if (found) {
+      ++m_sr->hits;
+    } else {
+      ++m_sr->misses;
+    }
+    cc_log(
+      "accumulated stats for inode cache: hits=%ld, misses=%ld, errors=%ld",
+      static_cast<long>(m_sr->hits.load()),
+      static_cast<long>(m_sr->misses.load()),
+      static_cast<long>(m_sr->errors.load()));
+  }
+  return found;
+}
+
+bool
+InodeCache::put(const char* path,
+                ContentType type,
+                const digest& file_digest,
+                int return_value)
+{
+  if (!initialize()) {
+    return false;
+  }
+
+  digest key_digest;
+  if (!hash_inode(path, type, &key_digest)) {
+    return false;
+  }
+
+  Bucket* bucket = acquire_bucket(key_digest);
+
+  if (!bucket) {
+    return false;
+  }
+
+  memmove(&bucket->entries[1],
+          &bucket->entries[0],
+          sizeof(Entry) * (k_num_entries - 1));
+
+  bucket->entries[0].key_digest = key_digest;
+  bucket->entries[0].file_digest = file_digest;
+  bucket->entries[0].return_value = return_value;
+
+  release_bucket(bucket);
+
+  cc_log("inode cache insert: %s", path);
+
+  return true;
+}
+
+bool
+InodeCache::drop()
+{
+  std::string file = get_file();
+  if (unlink(file.c_str()) != 0) {
+    return false;
+  }
+  if (m_sr) {
+    munmap(m_sr, sizeof(SharedRegion));
+    m_sr = nullptr;
+  }
+  return true;
+}
+
+std::string
+InodeCache::get_file()
+{
+  return fmt::format("{}/inode-cache.v{}", m_config.temporary_dir(), k_version);
+}
+
+int64_t
+InodeCache::get_hits()
+{
+  return initialize() ? m_sr->hits.load() : -1;
+}
+
+int64_t
+InodeCache::get_misses()
+{
+  return initialize() ? m_sr->misses.load() : -1;
+}
+
+int64_t
+InodeCache::get_errors()
+{
+  return initialize() ? m_sr->errors.load() : -1;
+}
+
+#endif

commit e412b14afee24cf92a8f8aaf4fd2d0ac6fb9830e

Fix bug in hash_binary_file
Introduced in #577 (213d9883).

src/hashutil.cpp
@@ -361,7 +361,7 @@ hash_binary_file(const Context& ctx, struct hash* hash, const char* path)
   Digest digest;
   if (!ctx.inode_cache.get(path, InodeCache::ContentType::binary, digest)) {
     struct hash* file_hash = hash_init();
-    if (!hash_file(hash, path)) {
+    if (!hash_file(file_hash, path)) {
       return false;
     }
     digest = hash_result(file_hash);