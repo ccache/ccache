ccache/ccache

commit f2341b2ba3369d5641a53deb3f6b1d513a8924e2

Switch to CMake (#573)
Co-authored-by: Cristian Adam <cristian.adam@gmail.com>
Co-authored-by: Joel Rosdahl <joel@rosdahl.net>

CMakeLists.txt
@@ -1,0 +1,141 @@
+# Building upon same version as llvm. Let's assume they did their homework and
+# it's a good version. Compare:
+# ~~~
+# https://repology.org/badge/vertical-allrepos/cmake.svg?minversion=3.4.3
+# ~~~
+cmake_minimum_required(VERSION 3.4.3)
+
+project(ccache LANGUAGES C CXX)
+set(CMAKE_PROJECT_DESCRIPTION "a fast C/C++ compiler cache")
+
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD_REQUIRED YES)
+set(CMAKE_CXX_EXTENSIONS NO)
+
+set(CMAKE_C_STANDARD 99)
+set(CMAKE_C_STANDARD_REQUIRED YES)
+set(CMAKE_C_EXTENSIONS NO)
+
+# Always export compile_commands.json as it's useful for so many tools.
+set(CMAKE_EXPORT_COMPILE_COMMANDS TRUE)
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake")
+
+#
+# Settings
+#
+include(StandardSettings)
+include(StandardWarnings)
+include(DefaultBuildType)
+
+#
+# Configuration
+#
+include(GenerateConfigurationFile)
+include(GenerateVersionFile)
+
+#
+# 3rd party
+#
+option(USE_LIBZSTD_FROM_INTERNET "Download and use libzstd from the Internet"
+       OFF)
+find_package(zstd 1.1.2 REQUIRED)
+
+option(USE_LIBB2_FROM_INTERNET "Download and use libb2 from the Internet" OFF)
+find_package(libb2 0.98 REQUIRED)
+
+#
+# special flags
+#
+# Note: cppcheck will scan everything after this point. Zstd and libb2 are above
+# so they don't get scanned.
+#
+include(CodeAnalysis)
+option(ENABLE_TRACING "Enable possibility to use internal ccache tracing" OFF)
+
+#
+# ccache_lib
+#
+add_subdirectory(src)
+
+#
+# ccache
+#
+add_executable(ccache src/main.cpp)
+target_link_libraries(ccache PRIVATE standard_settings standard_warnings
+                                     ccache_lib)
+
+add_subdirectory(doc)
+
+#
+# installation
+#
+include(GNUInstallDirs)
+install(TARGETS ccache DESTINATION ${CMAKE_INSTALL_BINDIR})
+
+# before adding documentation to package run misc/update_authors.sh
+#
+# install(TARGETS documentation DESTINATION ${CMAKE_INSTALL_DOCDIR})
+
+#
+# packaging
+#
+include(CCachePackConfig)
+
+#
+# test and unittest
+#
+option(ENABLE_TESTING "Enable Tests" ON)
+if(ENABLE_TESTING)
+  enable_testing()
+  add_subdirectory(unittest)
+  add_subdirectory(test)
+
+  # Note: VERSION_GREATER_EQUAL requires cmake 3.17
+  if(NOT ${CMAKE_VERSION} VERSION_LESS "3.17")
+    list(APPEND CMAKE_CTEST_ARGUMENTS "--output-on-failure")
+  endif()
+
+  # Add 'check' target which will compile & run tests
+  if(CMAKE_CONFIGURATION_TYPES)
+    add_custom_target(
+      check
+      COMMAND ${CMAKE_CTEST_COMMAND} --force-new-ctest-process
+              --output-on-failure --build-config "$<CONFIGURATION>"
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+      DEPENDS ccache unittest)
+  else()
+    add_custom_target(
+      check
+      COMMAND ${CMAKE_CTEST_COMMAND} --force-new-ctest-process
+              --output-on-failure
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+      DEPENDS ccache unittest)
+  endif()
+endif()
+
+#
+# Special formatting targets. You can also run the scripts without cmake/make!
+#
+
+find_program(CLANG_FORMAT_EXE NAMES "clang-format"
+             DOC "Path to clang-format executable")
+mark_as_advanced(CLANG_FORMAT_EXE) # don't show in ccmake
+
+if(NOT CLANG_FORMAT_EXE)
+  message(WARNING "clang-format not found.")
+else()
+  add_custom_target(
+    format
+    COMMAND misc/format.sh
+    COMMENT "Formatting code"
+    USES_TERMINAL
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+
+  add_custom_target(
+    check_format
+    COMMAND misc/check_format.sh
+    COMMENT "Checking correctly formatted code"
+    USES_TERMINAL
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+endif()

cmake/config.h.in
@@ -1,0 +1,142 @@
+#pragma once
+#ifdef __clang__
+#  pragma clang diagnostic push
+#  if __has_warning("-Wreserved-id-macro")
+#    pragma clang diagnostic ignored "-Wreserved-id-macro"
+#  endif
+#endif
+
+// For example for vasprintf under i686-w64-mingw32-g++-posix.
+// The later defininition of _XOPEN_SOURCE disables certain features
+// on Linux, so we need _GNU_SOURCE to re-enable them (makedev, tm_zone).
+#define _GNU_SOURCE 1
+
+// The later defininition of _XOPEN_SOURCE and _POSIX_C_SOURCE disables
+// certain features on NetBSD, so we need _NETBSD_SOURCE to re-enable
+// them.
+#define _NETBSD_SOURCE 1
+
+// The later defininition of _XOPEN_SOURCE and _POSIX_C_SOURCE disables
+// certain features on FreeBSD, so we need __BSD_VISIBLE to re-enable
+// them.
+#define __BSD_VISIBLE 1
+
+// The later defininition of _XOPEN_SOURCE and _POSIX_C_SOURCE disables
+// u_int on Irix 5.3. Defining _BSD_TYPES brings it back.
+#define _BSD_TYPES 1
+
+// The later defininition of _XOPEN_SOURCE and _POSIX_C_SOURCE disables
+// certain features on Mac OS X, so we need _DARWIN_C_SOURCE to re-enable
+// them.
+#cmakedefine _DARWIN_C_SOURCE
+
+// Define to activate features from IEEE Stds 1003.1-2001.
+#define _POSIX_C_SOURCE 200809L
+
+#if defined(__SunOS_5_8) || defined(__SunOS_5_9) || defined(__SunOS_5_10)
+  #define _XOPEN_SOURCE 500
+#elif !defined(__SunOS_5_11) && !defined(__APPLE__)
+  #define _XOPEN_SOURCE
+#endif
+
+#if defined(__SunOS_5_10) || defined(__SunOS_5_11)
+  #define __EXTENSIONS__ 1
+#else
+  #define _XOPEN_SOURCE_EXTENDED
+#endif
+
+
+#cmakedefine _WIN32_WINNT @_WIN32_WINNT@
+
+#ifdef __clang__
+#  pragma clang diagnostic pop
+#endif
+
+#cmakedefine MTR_ENABLED
+
+/* Define to 1 if you have the `asctime_r' function. */
+#cmakedefine HAVE_ASCTIME_R
+
+/* Define to 1 if your compiler supports AVX2. */
+#cmakedefine HAVE_AVX2
+
+/* Define to 1 if you have the `geteuid' function. */
+#cmakedefine HAVE_GETEUID
+
+/* Define to 1 if you have the `GetFinalPathNameByHandleW' function. */
+#cmakedefine HAVE_GETFINALPATHNAMEBYHANDLEW
+
+/* Define to 1 if you have the `getopt_long' function. */
+#cmakedefine HAVE_GETOPT_LONG
+
+/* Define to 1 if you have the `getpwuid' function. */
+#cmakedefine HAVE_GETPWUID
+
+/* Define to 1 if you have the `gettimeofday' function. */
+#cmakedefine HAVE_GETTIMEOFDAY
+
+/* Define to 1 if you have the <linux/fs.h> header file. */
+#cmakedefine HAVE_LINUX_FS_H
+
+/* Define to 1 if you have the `localtime_r' function. */
+#cmakedefine HAVE_LOCALTIME_R
+
+/* Define to 1 if the system has the type `long long'. */
+#cmakedefine HAVE_LONG_LONG
+
+/* Define to 1 if you have the `mkstemp' function. */
+#cmakedefine HAVE_MKSTEMP
+
+/* Define to 1 if you have the `posix_fallocate. */
+#cmakedefine HAVE_POSIX_FALLOCATE
+
+/* Define to 1 if you have the <pwd.h> header file. */
+#cmakedefine HAVE_PWD_H
+
+/* Define to 1 if you have the `realpath' function. */
+#cmakedefine HAVE_REALPATH
+
+/* Define to 1 if you have the `setenv' function. */
+#cmakedefine HAVE_SETENV
+
+/* Define to 1 if you have the `strndup' function. */
+#cmakedefine HAVE_STRNDUP
+
+/* Define to 1 if `f_fstypename' is a member of `struct statfs'. */
+#cmakedefine HAVE_STRUCT_STATFS_F_FSTYPENAME
+
+/* Define to 1 if `st_ctim' is a member of `struct stat'. */
+#cmakedefine HAVE_STRUCT_STAT_ST_CTIM
+
+/* Define to 1 if `st_mtim' is a member of `struct stat'. */
+#cmakedefine HAVE_STRUCT_STAT_ST_MTIM
+
+/* Define to 1 if you have the `syslog' function. */
+#cmakedefine HAVE_SYSLOG
+
+/* Define to 1 if you have the <syslog.h> header file. */
+#cmakedefine HAVE_SYSLOG_H
+
+/* Define to 1 if you have the <sys/clonefile.h> header file. */
+#cmakedefine HAVE_SYS_CLONEFILE_H
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#cmakedefine HAVE_SYS_IOCTL_H
+
+/* Define to 1 if you have the <sys/mman.h> header file. */
+#cmakedefine HAVE_SYS_MMAN_H
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#cmakedefine HAVE_SYS_TIME_H
+
+/* Define to 1 if you have <sys/wait.h> that is POSIX.1 compatible. */
+#cmakedefine HAVE_SYS_WAIT_H
+
+/* Define to 1 if you have the <termios.h> header file. */
+#cmakedefine HAVE_TERMIOS_H
+
+/* Define to 1 if you have the `unsetenv' function. */
+#cmakedefine HAVE_UNSETENV
+
+/* Define to 1 if you have the `utimes' function. */
+#cmakedefine HAVE_UTIMES

doc/CMakeLists.txt
@@ -1,0 +1,57 @@
+
+find_program (ASCIIDOC_EXE asciidoc)
+mark_as_advanced(ASCIIDOC_EXE) # don't show in ccmake
+if (NOT ASCIIDOC_EXE)
+    message (WARNING "Could not find asciidoc: documentation & manpage will not be generated")
+else()
+  #
+  # documentation
+  #
+  add_custom_target(documentation)
+
+  function(generate_html in out)
+    add_custom_command (
+      TARGET documentation
+      COMMAND ${ASCIIDOC_EXE} -o "${CMAKE_BINARY_DIR}/${out}" -a revnumber="${VERSION}" -a toc -b xhtml11 "${CMAKE_SOURCE_DIR}/${in}"
+      BYPRODUCTS "${out}"
+    )
+  endfunction()
+
+  add_custom_command (
+    TARGET documentation
+    COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_BINARY_DIR}/doc"
+    COMMAND misc/update_authors.sh
+    BYPRODUCTS "doc"
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  )
+
+  generate_html(LICENSE.adoc LICENSE.html)
+  generate_html(doc/AUTHORS.adoc doc/AUTHORS.html)
+  generate_html(doc/MANUAL.adoc doc/MANUAL.html)
+  generate_html(doc/NEWS.adoc doc/NEWS.html)
+
+  #
+  # manpage
+  #
+
+  find_program (A2X_EXE a2x)
+  mark_as_advanced(A2X_EXE) # don't show in ccmake
+  if (NOT A2X_EXE)
+      message (WARNING "Could not find a2x: manpage will not be generated")
+  else()
+    add_custom_target(manpage)
+
+    # MANUAL.adoc -> MANUAL.xml -> MANUAL manpage
+    add_custom_command (
+            TARGET manpage
+            COMMAND ${ASCIIDOC_EXE} -o - -a revnumber=${VERSION} -d manpage -b docbook "${CMAKE_SOURCE_DIR}/doc/MANUAL.adoc" | perl -pe 's!<literal>\(.*?\)</literal>!<emphasis role="strong">\\1</emphasis>!g' > "${CMAKE_BINARY_DIR}/MANUAL.xml"
+            BYPRODUCTS "${CMAKE_BINARY_DIR}/MANUAL.xml"
+        )
+
+        add_custom_command (
+            TARGET manpage
+            COMMAND a2x --doctype manpage --format manpage "${CMAKE_BINARY_DIR}/MANUAL.xml"
+            BYPRODUCTS ccache.1
+        )
+  endif()
+endif()

src/CMakeLists.txt
@@ -1,0 +1,62 @@
+add_library(
+  ccache_lib STATIC
+  Args.cpp
+  AtomicFile.cpp
+  CacheEntryReader.cpp
+  CacheEntryWriter.cpp
+  CacheFile.cpp
+  Compression.cpp
+  Compressor.cpp
+  Config.cpp
+  Context.cpp
+  Counters.cpp
+  Decompressor.cpp
+  InodeCache.cpp
+  Lockfile.cpp
+  MiniTrace.cpp
+  NullCompressor.cpp
+  NullDecompressor.cpp
+  ProgressBar.cpp
+  SignalHandler.cpp
+  Stat.cpp
+  ThreadPool.cpp
+  Util.cpp
+  ZstdCompressor.cpp
+  ZstdDecompressor.cpp
+  argprocessing.cpp
+  ccache.cpp
+  cleanup.cpp
+  compopt.cpp
+  compress.cpp
+  execute.cpp
+  hash.cpp
+  hashutil.cpp
+  language.cpp
+  legacy_util.cpp
+  logging.cpp
+  manifest.cpp
+  result.cpp
+  stats.cpp
+  win32compat.cpp
+  Version.cpp)
+
+if(WIN32)
+  target_link_libraries(ccache_lib PRIVATE ws2_32 "psapi")
+
+  if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+    target_link_libraries(ccache_lib PRIVATE -static gcc stdc++ winpthread
+                                             -dynamic)
+  else()
+    target_link_libraries(ccache_lib PRIVATE -static c++ -dynamic)
+  endif()
+endif()
+
+find_package(Threads REQUIRED)
+target_link_libraries(
+  ccache_lib PRIVATE standard_settings standard_warnings ZSTD::ZSTD
+                     libb2::libb2 Threads::Threads third_party_lib)
+
+# There is currently no separate interface directory.
+target_include_directories(ccache_lib PRIVATE ${CMAKE_BINARY_DIR} .)
+
+add_subdirectory(third_party)

src/third_party/CMakeLists.txt
@@ -1,0 +1,30 @@
+add_library(third_party_lib STATIC format.cpp getopt_long.c xxhash.c)
+if(ENABLE_TRACING)
+  target_sources(third_party_lib PRIVATE minitrace.c)
+endif()
+
+# Treat third party headers as system files. (No warning from those headers)
+target_include_directories(third_party_lib PRIVATE ${CMAKE_BINARY_DIR} . SYSTEM
+                           INTERFACE .)
+
+target_link_libraries(third_party_lib PRIVATE standard_settings)
+
+# third_party (for now) has way fewer compiler warnings enabled than the rest of
+# the code.
+
+# These warnings are enabled by default even without e.g. -Wall. But (for now)
+# we don't want even them in third_party.
+target_compile_options(
+  third_party_lib
+  PRIVATE $<$<COMPILE_LANGUAGE:C>:-Wno-implicit-function-declaration
+          -Wno-int-conversion>)
+if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
+  target_compile_options(third_party_lib
+                         PRIVATE $<$<COMPILE_LANGUAGE:C>:-Wno-attributes>)
+endif()
+
+# The headers are included from the rest of the project, so turn off warnings as
+# requried (for now).
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  target_compile_options(third_party_lib INTERFACE -Wno-shadow)
+endif()

test/CMakeLists.txt
@@ -1,0 +1,39 @@
+function(addtest name)
+  add_test(NAME "producttest.${name}"
+    COMMAND test/run ${name}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+  set_tests_properties("producttest.${name}" PROPERTIES ENVIRONMENT CCACHE=${CMAKE_BINARY_DIR}/ccache)
+endfunction()
+
+addtest(base)
+addtest(nocpp2)
+addtest(cpp1)
+addtest(multi_arch)
+addtest(serialize_diagnostics)
+addtest(color_diagnostics)
+addtest(sanitize_blacklist)
+addtest(debug_prefix_map)
+addtest(profiling)
+addtest(profiling_gcc)
+addtest(profiling_clang)
+addtest(split_dwarf)
+addtest(masquerading)
+addtest(hardlink)
+addtest(fileclone)
+addtest(direct)
+addtest(direct_gcc)
+addtest(depend)
+addtest(basedir)
+addtest(no_compression)
+addtest(readonly)
+addtest(readonly_direct)
+addtest(cleanup)
+addtest(pch)
+addtest(modules)
+addtest(upgrade)
+addtest(input_charset)
+addtest(nvcc)
+addtest(nvcc_direct)
+addtest(nvcc_ldir)
+addtest(nvcc_nocpp2)
+addtest(inode_cache)

unittest/CMakeLists.txt
@@ -1,0 +1,30 @@
+add_executable(
+  unittest
+  TestUtil.cpp
+  catch2_tests.cpp
+  main.cpp
+  test_Args.cpp
+  test_AtomicFile.cpp
+  test_Checksum.cpp
+  test_Compression.cpp
+  test_Config.cpp
+  test_FormatNonstdStringView.cpp
+  test_InodeCache.cpp
+  test_Lockfile.cpp
+  test_NullCompression.cpp
+  test_Stat.cpp
+  test_Util.cpp
+  test_ZstdCompression.cpp
+  test_argprocessing.cpp
+  test_compopt.cpp
+  test_hash.cpp
+  test_hashutil.cpp
+  test_legacy_util.cpp)
+
+target_link_libraries(unittest PRIVATE standard_settings standard_warnings
+                                       ccache_lib)
+
+target_include_directories(unittest PRIVATE ${CMAKE_BINARY_DIR} . ../src)
+
+# add to ctest
+add_test(NAME unittest COMMAND unittest)

.cmake-format
@@ -1,0 +1,21 @@
+# This is for cmake-format.
+# See https://github.com/cheshirekow/cmake_format
+# for GUI integration and more details.
+
+# Installation: pip3 install cmake-format
+
+# cmake-format
+# Execution: cmake-format CMakeLists.txt cmake/*.cmake src/CMakeLists.txt unittest/CMakeLists.txt -i
+bullet_char: '*'
+dangle_parens: false
+enum_char: .
+line_ending: unix
+line_width: 80
+separate_ctrl_name_with_space: false
+separate_fn_name_with_space: false
+tab_size: 2
+max_subgroups_hwrap: 3 # default is 2 which prevents trivial set(key value PARENT_SCOPE)
+
+# cmake-lint
+# Execution: cmake-lint CMakeLists.txt cmake/*.cmake src/CMakeLists.txt unittest/CMakeLists.txt
+max_statement_spacing: 2

commit b968312a122e6fcfc34f0602820eecb1c962bf04

Fix location of secondary configuration
Regression in #573 (f2341b2b).

CMakeLists.txt
@@ -26,6 +26,7 @@ include(DefaultBuildType)
 #
 # Configuration
 #
+include(GNUInstallDirs)
 include(GenerateConfigurationFile)
 include(GenerateVersionFile)
 
@@ -63,7 +64,6 @@ add_subdirectory(doc)
 #
 # Installation
 #
-include(GNUInstallDirs)
 install(TARGETS ccache DESTINATION ${CMAKE_INSTALL_BINDIR})
 
 #

cmake/config.h.in
@@ -46,6 +46,8 @@
 #cmakedefine _WIN32_WINNT @_WIN32_WINNT@
 // clang-format on
 
+#define SYSCONFDIR "@CMAKE_INSTALL_FULL_SYSCONFDIR@"
+
 #ifdef __clang__
 #  pragma clang diagnostic pop
 #endif

src/ccache.cpp
@@ -56,9 +56,6 @@
 
 #include <limits>
 
-#define STRINGIFY(x) #x
-#define TO_STRING(x) STRINGIFY(x)
-
 using nonstd::nullopt;
 using nonstd::optional;
 using nonstd::string_view;
@@ -1859,8 +1856,7 @@ set_up_config(Config& config)
   if (p) {
     config.set_primary_config_path(p);
   } else {
-    config.set_secondary_config_path(
-      fmt::format("{}/ccache.conf", TO_STRING(SYSCONFDIR)));
+    config.set_secondary_config_path(fmt::format("{}/ccache.conf", SYSCONFDIR));
     MTR_BEGIN("config", "conf_read_secondary");
     // A missing config file in SYSCONFDIR is OK so don't check return value.
     config.update_from_file(config.secondary_config_path());