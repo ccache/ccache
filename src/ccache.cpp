21480ca438456ff7010a309f24ffdcd12aec83f3
Joel Rosdahl <joel@rosdahl.net>
2020-07-28 20:06:26 +0200

    Tweak function comment

--- src/ccache.cpp
+++ src/ccache.cpp
@@ -1372,7 +1372,7 @@ option_should_be_ignored(const std::string& arg,
 
 // Update a hash sum with information specific to the direct and preprocessor
 // modes and calculate the result name. Returns the result name on success,
-// otherwise NULL.
+// otherwise nullopt.
 static optional<Digest>
 calculate_result_name(Context& ctx,
                       const Args& args,
