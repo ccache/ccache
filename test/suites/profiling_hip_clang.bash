4282ce74b11f965428a880f43715ed6b24cce776
Paul <pfultz2@yahoo.com>
2020-09-21 12:08:43 -0500

    Update tests

--- test/suites/profiling_hip_clang.bash
+++ test/suites/profiling_hip_clang.bash
@@ -1,25 +1,55 @@
-SUITE_profiling_clang_PROBE() {
+SUITE_profiling_hip_clang_PROBE() {
     if ! $COMPILER_TYPE_CLANG; then
         echo "compiler is not Clang"
-    elif ! $COMPILER -x hip --cuda-arch=gfx900 -c - > /dev/null; then
+    elif ! echo | $COMPILER -x hip --cuda-gpu-arch=gfx900 -nogpulib -c - > /dev/null; then
         echo "Hip not supported"
     fi
 }
 
-SUITE_profiling_clang_SETUP() {
-    echo 'int main(void) { return 0; }' >test.c
+SUITE_profiling_hip_clang_SETUP() {
+    echo 'int main(void) { return 0; }' >test1.hip
+    echo 'int main(void) { int x = 0+0; return 0; }' >test2.hip
     unset CCACHE_NODIRECT
 }
 
-SUITE_profiling_clang() {
+SUITE_profiling_hip_clang() {
     # -------------------------------------------------------------------------
     TEST "hip-clang"
 
-    $CCACHE_COMPILE -x hip --cuda-arch=gfx900 -c test.c
+    hip_opts="-x hip --cuda-gpu-arch=gfx900 -nogpulib"
+
+    $CCACHE_COMPILE $hip_opts -c test1.hip
+    expect_stat 'cache hit (preprocessed)' 0
     expect_stat 'cache hit (direct)' 0
     expect_stat 'cache miss' 1
 
-    $CCACHE_COMPILE -x hip --cuda-arch=gfx900 -c test.c
+    $CCACHE_COMPILE $hip_opts -c test1.hip
+    expect_stat 'cache hit (preprocessed)' 0
     expect_stat 'cache hit (direct)' 1
     expect_stat 'cache miss' 1
+
+    $CCACHE_COMPILE $hip_opts --cuda-gpu-arch=gfx906 -c test1.hip
+    expect_stat 'cache hit (preprocessed)' 0
+    expect_stat 'cache hit (direct)' 1
+    expect_stat 'cache miss' 2
+
+    $CCACHE_COMPILE $hip_opts -c test2.hip
+    expect_stat 'cache hit (preprocessed)' 0
+    expect_stat 'cache hit (direct)' 1
+    expect_stat 'cache miss' 3
+
+    $CCACHE_COMPILE $hip_opts -c test2.hip
+    expect_stat 'cache hit (preprocessed)' 0
+    expect_stat 'cache hit (direct)' 2
+    expect_stat 'cache miss' 3
+
+    $CCACHE_COMPILE $hip_opts -Dx=x -c test2.hip
+    expect_stat 'cache hit (preprocessed)' 1
+    expect_stat 'cache hit (direct)' 2
+    expect_stat 'cache miss' 3
+
+    $CCACHE_COMPILE $hip_opts -Dx=y -c test2.hip
+    expect_stat 'cache hit (preprocessed)' 1
+    expect_stat 'cache hit (direct)' 2
+    expect_stat 'cache miss' 4
 }
