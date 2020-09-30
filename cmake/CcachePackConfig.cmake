a5a5804abb2b9b2c15039403fbbab80b6883f275
Joel Rosdahl <joel@rosdahl.net>
2020-09-30 22:18:09 +0200

    Simplify source package generation
    Since it now works to build from “git archive” archives, use such an
    archive as the official source code release archive instead of using
    CPack with a custom install script for modifying the source code. We can
    revisit this in the future when and if we want a source code release
    archive that is not simply an export of the version-controlled source
    code.

--- cmake/CcachePackConfig.cmake
+++ cmake/CcachePackConfig.cmake
@@ -5,7 +5,7 @@ if(${CMAKE_VERSION} VERSION_LESS "3.9")
   set(CPACK_PACKAGE_DESCRIPTION "${CMAKE_PROJECT_DESCRIPTION}")
 endif()
 
-# From GenerateVersionFile.cmake.
+# From CcacheVersion.cmake.
 set(CPACK_PACKAGE_VERSION ${VERSION})
 
 set(CPACK_VERBATIM_VARIABLES ON)
@@ -16,28 +16,9 @@ else()
   set(CPACK_GENERATOR "TXZ")
 endif()
 
-set(CPACK_SOURCE_GENERATOR "TGZ;TXZ")
-
-list(APPEND CPACK_SOURCE_IGNORE_FILES "^${CMAKE_SOURCE_DIR}/\\.git")
-list(APPEND CPACK_SOURCE_IGNORE_FILES "^${CMAKE_SOURCE_DIR}/build")
-list(APPEND CPACK_SOURCE_IGNORE_FILES "^${CMAKE_BINARY_DIR}")
-
 set(
   CPACK_PACKAGE_FILE_NAME
   "ccache-${VERSION}-${CMAKE_HOST_SYSTEM_NAME}-${CMAKE_HOST_SYSTEM_PROCESSOR}"
 )
-set(CPACK_SOURCE_PACKAGE_FILE_NAME "ccache-${VERSION}")
-
-configure_file(
-  ${CMAKE_SOURCE_DIR}/cmake/PreparePackage.cmake.in
-  ${CMAKE_BINARY_DIR}/PreparePackage.cmake
-  @ONLY
-)
-
-if(${CMAKE_VERSION} VERSION_LESS "3.16")
-  set(CPACK_INSTALL_SCRIPT ${CMAKE_BINARY_DIR}/PreparePackage.cmake)
-else()
-  set(CPACK_INSTALL_SCRIPTS ${CMAKE_BINARY_DIR}/PreparePackage.cmake)
-endif()
 
 include(CPack)
--- cmake/GenerateVersionFile.cmake
+++ cmake/GenerateVersionFile.cmake
@@ -1,15 +1,6 @@
-set(version_file ${CMAKE_SOURCE_DIR}/VERSION)
-
-if(EXISTS ${version_file})
-  file(READ ${version_file} VERSION)
-  string(STRIP ${VERSION} VERSION)
-else()
-  include(CcacheVersion)
-endif()
-
+include(CcacheVersion)
 configure_file(
   ${CMAKE_SOURCE_DIR}/cmake/version.cpp.in
   ${CMAKE_BINARY_DIR}/src/version.cpp
   @ONLY)
-
 message(STATUS "Ccache version: ${VERSION}")
