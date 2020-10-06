ccache/ccache

commit aaadd43dbdf157ce20ab901fc9ce1c2e4924a73c

Adapt *_depfile() functions to use split_into_views()/_strings()

src/ccache.cpp
@@ -861,90 +861,74 @@ process_preprocessed_file(Context& ctx,
 static void
 use_relative_paths_in_depfile(const Context& ctx)
 {
-  const char* depfile = ctx.args_info.output_dep.c_str();
-
   if (ctx.config.base_dir().empty()) {
     cc_log("Base dir not set, skip using relative paths");
     return; // nothing to do
   }
   if (!ctx.has_absolute_include_headers) {
     cc_log(
-      "No absolute path for included files found, skip using relative"
-      " paths");
+      "No absolute path for included files found, skip using relative paths");
     return; // nothing to do
   }
 
-  FILE* f;
-  f = fopen(depfile, "r");
-  if (!f) {
-    cc_log("Cannot open dependency file: %s (%s)", depfile, strerror(errno));
+  const std::string& output_dep = ctx.args_info.output_dep;
+  std::string file_content;
+  try {
+    file_content = Util::read_file(output_dep);
+  } catch (const Error& e) {
+    cc_log("Cannot open dependency file %s: %s", output_dep.c_str(), e.what());
     return;
   }
 
-  char* tmp_file = format("%s.tmp", depfile);
-  FILE* tmpf = create_tmp_file(&tmp_file, "w");
+  std::string adjusted_file_content;
+  adjusted_file_content.reserve(file_content.size());
 
-  bool result = false;
-  char buf[10000];
-  while (fgets(buf, sizeof(buf), f) && !ferror(tmpf)) {
-    char* saveptr;
-    char* token = strtok_r(buf, " \t", &saveptr);
-    while (token) {
-      char* relpath = nullptr;
-      if (Util::is_absolute_path(token)
-          && str_startswith(token, ctx.config.base_dir().c_str())) {
-        relpath = x_strdup(make_relative_path(ctx, token).c_str());
-        result = true;
-      } else {
-        relpath = token;
-      }
-      if (token != buf) { // This is a dependency file.
-        fputc(' ', tmpf);
-      }
-      fputs(relpath, tmpf);
-      if (relpath != token) {
-        free(relpath);
-      }
-      token = strtok_r(nullptr, " \t", &saveptr);
-    }
-  }
+  bool rewritten = false;
 
-  if (ferror(f)) {
-    cc_log("Error reading dependency file: %s, skip relative path usage",
-           depfile);
-    result = false;
-    goto out;
-  }
-  if (ferror(tmpf)) {
-    cc_log(
-      "Error writing temporary dependency file: %s, skip relative path"
-      " usage",
-      tmp_file);
-    result = false;
-    goto out;
-  }
-
-out:
-  fclose(tmpf);
-  fclose(f);
-  if (result) {
-    if (x_rename(tmp_file, depfile) != 0) {
-      cc_log(
-        "Error renaming dependency file: %s -> %s (%s), skip relative"
-        " path usage",
-        tmp_file,
-        depfile,
-        strerror(errno));
-      result = false;
+  for (string_view token : Util::split_into_views(file_content, " \t\r\n")) {
+    if (Util::is_absolute_path(token)
+        && token.starts_with(ctx.config.base_dir())) {
+      adjusted_file_content.append(make_relative_path(ctx, token));
+      rewritten = true;
     } else {
-      cc_log("Renamed dependency file: %s -> %s", tmp_file, depfile);
+      adjusted_file_content.append(token.begin(), token.end());
     }
+    adjusted_file_content.push_back(' ');
   }
-  if (!result) {
-    cc_log("Removing temporary dependency file: %s", tmp_file);
-    tmp_unlink(tmp_file);
+
+  if (!rewritten) {
+    cc_log(
+      "No paths in dependency file %s made relative, skip relative path usage",
+      output_dep.c_str());
+    return;
   }
-  free(tmp_file);
+
+  std::string tmp_file = fmt::format("{}.tmp{}", output_dep, tmp_string());
+
+  try {
+    Util::write_file(tmp_file, adjusted_file_content);
+  } catch (const Error& e) {
+    cc_log(
+      "Error writing temporary dependency file %s (%s), skip relative path"
+      " usage",
+      tmp_file.c_str(),
+      e.what());
+    x_unlink(tmp_file.c_str());
+    return;
+  }
+
+  if (x_rename(tmp_file.c_str(), output_dep.c_str()) != 0) {
+    cc_log(
+      "Error renaming dependency file: %s -> %s (%s), skip relative path usage",
+      tmp_file.c_str(),
+      output_dep.c_str(),
+      strerror(errno));
+    x_unlink(tmp_file.c_str());
+  } else {
+    cc_log("Renamed dependency file: %s -> %s",
+           tmp_file.c_str(),
+           output_dep.c_str());
+  }
 }
 
 // Extract the used includes from the dependency file. Note that we cannot
@@ -952,32 +936,26 @@ out:
 static struct digest*
 result_name_from_depfile(Context& ctx, struct hash* hash)
 {
-  FILE* f = fopen(ctx.args_info.output_dep.c_str(), "r");
-  if (!f) {
+  std::string file_content;
+  try {
+    file_content = Util::read_file(ctx.args_info.output_dep);
+  } catch (const Error& e) {
     cc_log("Cannot open dependency file %s: %s",
            ctx.args_info.output_dep.c_str(),
-           strerror(errno));
+           e.what());
     return nullptr;
   }
 
-  char buf[10000];
-  while (fgets(buf, sizeof(buf), f) && !ferror(f)) {
-    char* saveptr;
-    char* token;
-    for (token = strtok_r(buf, " \t\n", &saveptr); token;
-         token = strtok_r(nullptr, " \t\n", &saveptr)) {
-      if (str_endswith(token, ":") || str_eq(token, "\\")) {
-        continue;
-      }
-      if (!ctx.has_absolute_include_headers) {
-        ctx.has_absolute_include_headers = Util::is_absolute_path(token);
-      }
-      std::string path = make_relative_path(ctx, token);
-      remember_include_file(ctx, path, hash, false, hash);
+  for (string_view token : Util::split_into_views(file_content, " \t\r\n")) {
+    if (token == "\\" || token.ends_with(":")) {
+      continue;
     }
+    if (!ctx.has_absolute_include_headers) {
+      ctx.has_absolute_include_headers = Util::is_absolute_path(token);
+    }
+    std::string path = make_relative_path(ctx, token);
+    remember_include_file(ctx, path, hash, false, hash);
   }
-
-  fclose(f);
 
   // Explicitly check the .gch/.pch/.pth file as it may not be mentioned in the
   // dependencies output.

commit 8f097e293a2f78d327184a8f94337ea1600b73f8

Fix broken dependency file when using base_dir
If a path in the dependency file matches base_dir, the dependency file
will be completely broken since newlines won’t be not retained by
use_relative_paths_in_depfile. Fix this by tokenizing lines instead of
the full file content once again.

Regression in aaadd43dbdf157ce20ab901fc9ce1c2e4924a73c (#555).

src/ccache.cpp
@@ -608,6 +608,54 @@ process_preprocessed_file(Context& ctx,
   return true;
 }
 
+nonstd::optional<std::string>
+rewrite_dep_file_paths(const Context& ctx, const std::string& file_content)
+{
+  ASSERT(!ctx.config.base_dir().empty());
+  ASSERT(ctx.has_absolute_include_headers);
+
+  // Fast path for the common case:
+  if (file_content.find(ctx.config.base_dir()) == std::string::npos) {
+    return nonstd::nullopt;
+  }
+
+  std::string adjusted_file_content;
+  adjusted_file_content.reserve(file_content.size());
+
+  bool content_rewritten = false;
+  for (const auto& line : Util::split_into_views(file_content, "\n")) {
+    const auto tokens = Util::split_into_views(line, " \t");
+    for (size_t i = 0; i < tokens.size(); ++i) {
+      DEBUG_ASSERT(line.length() > 0); // line.empty() -> no tokens
+      if (i > 0 || line[0] == ' ' || line[0] == '\t') {
+        adjusted_file_content.push_back(' ');
+      }
+
+      const auto& token = tokens[i];
+      bool token_rewritten = false;
+      if (Util::is_absolute_path(token)) {
+        const auto new_path = Util::make_relative_path(ctx, token);
+        if (new_path != token) {
+          adjusted_file_content.append(new_path);
+          token_rewritten = true;
+        }
+      }
+      if (token_rewritten) {
+        content_rewritten = true;
+      } else {
+        adjusted_file_content.append(token.begin(), token.end());
+      }
+    }
+    adjusted_file_content.push_back('\n');
+  }
+
+  if (content_rewritten) {
+    return adjusted_file_content;
+  } else {
+    return nonstd::nullopt;
+  }
+}
+
 // Replace absolute paths with relative paths in the provided dependency file.
 static void
 use_relative_paths_in_depfile(const Context& ctx)
@@ -629,33 +677,12 @@ use_relative_paths_in_depfile(const Context& ctx)
     log("Cannot open dependency file {}: {}", output_dep, e.what());
     return;
   }
-
-  std::string adjusted_file_content;
-  adjusted_file_content.reserve(file_content.size());
-
-  bool rewritten = false;
-
-  for (string_view token : Util::split_into_views(file_content, " \t\r\n")) {
-    if (Util::is_absolute_path(token)
-        && token.starts_with(ctx.config.base_dir())) {
-      adjusted_file_content.append(Util::make_relative_path(ctx, token));
-      rewritten = true;
-    } else {
-      adjusted_file_content.append(token.begin(), token.end());
-    }
-    adjusted_file_content.push_back(' ');
+  const auto new_content = rewrite_dep_file_paths(ctx, file_content);
+  if (new_content) {
+    Util::write_file(output_dep, *new_content);
+  } else {
+    log("No paths in dependency file {} made relative", output_dep);
   }
-
-  if (!rewritten) {
-    log(
-      "No paths in dependency file {} made relative, skip relative path usage",
-      output_dep);
-    return;
-  }
-
-  std::string tmp_file = output_dep + ".tmp";
-  Util::write_file(tmp_file, adjusted_file_content);
-  Util::rename(tmp_file, output_dep);
 }
 
 // Extract the used includes from the dependency file. Note that we cannot

src/ccache.hpp
@@ -21,6 +21,8 @@
 
 #include "system.hpp"
 
+#include "third_party/nonstd/optional.hpp"
+
 #include <functional>
 #include <string>
 
@@ -58,3 +60,5 @@ using FindExecutableFunction =
 // Tested by unit tests.
 void find_compiler(Context& ctx,
                    const FindExecutableFunction& find_executable_function);
+nonstd::optional<std::string>
+rewrite_dep_file_paths(const Context& ctx, const std::string& file_content);

unittest/test_ccache.cpp
@@ -144,4 +144,43 @@ TEST_CASE("find_compiler")
   }
 }
 
+TEST_CASE("rewrite_dep_file_paths")
+{
+  Context ctx;
+
+  const auto cwd = Util::get_actual_cwd();
+  ctx.actual_cwd = cwd;
+  ctx.apparent_cwd = cwd;
+  ctx.has_absolute_include_headers = true;
+
+  const auto content =
+    fmt::format("foo.o: bar.c {0}/bar.h \\\n {1}/fie.h {0}/fum.h\n",
+                cwd,
+                Util::dir_name(cwd));
+
+  SUBCASE("Base directory not in dep file content")
+  {
+    ctx.config.set_base_dir("/foo/bar");
+    CHECK(!rewrite_dep_file_paths(ctx, ""));
+    CHECK(!rewrite_dep_file_paths(ctx, content));
+  }
+
+  SUBCASE("Base directory in dep file content but not matching")
+  {
+    ctx.config.set_base_dir(fmt::format("{}/other", Util::dir_name(cwd)));
+    CHECK(!rewrite_dep_file_paths(ctx, ""));
+    CHECK(!rewrite_dep_file_paths(ctx, content));
+  }
+
+  SUBCASE("Absolute paths under base directory rewritten")
+  {
+    ctx.config.set_base_dir(cwd);
+    const auto actual = rewrite_dep_file_paths(ctx, content);
+    const auto expected = fmt::format(
+      "foo.o: bar.c ./bar.h \\\n {}/fie.h ./fum.h\n", Util::dir_name(cwd));
+    REQUIRE(actual);
+    CHECK(*actual == expected);
+  }
+}
+
 TEST_SUITE_END();