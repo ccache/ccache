f806905267be8b4cc2096bb19eae34db6bd26df0
Chris Burr <christopher.burr@cern.ch>
2020-10-08 21:33:17 +0200

    Apply suggested patch from code review

--- test/suites/profiling_gcc.bash
+++ test/suites/profiling_gcc.bash
@@ -17,7 +17,7 @@ SUITE_profiling_gcc() {
     expect_stat 'cache hit (direct)' 0
     expect_stat 'cache miss' 1
 
-    $COMPILER -fprofile-generate test.o -lgcov -o test
+    $COMPILER -fprofile-generate test.o -o test
 
     ./test
 
@@ -44,7 +44,7 @@ SUITE_profiling_gcc() {
     expect_stat 'cache hit (direct)' 0
     expect_stat 'cache miss' 1
 
-    $COMPILER -fprofile-dir=data -fprofile-generate test.o -lgcov -o test
+    $COMPILER -fprofile-generate test.o -o test
 
     ./test
 
@@ -71,7 +71,7 @@ SUITE_profiling_gcc() {
     expect_stat 'cache hit (direct)' 0
     expect_stat 'cache miss' 1
 
-    $COMPILER -fprofile-generate -fprofile-dir=data test.o -lgcov -o test
+    $COMPILER -fprofile-generate test.o -o test
 
     ./test
 
@@ -98,7 +98,7 @@ SUITE_profiling_gcc() {
     expect_stat 'cache hit (direct)' 0
     expect_stat 'cache miss' 1
 
-    $COMPILER -fprofile-dir=data2 -fprofile-generate=data test.o -lgcov -o test
+    $COMPILER -fprofile-generate test.o -o test
 
     ./test
 
