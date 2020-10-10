f7d76882366325e8044624151705088213d6493b
Joel Rosdahl <joel@rosdahl.net>
2020-10-10 19:33:25 +0200

    test: Remove now unnecessary setting of CCACHE_COMPILE

--- test/suites/masquerading.bash
+++ test/suites/masquerading.bash
@@ -12,7 +12,6 @@ SUITE_masquerading_PROBE() {
 
 SUITE_masquerading_SETUP() {
     ln -s "$CCACHE" $COMPILER_BIN
-    CCACHE_COMPILE="./$COMPILER_BIN $COMPILER_ARGS"
     generate_code 1 test1.c
 }
 
@@ -22,13 +21,13 @@ SUITE_masquerading() {
 
     $REAL_COMPILER -c -o reference_test1.o test1.c
 
-    $CCACHE_COMPILE -c test1.c
+    ./$COMPILER_BIN $COMPILER_ARGS -c test1.c
     expect_stat 'cache hit (preprocessed)' 0
     expect_stat 'cache miss' 1
     expect_stat 'files in cache' 1
     expect_equal_object_files reference_test1.o test1.o
 
-    $CCACHE_COMPILE -c test1.c
+    ./$COMPILER_BIN $COMPILER_ARGS -c test1.c
     expect_stat 'cache hit (preprocessed)' 1
     expect_stat 'cache miss' 1
     expect_stat 'files in cache' 1
