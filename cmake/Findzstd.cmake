0edac1174e91f828e9ca3fb688235373229bac04
Ka Ho Ng <khng300@gmail.com>
2020-12-21 00:13:09 +0800

    Add option to link statically with zstd from system
    ZSTD_SYSTEM_STATIC is added as an CMake option to let libzstd from
    system being linked statically.

--- cmake/Findzstd.cmake
+++ cmake/Findzstd.cmake
@@ -41,7 +41,13 @@ if(ZSTD_FROM_INTERNET)
 
   set(zstd_FOUND TRUE)
 else()
-  find_library(ZSTD_LIBRARY zstd)
+  set(zstd_library_name zstd)
+  if (ZSTD_SYSTEM_STATIC)
+    string(CONCAT zstd_library_name
+      ${CMAKE_STATIC_LIBRARY_PREFIX} zstd ${CMAKE_STATIC_LIBRARY_SUFFIX})
+  endif()
+  message(STATUS ${zstd_library_name})
+  find_library(ZSTD_LIBRARY ${zstd_library_name})
   find_path(ZSTD_INCLUDE_DIR zstd.h)
 
   include(FindPackageHandleStandardArgs)
