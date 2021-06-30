7ed608d7f23af46cfe27b50e3def91886394b150
Anders F Björklund <anders.f.bjorklund@gmail.com>
2021-06-30 20:02:33 +0200

    Clean up the test case and add timeout
    Making it more like the secondary_file

--- test/suites/secondary_redis.bash
+++ test/suites/secondary_redis.bash
@@ -13,51 +13,79 @@ SUITE_secondary_redis_SETUP() {
     unset CCACHE_NODIRECT
     export CCACHE_SECONDARY_STORAGE="redis://localhost:7777"
 
-    redis-server --bind localhost --port 7777 &
-    sleep 1 # wait for boot
-    redis-cli -p 7777 ping
-    trap "kill $!" EXIT
-
     generate_code 1 test.c
 }
 
+expect_number_of_cache_entries() {
+    local expected=$1
+    local host=$2
+    local port=$3
+    local actual
+
+    actual=$(redis-cli -h $host -p $port keys "ccache:*" | wc -l)
+    if [ "$actual" -ne "$expected" ]; then
+        test_failed_internal "Found $actual (expected $expected) entries in $host:$port"
+    fi
+}
+
 SUITE_secondary_redis() {
+    timeout 10s redis-server --bind localhost --port 7777 &
+    sleep 0.1s # wait for boot
+    redis-cli -p 7777 ping
+    trap "kill $!" EXIT
+
     # -------------------------------------------------------------------------
-    TEST "Redis"
+    TEST "Base case"
 
     $CCACHE_COMPILE -c test.c
     expect_stat 'cache hit (direct)' 0
     expect_stat 'cache miss' 1
     expect_stat 'files in cache' 2
+    expect_number_of_cache_entries 2 localhost 7777 # result + manifest
 
     $CCACHE_COMPILE -c test.c
     expect_stat 'cache hit (direct)' 1
     expect_stat 'cache miss' 1
     expect_stat 'files in cache' 2
+    expect_number_of_cache_entries 2 localhost 7777 # result + manifest
 
     $CCACHE -C >/dev/null
     expect_stat 'files in cache' 0
+    expect_number_of_cache_entries 2 localhost 7777 # result + manifest
 
     $CCACHE_COMPILE -c test.c
     expect_stat 'cache hit (direct)' 2
     expect_stat 'cache miss' 1
     expect_stat 'files in cache' 0
+    expect_number_of_cache_entries 2 localhost 7777 # result + manifest
+
+    redis-cli -p 7777 flushdb
+
+    # -------------------------------------------------------------------------
+    TEST "Read-only"
+
+    $CCACHE_COMPILE -c test.c
+    expect_stat 'cache hit (direct)' 0
+    expect_stat 'cache miss' 1
+    expect_stat 'files in cache' 2
+    expect_number_of_cache_entries 2 localhost 7777 # result + manifest
 
     $CCACHE -C >/dev/null
     expect_stat 'files in cache' 0
+    expect_number_of_cache_entries 2 localhost 7777 # result + manifest
 
     CCACHE_SECONDARY_STORAGE+="|read-only"
 
     $CCACHE_COMPILE -c test.c
-    expect_stat 'cache hit (direct)' 3
+    expect_stat 'cache hit (direct)' 1
     expect_stat 'cache miss' 1
     expect_stat 'files in cache' 0
+    expect_number_of_cache_entries 2 localhost 7777 # result + manifest
 
     echo 'int x;' >> test.c
     $CCACHE_COMPILE -c test.c
-    expect_stat 'cache hit (direct)' 3
+    expect_stat 'cache hit (direct)' 1
     expect_stat 'cache miss' 2
     expect_stat 'files in cache' 2
-
-    redis-cli -p 7777 keys "ccache:*"
+    expect_number_of_cache_entries 2 localhost 7777 # result + manifest
 }
