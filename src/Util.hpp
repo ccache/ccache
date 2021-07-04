40b6642e1f54f8b61a232e1d36b739c43ba6c8e4
Joel Rosdahl <joel@rosdahl.net>
2021-07-04 18:45:14 +0200

    Fix typo in Util::split_into_views doc string

--- src/Util.hpp
+++ src/Util.hpp
@@ -419,7 +419,7 @@ size_change_kibibyte(const Stat& old_stat, const Stat& new_stat)
 }
 
 // Split `input` into words at any of the characters listed in `separators`.
-// These words are a view into `input`; empty words are omitted. `separators2`
+// These words are a view into `input`; empty words are omitted. `separators`
 // must neither be the empty string nor a nullptr.
 std::vector<nonstd::string_view> split_into_views(nonstd::string_view input,
                                                   const char* separators);
