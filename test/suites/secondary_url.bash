456986fc12ac7543e85a6d1b9928d59352b82c31
Gregor Jasny <gregor.jasny@logmein.com>
2021-07-05 22:08:46 +0200

    Use URL class for URL parsing

---
+++ test/suites/secondary_url.bash
@@ -0,0 +1,33 @@
+SUITE_secondary_url_SETUP() {
+    generate_code 1 test.c
+}
+
+SUITE_secondary_url() {
+    # -------------------------------------------------------------------------
+    TEST "Reject empty url (without config attributes)"
+
+    export CCACHE_SECONDARY_STORAGE="|"
+    $CCACHE_COMPILE -c test.c 2>stderr.log
+    expect_contains stderr.log "must provide a URL"
+
+    # -------------------------------------------------------------------------
+    TEST "Reject empty url (but with config attributes)"
+
+    export CCACHE_SECONDARY_STORAGE="|key=value"
+    $CCACHE_COMPILE -c test.c 2>stderr.log
+    expect_contains stderr.log "must provide a URL"
+
+    # -------------------------------------------------------------------------
+    TEST "Reject invalid url"
+
+    export CCACHE_SECONDARY_STORAGE="://qwerty"
+    $CCACHE_COMPILE -c test.c 2>stderr.log
+    expect_contains stderr.log "Cannot parse URL"
+
+    # -------------------------------------------------------------------------
+    TEST "Reject missing scheme"
+
+    export CCACHE_SECONDARY_STORAGE="/qwerty"
+    $CCACHE_COMPILE -c test.c 2>stderr.log
+    expect_contains stderr.log "URL scheme must not be empty"
+}
