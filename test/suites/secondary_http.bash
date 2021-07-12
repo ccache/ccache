d9683354f57c504a017aac63e909f6ee42a1a45a
Gregor Jasny <gregor.jasny@logmein.com>
2021-07-12 09:09:48 +0200

    HTTP storage: Implement authentication support
    Fixes: #887

--- test/suites/secondary_http.bash
+++ test/suites/secondary_http.bash
@@ -1,11 +1,14 @@
 start_http_server() {
     local port="$1"
     local cache_dir="$2"
+    local credentials="$3" # optional parameter
 
     mkdir -p "${cache_dir}"
     "${HTTP_SERVER}" --bind localhost --directory "${cache_dir}" "${port}" \
+        ${credentials:+--basic-auth ${credentials}} \
         &>http-server.log &
     "${HTTP_CLIENT}" "http://localhost:${port}" &>http-client.log \
+        ${credentials:+--basic-auth ${credentials}} \
         || test_failed_internal "Cannot connect to server"
 }
 
@@ -50,4 +53,43 @@ SUITE_secondary_http() {
     expect_stat 'files in cache' 0
     expect_stat 'files in cache' 0
     expect_file_count 2 '*' secondary # result + manifest
+
+    # -------------------------------------------------------------------------
+    TEST "Basic auth"
+
+    start_http_server 12780 secondary "somebody:secret"
+    export CCACHE_SECONDARY_STORAGE="http://somebody:secret@localhost:12780"
+
+    $CCACHE_COMPILE -c test.c
+    expect_stat 'cache hit (direct)' 0
+    expect_stat 'cache miss' 1
+    expect_stat 'files in cache' 2
+    expect_file_count 2 '*' secondary # result + manifest
+
+    # -------------------------------------------------------------------------
+    TEST "Basic auth required"
+
+    start_http_server 12780 secondary "somebody:secret"
+    # no authentication configured on client
+    export CCACHE_SECONDARY_STORAGE="http://localhost:12780"
+
+    CCACHE_DEBUG=1 $CCACHE_COMPILE -c test.c
+    expect_stat 'cache hit (direct)' 0
+    expect_stat 'cache miss' 1
+    expect_stat 'files in cache' 2
+    expect_file_count 0 '*' secondary # result + manifest
+    expect_contains test.o.ccache-log "status code: 401"
+
+    # -------------------------------------------------------------------------
+    TEST "Basic auth failed"
+
+    start_http_server 12780 secondary "somebody:secret"
+    export CCACHE_SECONDARY_STORAGE="http://somebody:wrong@localhost:12780"
+
+    CCACHE_DEBUG=1 $CCACHE_COMPILE -c test.c
+    expect_stat 'cache hit (direct)' 0
+    expect_stat 'cache miss' 1
+    expect_stat 'files in cache' 2
+    expect_file_count 0 '*' secondary # result + manifest
+    expect_contains test.o.ccache-log "status code: 401"
 }
