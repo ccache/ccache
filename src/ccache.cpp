ccache/ccache

commit 2d2de82bb9231b981b40486b093a2c0002200c40

fix: Avoid .incbin false positive (#983)

src/ccache.cpp
@@ -567,8 +567,8 @@ process_preprocessed_file(Context& ctx, Hash& hash, const std::string& path)
           Statistic::could_not_use_precompiled_header);
       }
       p = q; // Everything of interest between p and q has been hashed now.
-    } else if (q[0] == '.' && q[1] == 'i' && q[2] == 'n' && q[3] == 'c'
-               && q[4] == 'b' && q[5] == 'i' && q[6] == 'n') {
+    } else if (strncmp(q, ".incbin \\\"", 10) == 0
+               || strncmp(q, ".incbin \"", 9) == 0) {
       // An assembler .inc bin (without the space) statement, which could be
       // part of inline assembly, refers to an external file. If the file
       // changes, the hash should change as well, but finding out what file to

test/suites/base.bash
@@ -1440,6 +1440,20 @@ EOF
     expect_stat preprocessed_cache_hit 0
     expect_stat cache_miss 0
     expect_stat unsupported_code_directive 2
+
+    cat <<EOF >incbin.cpp
+        struct A {
+             void incbin() const { }
+        };
+        void f() {
+             A a;
+             a.incbin();
+        }
+EOF
+    $CCACHE_COMPILE -x c++ -c incbin.cpp
+    expect_stat preprocessed_cache_hit 0
+    expect_stat unsupported_code_directive 2
+    expect_stat cache_miss 1
 fi
 
     # -------------------------------------------------------------------------

commit bc97d547288e046978b18130d2ddcb404f9ca45b

fix: Make src/ccache.cpp cacheable by ccache itself
“.incbin” should not be present verbatim in the source code since that
makes ccache consider the code uncacheable.

Regression in 2d2de82bb9231b981b40486b093a2c0002200c40.

src/ccache.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2002-2007 Andrew Tridgell
-// Copyright (C) 2009-2021 Joel Rosdahl and other contributors
+// Copyright (C) 2009-2022 Joel Rosdahl and other contributors
 //
 // See doc/AUTHORS.adoc for a complete list of contributors.
 //
@@ -460,6 +460,10 @@ process_preprocessed_file(Context& ctx, Hash& hash, const std::string& path)
       "# 31 \"<command-line>\"\n";
     static const string_view hash_32_command_line_2_newline =
       "# 32 \"<command-line>\" 2\n";
+    // Note: Intentionally not using the string form to avoid false positive
+    // match by ccache itself.
+    static const char incbin_prefix[] = {
+      '.', 'i', 'n', 'c', 'b', 'i', 'n', ' '};
 
     // Check if we look at a line containing the file name of an included file.
     // At least the following formats exist (where N is a positive integer):
@@ -572,8 +576,8 @@ process_preprocessed_file(Context& ctx, Hash& hash, const std::string& path)
           Statistic::could_not_use_precompiled_header);
       }
       p = q; // Everything of interest between p and q has been hashed now.
-    } else if (strncmp(q, ".incbin \\\"", 10) == 0
-               || strncmp(q, ".incbin \"", 9) == 0) {
+    } else if (strncmp(q, incbin_prefix, sizeof(incbin_prefix)) == 0
+               && (q[8] == '"' || (q[8] == '\\' && q[9] == '"'))) {
       // An assembler .inc bin (without the space) statement, which could be
       // part of inline assembly, refers to an external file. If the file
       // changes, the hash should change as well, but finding out what file to