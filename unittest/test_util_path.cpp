998cddab1022d1a05fa50f5acf65603c5ef977e1
Joel Rosdahl <joel@rosdahl.net>
2022-02-27 20:41:29 +0100

    style: Add missing newline at end of file

--- unittest/test_util_path.cpp
+++ unittest/test_util_path.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2021 Joel Rosdahl and other contributors
+// Copyright (C) 2021-2022 Joel Rosdahl and other contributors
 //
 // See doc/AUTHORS.adoc for a complete list of contributors.
 //
