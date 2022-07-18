600f6be5f9b34cba8548b43a91956a07fe2cbc4a
Rafael Kitover <rkitover@gmail.com>
2022-07-18 22:10:05 +0000

    feat: auto dep FROM_INTERNET when unavailable
    Change the behavior of ZSTD_FROM_INTERNET and HIREDIS_FROM_INTERNET to
    always be on and only come into affect when the package is not found on
    the system by normal means.
    
    Also use the cmake standard module FetchContent for the
    download/checksum/unpack logic.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- cmake/Findhiredis.cmake
+++ cmake/Findhiredis.cmake
@@ -1,26 +1,57 @@
-if(HIREDIS_FROM_INTERNET)
-  set(hiredis_version "1.0.2")
-  set(hiredis_url https://github.com/redis/hiredis/archive/v${hiredis_version}.tar.gz)
+if(hiredis_FOUND)
+  return()
+endif()
 
-  set(hiredis_dir ${CMAKE_BINARY_DIR}/hiredis-${hiredis_version})
-  set(hiredis_build ${CMAKE_BINARY_DIR}/hiredis-build)
+set(hiredis_FOUND FALSE)
 
-  if(NOT EXISTS "${hiredis_dir}.tar.gz")
-    file(DOWNLOAD "${hiredis_url}" "${hiredis_dir}.tar.gz" STATUS download_status)
-    list(GET download_status 0 error_code)
-    if(error_code)
-      file(REMOVE "${hiredis_dir}.tar.gz")
-      list(GET download_status 1 error_message)
-      message(FATAL "Failed to download hiredis: ${error_message}")
-    endif()
+find_package(PkgConfig)
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules(HIREDIS hiredis>=${hiredis_FIND_VERSION})
+  find_library(HIREDIS_LIBRARY ${HIREDIS_LIBRARIES} HINTS ${HIREDIS_LIBDIR})
+  find_path(HIREDIS_INCLUDE_DIR hiredis/hiredis.h HINTS ${HIREDIS_PREFIX}/include)
+else()
+  find_library(HIREDIS_LIBRARY hiredis)
+  find_path(HIREDIS_INCLUDE_DIR hiredis/hiredis.h)
+endif()
+
+if(HIREDIS_INCLUDE_DIR AND HIREDIS_LIBRARY)
+  mark_as_advanced(HIREDIS_INCLUDE_DIR HIREDIS_LIBRARY)
+
+  add_library(HIREDIS::HIREDIS UNKNOWN IMPORTED)
+  set_target_properties(
+    HIREDIS::HIREDIS
+    PROPERTIES
+    IMPORTED_LOCATION "${HIREDIS_LIBRARY}"
+    INTERFACE_COMPILE_OPTIONS "${HIREDIS_CFLAGS_OTHER}"
+    INTERFACE_INCLUDE_DIRECTORIES "${HIREDIS_INCLUDE_DIR}")
+  if(WIN32 AND STATIC_LINK)
+    target_link_libraries(HIREDIS::HIREDIS INTERFACE ws2_32)
   endif()
 
-  execute_process(
-    COMMAND tar xf "${hiredis_dir}.tar.gz"
-    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
-    RESULT_VARIABLE tar_error)
-  if(NOT tar_error EQUAL 0)
-    message(FATAL "extracting ${hiredis_dir}.tar.gz failed")
+  set(hiredis_FOUND TRUE)
+  set(target HIREDIS::HIREDIS)
+elseif(HIREDIS_FROM_INTERNET)
+  message(STATUS "*** WARNING ***: Using hiredis from the internet because it was NOT found and HIREDIS_FROM_INTERNET is TRUE")
+
+  set(hiredis_version "1.0.2")
+
+  set(hiredis_dir   ${CMAKE_BINARY_DIR}/hiredis-${hiredis_version})
+  set(hiredis_build ${CMAKE_BINARY_DIR}/hiredis-build)
+
+  include(FetchContent)
+
+  FetchContent_Declare(
+    hiredis
+    URL         https://github.com/redis/hiredis/archive/v${hiredis_version}.tar.gz
+    URL_HASH    SHA256=e0ab696e2f07deb4252dda45b703d09854e53b9703c7d52182ce5a22616c3819
+    SOURCE_DIR  ${hiredis_dir}
+    BINARY_DIR  ${hiredis_build}
+  )
+
+  FetchContent_GetProperties(hiredis)
+
+  if(NOT hiredis_POPULATED)
+    FetchContent_Populate(hiredis)
   endif()
 
   set(
@@ -36,9 +67,9 @@ if(HIREDIS_FROM_INTERNET)
   )
   add_library(libhiredis_static STATIC EXCLUDE_FROM_ALL ${hiredis_sources})
   add_library(HIREDIS::HIREDIS ALIAS libhiredis_static)
+
   if(WIN32)
     target_compile_definitions(libhiredis_static PRIVATE _CRT_SECURE_NO_WARNINGS)
-    target_link_libraries(libhiredis_static PUBLIC ws2_32)
   endif()
 
   make_directory("${hiredis_dir}/include")
@@ -49,39 +80,20 @@ if(HIREDIS_FROM_INTERNET)
     libhiredis_static
     PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "$<BUILD_INTERFACE:${hiredis_dir}/include>")
-else()
-  find_package(PkgConfig)
-  if(PKG_CONFIG_FOUND)
-    pkg_check_modules(HIREDIS hiredis>=${hiredis_FIND_VERSION})
-    find_library(HIREDIS_LIBRARY ${HIREDIS_LIBRARIES} HINTS ${HIREDIS_LIBDIR})
-    find_path(HIREDIS_INCLUDE_DIR hiredis/hiredis.h HINTS ${HIREDIS_PREFIX}/include)
-  else()
-    find_library(HIREDIS_LIBRARY hiredis)
-    find_path(HIREDIS_INCLUDE_DIR hiredis/hiredis.h)
-  endif()
 
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(
-    hiredis
-    "please install libhiredis or use -DHIREDIS_FROM_INTERNET=ON or disable with -DREDIS_STORAGE_BACKEND=OFF"
-    HIREDIS_INCLUDE_DIR HIREDIS_LIBRARY)
-  mark_as_advanced(HIREDIS_INCLUDE_DIR HIREDIS_LIBRARY)
-
-  add_library(HIREDIS::HIREDIS UNKNOWN IMPORTED)
-  set_target_properties(
-    HIREDIS::HIREDIS
-    PROPERTIES
-    IMPORTED_LOCATION "${HIREDIS_LIBRARY}"
-    INTERFACE_COMPILE_OPTIONS "${HIREDIS_CFLAGS_OTHER}"
-    INTERFACE_INCLUDE_DIRECTORIES "${HIREDIS_INCLUDE_DIR}")
-  if(WIN32 AND STATIC_LINK)
-    target_link_libraries(HIREDIS::HIREDIS INTERFACE ws2_32)
-  endif()
+  set(hiredis_FOUND TRUE)
+  set(target libhiredis_static)
 endif()
+
+if(WIN32 AND hiredis_FOUND)
+  target_link_libraries(${target} INTERFACE ws2_32)
+endif()
+unset(target)
 
 include(FeatureSummary)
 set_package_properties(
   hiredis
   PROPERTIES
   URL "https://github.com/redis/hiredis"
-  DESCRIPTION "Hiredis is a minimalistic C client library for the Redis database")
+  DESCRIPTION "Hiredis is a minimalistic C client library for the Redis database"
+)
