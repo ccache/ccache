2c742c2c7ca988f9a6c57cee72835d62be30b451
Rafael Kitover <rkitover@gmail.com>
2022-07-19 01:29:27 -0700

    feat: Download dependencies from the Internet when unavailable (#1112)
    Change the behavior of ZSTD_FROM_INTERNET and HIREDIS_FROM_INTERNET to
    always be on and only come into affect when the package is not found on
    the system by normal means.
    
    Also use the cmake standard module FetchContent for the
    download/checksum/unpack logic.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- cmake/Findzstd.cmake
+++ cmake/Findzstd.cmake
@@ -2,32 +2,47 @@ if(zstd_FOUND)
   return()
 endif()
 
-if(ZSTD_FROM_INTERNET)
+set(zstd_FOUND FALSE)
+
+find_library(ZSTD_LIBRARY zstd)
+find_path(ZSTD_INCLUDE_DIR zstd.h)
+
+if(ZSTD_LIBRARY AND ZSTD_INCLUDE_DIR)
+  mark_as_advanced(ZSTD_INCLUDE_DIR ZSTD_LIBRARY)
+
+  add_library(ZSTD::ZSTD UNKNOWN IMPORTED)
+  set_target_properties(
+    ZSTD::ZSTD
+    PROPERTIES
+    IMPORTED_LOCATION "${ZSTD_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${ZSTD_INCLUDE_DIR}")
+
+  set(zstd_FOUND TRUE)
+elseif(ZSTD_FROM_INTERNET)
+  message(STATUS "*** WARNING ***: Using zstd from the internet because it was NOT found and ZSTD_FROM_INTERNET is TRUE")
+
   # Although ${zstd_FIND_VERSION} was requested, let's download a newer version.
   # Note: The directory structure has changed in 1.3.0; we only support 1.3.0
   # and newer.
   set(zstd_version "1.5.2")
-  set(zstd_url https://github.com/facebook/zstd/archive/v${zstd_version}.tar.gz)
 
-  set(zstd_dir ${CMAKE_BINARY_DIR}/zstd-${zstd_version})
+  set(zstd_dir   ${CMAKE_BINARY_DIR}/zstd-${zstd_version})
   set(zstd_build ${CMAKE_BINARY_DIR}/zstd-build)
 
-  if(NOT EXISTS "${zstd_dir}.tar.gz")
-    file(DOWNLOAD "${zstd_url}" "${zstd_dir}.tar.gz" STATUS download_status)
-    list(GET download_status 0 error_code)
-    if(error_code)
-      file(REMOVE "${zstd_dir}.tar.gz")
-      list(GET download_status 1 error_message)
-      message(FATAL "Failed to download zstd: ${error_message}")
-    endif()
-  endif()
+  include(FetchContent)
 
-  execute_process(
-    COMMAND tar xf "${zstd_dir}.tar.gz"
-    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
-    RESULT_VARIABLE tar_error)
-  if(NOT tar_error EQUAL 0)
-    message(FATAL "extracting ${zstd_dir}.tar.gz failed")
+  FetchContent_Declare(
+    zstd
+    URL         https://github.com/facebook/zstd/archive/v${zstd_version}.tar.gz
+    URL_HASH    SHA256=f7de13462f7a82c29ab865820149e778cbfe01087b3a55b5332707abf9db4a6e
+    SOURCE_DIR  ${zstd_dir}
+    BINARY_DIR  ${zstd_build}
+  )
+
+  FetchContent_GetProperties(zstd)
+
+  if(NOT zstd_POPULATED)
+    FetchContent_Populate(zstd)
   endif()
 
   set(ZSTD_BUILD_SHARED OFF)
@@ -37,25 +52,10 @@ if(ZSTD_FROM_INTERNET)
   set_target_properties(
     libzstd_static
     PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "$<BUILD_INTERFACE:${zstd_dir}/lib>")
+    INTERFACE_INCLUDE_DIRECTORIES "$<BUILD_INTERFACE:${zstd_dir}/lib>"
+  )
 
   set(zstd_FOUND TRUE)
-else()
-  find_library(ZSTD_LIBRARY zstd)
-  find_path(ZSTD_INCLUDE_DIR zstd.h)
-
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(
-    zstd "please install libzstd or use -DZSTD_FROM_INTERNET=ON"
-    ZSTD_INCLUDE_DIR ZSTD_LIBRARY)
-  mark_as_advanced(ZSTD_INCLUDE_DIR ZSTD_LIBRARY)
-
-  add_library(ZSTD::ZSTD UNKNOWN IMPORTED)
-  set_target_properties(
-    ZSTD::ZSTD
-    PROPERTIES
-    IMPORTED_LOCATION "${ZSTD_LIBRARY}"
-    INTERFACE_INCLUDE_DIRECTORIES "${ZSTD_INCLUDE_DIR}")
 endif()
 
 include(FeatureSummary)
