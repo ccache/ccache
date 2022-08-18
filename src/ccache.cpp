ccache/ccache

commit 02dac1f18084c0931aa6b8c1a07e9aedfadb5642

feat: Add --inspect option instead of --dump-manifest/--dump-result
“Inspect” sounds more appropriate than “dump” for viewing metadata of
cache files. Also, it’s convenient to have one option instead of two
different.

src/ccache.cpp
@@ -1844,13 +1844,17 @@ from_cache(Context& ctx, FromCacheCallMode mode, const Digest& result_key)
     return false;
   }
 
-  Result::Reader result_reader(*result_path);
+  File file(*result_path, "rb");
+  core::FileReader file_reader(file.get());
+  core::CacheEntryReader cache_entry_reader(file_reader);
+  Result::Reader result_reader(cache_entry_reader, *result_path);
   ResultRetriever result_retriever(
     ctx, should_rewrite_dependency_target(ctx.args_info));
 
-  auto error = result_reader.read(result_retriever);
-  if (error) {
-    LOG("Failed to get result from cache: {}", *error);
+  try {
+    result_reader.read(result_retriever);
+  } catch (core::Error& e) {
+    LOG("Failed to get result from cache: {}", e.what());
     return false;
   }
 

src/CMakeLists.txt
@@ -11,8 +11,8 @@ set(
   Logging.cpp
   ProgressBar.cpp
   Result.cpp
-  ResultDumper.cpp
   ResultExtractor.cpp
+  ResultInspector.cpp
   ResultRetriever.cpp
   SignalHandler.cpp
   Stat.cpp

src/Result.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019-2021 Joel Rosdahl and other contributors
+// Copyright (C) 2019-2022 Joel Rosdahl and other contributors
 //
 // See doc/AUTHORS.adoc for a complete list of contributors.
 //
@@ -188,75 +188,45 @@ FileSizeAndCountDiff::operator+=(const FileSizeAndCountDiff& other)
   return *this;
 }
 
-Result::Reader::Reader(const std::string& result_path)
-  : m_result_path(result_path)
+Result::Reader::Reader(core::CacheEntryReader& cache_entry_reader,
+                       const std::string& result_path)
+  : m_reader(cache_entry_reader),
+    m_result_path(result_path)
 {
 }
 
-optional<std::string>
-Result::Reader::read(Consumer& consumer)
+void
+Reader::read(Consumer& consumer)
 {
-  LOG("Reading result {}", m_result_path);
-
-  try {
-    if (read_result(consumer)) {
-      return nullopt;
-    } else {
-      return "No such result file";
-    }
-  } catch (const core::Error& e) {
-    return e.what();
-  }
-}
-
-bool
-Reader::read_result(Consumer& consumer)
-{
-  FILE* file_stream;
-  File file;
-  if (m_result_path == "-") {
-    file_stream = stdin;
-  } else {
-    file = File(m_result_path, "rb");
-    if (!file) {
-      // Cache miss.
-      return false;
-    }
-    file_stream = file.get();
+  if (m_reader.header().entry_type != core::CacheEntryType::result) {
+    throw core::Error("Unexpected cache entry type: {}",
+                      to_string(m_reader.header().entry_type));
   }
 
-  core::FileReader file_reader(file_stream);
-  core::CacheEntryReader cache_entry_reader(file_reader);
-
-  const auto result_format_version = cache_entry_reader.read_int<uint8_t>();
+  const auto result_format_version = m_reader.read_int<uint8_t>();
   if (result_format_version != k_result_format_version) {
     throw core::Error("Unknown result format version: {}",
                       result_format_version);
   }
 
-  consumer.on_header(cache_entry_reader, result_format_version);
-
-  const auto n_entries = cache_entry_reader.read_int<uint8_t>();
+  const auto n_entries = m_reader.read_int<uint8_t>();
 
   uint32_t i;
   for (i = 0; i < n_entries; ++i) {
-    read_entry(cache_entry_reader, i, consumer);
+    read_entry(i, consumer);
   }
 
   if (i != n_entries) {
     throw core::Error("Too few entries (read {}, expected {})", i, n_entries);
   }
 
-  cache_entry_reader.finalize();
-  return true;
+  m_reader.finalize();
 }
 
 void
-Reader::read_entry(core::CacheEntryReader& cache_entry_reader,
-                   uint32_t entry_number,
-                   Reader::Consumer& consumer)
+Reader::read_entry(uint32_t entry_number, Reader::Consumer& consumer)
 {
-  const auto marker = cache_entry_reader.read_int<uint8_t>();
+  const auto marker = m_reader.read_int<uint8_t>();
 
   switch (marker) {
   case k_embedded_file_marker:
@@ -267,9 +237,9 @@ Reader::read_entry(core::CacheEntryReader& cache_entry_reader,
     throw core::Error("Unknown entry type: {}", marker);
   }
 
-  const auto type = cache_entry_reader.read_int<UnderlyingFileTypeInt>();
+  const auto type = m_reader.read_int<UnderlyingFileTypeInt>();
   const auto file_type = FileType(type);
-  const auto file_len = cache_entry_reader.read_int<uint64_t>();
+  const auto file_len = m_reader.read_int<uint64_t>();
 
   if (marker == k_embedded_file_marker) {
     consumer.on_entry_start(entry_number, file_type, file_len, nullopt);
@@ -278,21 +248,24 @@ Reader::read_entry(core::CacheEntryReader& cache_entry_reader,
     size_t remain = file_len;
     while (remain > 0) {
       size_t n = std::min(remain, sizeof(buf));
-      cache_entry_reader.read(buf, n);
+      m_reader.read(buf, n);
       consumer.on_entry_data(buf, n);
       remain -= n;
     }
   } else {
     ASSERT(marker == k_raw_file_marker);
 
-    auto raw_path = get_raw_file_path(m_result_path, entry_number);
-    auto st = Stat::stat(raw_path, Stat::OnError::throw_error);
-    if (st.size() != file_len) {
-      throw core::Error(
-        "Bad file size of {} (actual {} bytes, expected {} bytes)",
-        raw_path,
-        st.size(),
-        file_len);
+    std::string raw_path;
+    if (m_result_path != "-") {
+      raw_path = get_raw_file_path(m_result_path, entry_number);
+      auto st = Stat::stat(raw_path, Stat::OnError::throw_error);
+      if (st.size() != file_len) {
+        throw core::Error(
+          "Bad file size of {} (actual {} bytes, expected {} bytes)",
+          raw_path,
+          st.size(),
+          file_len);
+      }
     }
 
     consumer.on_entry_start(entry_number, file_type, file_len, raw_path);

src/Result.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019-2021 Joel Rosdahl and other contributors
+// Copyright (C) 2019-2022 Joel Rosdahl and other contributors
 //
 // See doc/AUTHORS.adoc for a complete list of contributors.
 //
@@ -17,6 +17,8 @@
 // Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 #pragma once
+
+#include <core/Reader.hpp>
 
 #include "third_party/nonstd/expected.hpp"
 #include "third_party/nonstd/optional.hpp"
@@ -99,15 +101,14 @@ struct FileSizeAndCountDiff
 class Reader
 {
 public:
-  Reader(const std::string& result_path);
+  Reader(core::CacheEntryReader& cache_entry_reader,
+         const std::string& result_path);
 
   class Consumer
   {
   public:
     virtual ~Consumer() = default;
 
-    virtual void on_header(core::CacheEntryReader& cache_entry_reader,
-                           uint8_t result_format_version) = 0;
     virtual void on_entry_start(uint32_t entry_number,
                                 FileType file_type,
                                 uint64_t file_len,
@@ -116,16 +117,14 @@ public:
     virtual void on_entry_end() = 0;
   };
 
-  // Returns error message on error, otherwise nonstd::nullopt.
-  nonstd::optional<std::string> read(Consumer& consumer);
+  // Throws core::Error on error.
+  void read(Consumer& consumer);
 
 private:
+  core::CacheEntryReader& m_reader;
   const std::string m_result_path;
 
-  bool read_result(Consumer& consumer);
-  void read_entry(core::CacheEntryReader& cache_entry_reader,
-                  uint32_t entry_number,
-                  Reader::Consumer& consumer);
+  void read_entry(uint32_t entry_number, Reader::Consumer& consumer);
 };
 
 // This class knows how to write a result cache entry.

src/ResultExtractor.cpp
@@ -34,12 +34,6 @@ ResultExtractor::ResultExtractor(const std::string& directory)
 }
 
 void
-ResultExtractor::on_header(core::CacheEntryReader& /*cache_entry_reader*/,
-                           const uint8_t /*result_format_version*/)
-{
-}
-
-void
 ResultExtractor::on_entry_start(uint32_t /*entry_number*/,
                                 Result::FileType file_type,
                                 uint64_t /*file_len*/,
@@ -63,6 +57,9 @@ ResultExtractor::on_entry_start(uint32_t /*entry_number*/,
       throw core::Error(
         "Failed to open {} for writing: {}", m_dest_path, strerror(errno));
     }
+  } else if (raw_file->empty()) {
+    PRINT_RAW(stderr,
+              "Note: Can't extract raw file since reading from stdin\n");
   } else {
     try {
       Util::copy_file(*raw_file, m_dest_path, false);

src/ResultExtractor.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2021 Joel Rosdahl and other contributors
+// Copyright (C) 2020-2022 Joel Rosdahl and other contributors
 //
 // See doc/AUTHORS.adoc for a complete list of contributors.
 //
@@ -29,8 +29,6 @@ class ResultExtractor : public Result::Reader::Consumer
 public:
   ResultExtractor(const std::string& directory);
 
-  void on_header(core::CacheEntryReader& cache_entry_reader,
-                 uint8_t result_format_version) override;
   void on_entry_start(uint32_t entry_number,
                       Result::FileType file_type,
                       uint64_t file_len,

src/ResultInspector.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2021 Joel Rosdahl and other contributors
+// Copyright (C) 2020-2022 Joel Rosdahl and other contributors
 //
 // See doc/AUTHORS.adoc for a complete list of contributors.
 //
@@ -16,33 +16,23 @@
 // this program; if not, write to the Free Software Foundation, Inc., 51
 // Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-#include "ResultDumper.hpp"
+#include "ResultInspector.hpp"
 
 #include "Context.hpp"
 #include "Logging.hpp"
 #include "fmtmacros.hpp"
 
-#include <core/CacheEntryReader.hpp>
-
 using nonstd::optional;
 
-ResultDumper::ResultDumper(FILE* stream) : m_stream(stream)
+ResultInspector::ResultInspector(FILE* stream) : m_stream(stream)
 {
 }
 
 void
-ResultDumper::on_header(core::CacheEntryReader& cache_entry_reader,
-                        const uint8_t result_format_version)
-{
-  cache_entry_reader.header().dump(m_stream);
-  PRINT(m_stream, "Result format version: {}\n", result_format_version);
-}
-
-void
-ResultDumper::on_entry_start(uint32_t entry_number,
-                             Result::FileType file_type,
-                             uint64_t file_len,
-                             optional<std::string> raw_file)
+ResultInspector::on_entry_start(uint32_t entry_number,
+                                Result::FileType file_type,
+                                uint64_t file_len,
+                                optional<std::string> raw_file)
 {
   PRINT(m_stream,
         "{} file #{}: {} ({} bytes)\n",
@@ -53,11 +43,11 @@ ResultDumper::on_entry_start(uint32_t entry_number,
 }
 
 void
-ResultDumper::on_entry_data(const uint8_t* /*data*/, size_t /*size*/)
+ResultInspector::on_entry_data(const uint8_t* /*data*/, size_t /*size*/)
 {
 }
 
 void
-ResultDumper::on_entry_end()
+ResultInspector::on_entry_end()
 {
 }

src/ResultInspector.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2021 Joel Rosdahl and other contributors
+// Copyright (C) 2020-2022 Joel Rosdahl and other contributors
 //
 // See doc/AUTHORS.adoc for a complete list of contributors.
 //
@@ -23,14 +23,12 @@
 #include <cstdint>
 #include <cstdio>
 
-// This class dumps information about the result entry to `stream`.
-class ResultDumper : public Result::Reader::Consumer
+// This class writes information about the result entry to `stream`.
+class ResultInspector : public Result::Reader::Consumer
 {
 public:
-  ResultDumper(FILE* stream);
+  ResultInspector(FILE* stream);
 
-  void on_header(core::CacheEntryReader& cache_entry_reader,
-                 uint8_t result_format_version) override;
   void on_entry_start(uint32_t entry_number,
                       Result::FileType file_type,
                       uint64_t file_len,

commit 56aac268892491465b042db51a0a3884403b92e3

fix: Don't crash in race between file read and cleanup
As mentioned in discussion #1102.

Regression in 02dac1f18084c0931aa6b8c1a07e9aedfadb5642.

src/ccache.cpp
@@ -1926,6 +1926,9 @@ from_cache(Context& ctx, FromCacheCallMode mode, const Digest& result_key)
 
   try {
     File file(*result_path, "rb");
+    if (!file) {
+      throw core::Error("Failed to open {}: {}", strerror(errno));
+    }
     core::FileReader file_reader(file.get());
     core::CacheEntryReader cache_entry_reader(file_reader);
     Result::Reader result_reader(cache_entry_reader, *result_path);