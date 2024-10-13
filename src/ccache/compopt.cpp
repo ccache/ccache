94d2675c802f4db45afea0ebdf135c838df5d91d
silvanshade <silvanshade@users.noreply.github.com>
2024-10-13 08:58:02 -0600

    Remove -fmodule-ts from TOO_HARD category

--- src/ccache/compopt.cpp
+++ src/ccache/compopt.cpp
@@ -122,7 +122,6 @@ const CompOpt compopts[] = {
   {"-external:I",
    AFFECTS_CPP | TAKES_ARG | TAKES_CONCAT_ARG | TAKES_PATH}, // msvc
   {"-fmodule-header", TOO_HARD},
-  {"-fmodules-ts", TOO_HARD},
   {"-fno-working-directory", AFFECTS_CPP},
   {"-fplugin=libcc1plugin", TOO_HARD}, // interaction with GDB
   {"-frepo", TOO_HARD},
