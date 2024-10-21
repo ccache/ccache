69b0a4284aaab0f33a60305c0f0c9fef9907d16c
Joel Rosdahl <joel@rosdahl.net>
2024-10-21 20:29:26 +0200

    refactor: Restructure code to help Clang-Tidy understand it

--- src/ccache/context.cpp
+++ src/ccache/context.cpp
@@ -67,8 +67,9 @@ Context::initialize(Args&& compiler_and_args,
   // initial configuration file. The intention is that all files and directories
   // in the cache directory should be affected by the configured umask and that
   // no other files and directories should.
-  if (config.umask()) {
-    original_umask = util::set_umask(*config.umask());
+  auto mask = config.umask();
+  if (mask) {
+    original_umask = util::set_umask(*mask);
   }
 }
 
