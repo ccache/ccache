fc5467fc90667b705415a773944b53a6a367748a
Joel Rosdahl <joel@rosdahl.net>
2025-01-28 18:07:23 +0100

    fix: Fix --trim-method mtime

--- src/ccache/core/mainoptions.cpp
+++ src/ccache/core/mainoptions.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2021-2024 Joel Rosdahl and other contributors
+// Copyright (C) 2021-2025 Joel Rosdahl and other contributors
 //
 // See doc/AUTHORS.adoc for a complete list of contributors.
 //
@@ -561,7 +561,7 @@ process_main_options(int argc, const char* const* argv)
     }
 
     case TRIM_METHOD:
-      trim_lru_mtime = (arg == "ctime");
+      trim_lru_mtime = (arg == "mtime");
       break;
 
     case TRIM_RECOMPRESS:
