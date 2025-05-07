77156a47f8d9d193b7e7cc6ead5371a924f26d23
Maksym Sobolyev <sobomax@sippysoft.com>
2025-05-06 22:09:13 -0700

    Link libatomic statically when STATIC_LINK is set.

--- cmake/StdAtomic.cmake
+++ cmake/StdAtomic.cmake
@@ -1,5 +1,7 @@
 # Check if std::atomic needs -latomic
 
+set(LIBATOMIC_STATIC_PATH "" CACHE PATH "Directory containing static libatomic.a")
+
 include(CheckCXXSourceCompiles)
 
 set(
@@ -24,6 +26,17 @@ if(NOT std_atomic_without_libatomic)
   if(NOT std_atomic_with_libatomic)
     message(FATAL_ERROR "Toolchain doesn't support std::atomic with nor without -latomic")
   else()
-    target_link_libraries(standard_settings INTERFACE atomic)
+    if(STATIC_LINK)
+      find_library(ATOMIC_STATIC NAMES libatomic.a PATHS /usr/lib /usr/local/lib ${LIBATOMIC_STATIC_PATH} NO_DEFAULT_PATH)
+      if(ATOMIC_STATIC)
+        message(STATUS "Linking static libatomic: ${ATOMIC_STATIC}")
+        target_link_libraries(standard_settings INTERFACE ${ATOMIC_STATIC})
+      else()
+        message(WARNING "STATIC_LINK is set but static libatomic not found; falling back to -latomic")
+        target_link_libraries(standard_settings INTERFACE atomic)
+      endif()
+    else()
+      target_link_libraries(standard_settings INTERFACE atomic)
+    endif()
   endif()
 endif()
