9ce81a84c4368c51e721cf143d74ad334b8800b4
autoantwort <Leander.schulten@rwth-aachen.de>
2025-08-21 23:03:13 +0200

    Actually test stuff

--- test/suites/serialize_diagnostics.bash
+++ test/suites/serialize_diagnostics.bash
@@ -84,26 +84,4 @@ EOF
     expect_stat cache_miss 1
     expect_stat files_in_cache 2
 fi
-
-    # -------------------------------------------------------------------------
-if $COMPILER_TYPE_GCC; then
-    TEST "Sarif diagnostics"
-
-    $COMPILER -c -fdiagnostics-format=sarif-file test1.c
-
-    $CCACHE_COMPILE -c -fdiagnostics-format=sarif-file test1.c
-    expect_stat preprocessed_cache_hit 0
-    expect_stat cache_miss 1
-    expect_stat files_in_cache 1
-    expect_exists test1.c.sarif
-
-    rm test1.c.sarif
-
-    $CCACHE_COMPILE -c -fdiagnostics-format=sarif-file test1.c
-    expect_stat preprocessed_cache_hit 1
-    expect_stat cache_miss 1
-    expect_stat files_in_cache 1
-    expect_exists test1.c.sarif
-fi
-
 }
