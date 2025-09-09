c689941325679d0b132bf707fd428a0373183b96
autoantwort <41973254+autoantwort@users.noreply.github.com>
2025-09-10 01:08:26 +0200

    Update gcc sarif test
    Co-authored-by: Joel Rosdahl <joel@rosdahl.net>

--- test/suites/sarif.bash
+++ test/suites/sarif.bash
@@ -13,19 +13,17 @@ SUITE_sarif() {
     # -------------------------------------------------------------------------
     TEST "Sarif diagnostics"
 
-    $COMPILER -c -fdiagnostics-format=sarif-file test1.c
-
-    $CCACHE_COMPILE -c -fdiagnostics-format=sarif-file test1.c
+    $CCACHE_COMPILE -x c -fdiagnostics-format=sarif-file -c src/input.a.b -o obj/output.x.y
     expect_stat preprocessed_cache_hit 0
     expect_stat cache_miss 1
     expect_stat files_in_cache 1
-    expect_exists test1.c.sarif
+    expect_exists output.x.b.sarif
 
-    rm test1.c.sarif
+    rm output.x.b.sarif
 
-    $CCACHE_COMPILE -c -fdiagnostics-format=sarif-file test1.c
+    $CCACHE_COMPILE -x c -fdiagnostics-format=sarif-file -c src/input.a.b -o obj/output.x.y
     expect_stat preprocessed_cache_hit 1
     expect_stat cache_miss 1
     expect_stat files_in_cache 1
-    expect_exists test1.c.sarif
+    expect_exists output.x.b.sarif
 }
