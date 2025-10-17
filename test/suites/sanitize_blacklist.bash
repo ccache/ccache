ff8516d2f94ededb13f67cb12e63cdb622e03d7b
Diego Alonso <diego.alonso@appentra.com>
2025-10-17 18:57:49 +0000

    feat: Support rewriting absolute path for -fsanitize-blacklist (#1647)

--- test/suites/sanitize_blacklist.bash
+++ test/suites/sanitize_blacklist.bash
@@ -11,6 +11,10 @@ SUITE_sanitize_blacklist_SETUP() {
     echo "fun:foo" >blacklist.txt
     echo "fun_1:foo" >blacklist2.txt
 
+    mkdir -p dir1/ dir2/
+    cp blacklist.txt dir1/blacklist.txt
+    cp blacklist.txt dir2/blacklist.txt
+
     unset CCACHE_NODIRECT
 }
 
@@ -41,6 +45,35 @@ SUITE_sanitize_blacklist() {
     expect_stat direct_cache_hit 2
     expect_stat cache_miss 2
     expect_stat files_in_cache 4
+
+    # -------------------------------------------------------------------------
+    TEST "base_dir OK"
+
+    basedir1="$(pwd)/dir1"
+    basedir2="$(pwd)/dir2"
+
+    basedir=$basedir1
+    cd $basedir
+
+    $COMPILER -c -fsanitize-blacklist=blacklist.txt ../test1.c
+
+    CCACHE_BASEDIR="${basedir}" $CCACHE_COMPILE -c -fsanitize-blacklist=$basedir/blacklist.txt ../test1.c
+    expect_stat direct_cache_hit 0
+    expect_stat cache_miss 1
+    expect_stat files_in_cache 2
+
+    CCACHE_BASEDIR="${basedir}" $CCACHE_COMPILE -c -fsanitize-blacklist=$basedir/blacklist.txt ../test1.c
+    expect_stat direct_cache_hit 1
+    expect_stat cache_miss 1
+    expect_stat files_in_cache 2
+
+    basedir=$basedir2
+    cd $basedir
+
+    CCACHE_DEBUG=1 CCACHE_BASEDIR="${basedir}" $CCACHE_COMPILE -c -fsanitize-blacklist=$basedir/blacklist.txt ../test1.c
+    expect_stat direct_cache_hit 2
+    expect_stat cache_miss 1
+    expect_stat files_in_cache 2
 
     # -------------------------------------------------------------------------
     TEST "Unsuccessful compilation"
