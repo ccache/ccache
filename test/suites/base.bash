ccache/ccache

commit 5da91eb2b4f4cd09a5669f0aebfc534b514826ce

refactor: Generalize "already preprocessed" compiler language slightly (#1684)
- Rename ArgsInfo::direct_i_file to preprocess_input_file, since that's
  what it now means.
- Add "ir" as a language and let "-fthinlto-index=" use that language
  implicitly instead of piggybacking on "assembler".

src/ccache/argprocessing.cpp
@@ -739,8 +739,8 @@ process_option_arg(const Context& ctx,
     std::string thinlto_index = arg.substr(arg.find('=') + 1);
     args_info.thinlto_index_path = thinlto_index;
     // Thinlto backend phase, the extension of input file is .o but the file is
-    // IR. So treat it as assembler file.
-    args_info.actual_language = "assembler";
+    // IR.
+    args_info.actual_language = "ir";
     state.add_common_arg(args[i]);
     return Statistic::none;
   }
@@ -1612,14 +1612,19 @@ process_args(Context& ctx)
     return tl::unexpected(Statistic::unsupported_source_language);
   }
 
-  if (args_info.actual_language == "assembler") {
-    // -MD/-MMD for assembler file does not produce a dependency file.
+  if (args_info.actual_language == "assembler"
+      || args_info.actual_language == "ir") {
+    // -MD/-MMD do not produce a dependency file.
     args_info.generating_dependencies = false;
   }
 
-  args_info.direct_i_file = language_is_preprocessed(args_info.actual_language);
+  args_info.preprocess_input_file =
+    !language_is_preprocessed(args_info.actual_language);
 
-  if (config.cpp_extension().empty()) {
+  if (!args_info.preprocess_input_file) {
+    config.set_cpp_extension(
+      util::pstr(args_info.input_file.extension()).str().substr(1));
+  } else if (config.cpp_extension().empty()) {
     std::string p_language = p_language_for_language(args_info.actual_language);
     config.set_cpp_extension(extension_for_language(p_language).substr(1));
   }

src/ccache/argsinfo.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2025 Joel Rosdahl and other contributors
+// Copyright (C) 2020-2026 Joel Rosdahl and other contributors
 //
 // See doc/authors.adoc for a complete list of contributors.
 //
@@ -121,8 +121,9 @@ struct ArgsInfo
   // Have we seen -gsplit-dwarf?
   bool seen_split_dwarf = false;
 
-  // Are we compiling a .i or .ii file directly?
-  bool direct_i_file = false;
+  // Should we run the preprocessor on the input file? False for .i/.ii files,
+  // assembler or ThinLTO backend phase.
+  bool preprocess_input_file = true;
 
   // Whether the output is a precompiled header.
   bool output_is_precompiled_header = false;

src/ccache/ccache.cpp
@@ -1394,9 +1394,8 @@ get_result_key_from_cpp(Context& ctx, util::Args& args, Hash& hash)
 
   const bool capture_stdout = is_clang_cu;
 
-  if (ctx.args_info.direct_i_file) {
-    // We are compiling a .i or .ii file - that means we can skip the cpp stage
-    // and directly form the correct i_tmpfile.
+  if (!ctx.args_info.preprocess_input_file) {
+    // We are compiling a file that should be used as is (not be preprocessed).
     preprocessed_path = ctx.args_info.input_file;
   } else {
     // Run cpp on the input file to obtain the .i.

src/ccache/language.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2010-2024 Joel Rosdahl and other contributors
+// Copyright (C) 2010-2026 Joel Rosdahl and other contributors
 //
 // See doc/authors.adoc for a complete list of contributors.
 //
@@ -99,6 +99,7 @@ const struct
   {"objective-c++-cpp-output", "objective-c++-cpp-output"},
   {"assembler-with-cpp",       "assembler"               },
   {"assembler",                "assembler"               },
+  {"ir",                       "ir"                      }, // Clang ThinLTO
   {nullptr,                    nullptr                   },
 };
 

commit 1578aa21aa039b7085225a4f5478aab2910fc8dc

fix: Handle extensionless -x assembler inputs
Regression in 5da91eb2b4f4cd09a5669f0aebfc534b514826ce.

Fixes #1691.

src/ccache/argprocessing.cpp
@@ -1620,9 +1620,9 @@ process_args(Context& ctx)
   args_info.preprocess_input_file =
     !language_is_preprocessed(args_info.actual_language);
 
-  if (!args_info.preprocess_input_file) {
-    config.set_cpp_extension(
-      util::pstr(args_info.input_file.extension()).str().substr(1));
+  if (!args_info.preprocess_input_file && ctx.config.cpp_extension().empty()) {
+    const auto extension = util::pstr(args_info.input_file.extension()).str();
+    config.set_cpp_extension(extension.empty() ? "i" : extension.substr(1));
   } else if (config.cpp_extension().empty()) {
     std::string p_language = p_language_for_language(args_info.actual_language);
     config.set_cpp_extension(extension_for_language(p_language).substr(1));

test/suites/base.bash
@@ -858,6 +858,23 @@ fi
     expect_stat cache_miss 2
 
     # -------------------------------------------------------------------------
+    TEST "-x assembler with extensionless file"
+
+    $COMPILER -S -o test1asm test1.c
+
+    $CCACHE_COMPILE -x assembler -c test1asm -o test1asm.o
+    expect_stat preprocessed_cache_hit 0
+    expect_stat cache_miss 1
+    expect_exists test1asm.o
+
+    rm test1asm.o
+
+    $CCACHE_COMPILE -x assembler -c test1asm -o test1asm.o
+    expect_stat preprocessed_cache_hit 1
+    expect_stat cache_miss 1
+    expect_exists test1asm.o
+
+    # -------------------------------------------------------------------------
 if ! $HOST_OS_WINDOWS; then
     TEST "-frecord-gcc-switches"
 