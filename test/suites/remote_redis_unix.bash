2dc954ba5ce28b929f40b5191bbade940ed84c6e
Anders F Björklund <anders.f.bjorklund@gmail.com>
2026-04-13 18:18:56 +0200

    feat: allow running test with valkey
    Only the redis "tcp" test had changed.
    
    Not the redis "unix" test redis programs.

--- test/suites/remote_redis_unix.bash
+++ test/suites/remote_redis_unix.bash
@@ -1,20 +1,23 @@
+REDIS_SERVER=$(command -v redis-server || command -v valkey-server)
+REDIS_CLI=$(command -v redis-cli || command -v valkey-cli)
+
 SUITE_remote_redis_unix_PROBE() {
-    if ! command -v redis-server &> /dev/null; then
-        echo "redis-server not found"
+    if [ -z "${REDIS_SERVER}" ]; then
+        echo "neither redis-server nor valkey-server found"
         return
     fi
-    if redis-server --unixsocket /foo/redis.sock 2>&1 | grep -q "FATAL CONFIG FILE ERROR"; then
+    if ${REDIS_SERVER} --unixsocket /foo/redis.sock 2>&1 | grep -q "FATAL CONFIG FILE ERROR"; then
         # "Bad directive or wrong number of arguments"
         echo "redis-server without unixsocket"
         return
     fi
-    if ! command -v redis-cli &> /dev/null; then
-        echo "redis-cli not found"
+    if [ -z "${REDIS_CLI}" ]; then
+        echo "neither redis-cli nor valkey-cli found"
         return
     fi
-    if ! redis-cli -s /foo/redis.sock --version &> /dev/null; then
+    if ! ${REDIS_CLI} -s /foo/redis.sock --version &> /dev/null; then
         # "Unrecognized option or bad number of args"
-        echo "redis-cli without socket"
+        echo "${REDIS_CLI} without socket option"
         return
     fi
 }
@@ -23,16 +26,16 @@ start_redis_unix_server() {
     local socket="$1"
     local password="${2:-}"
 
-    redis-server --bind localhost --unixsocket "${socket}" --port 0 >/dev/null &
+    ${REDIS_SERVER} --bind localhost --unixsocket "${socket}" --port 0 >/dev/null &
     # Wait for server start.
     i=0
-    while [ $i -lt 100 ] && ! redis-cli -s "${socket}" ping &>/dev/null; do
+    while [ $i -lt 100 ] && ! ${REDIS_CLI} -s "${socket}" ping &>/dev/null; do
         sleep 0.1
         i=$((i + 1))
     done
 
     if [ -n "${password}" ]; then
-        redis-cli -s "${socket}" config set requirepass "${password}" &>/dev/null
+        ${REDIS_CLI} -s "${socket}" config set requirepass "${password}" &>/dev/null
     fi
 }
 
@@ -49,7 +52,7 @@ expect_number_of_redis_unix_cache_entries() {
     local socket=$2
     local actual
 
-    actual=$(redis-cli -s "$socket" keys "ccache:*" 2>/dev/null | wc -l)
+    actual=$(${REDIS_CLI} -s "$socket" keys "ccache:*" 2>/dev/null | wc -l)
     if [ "$actual" -ne "$expected" ]; then
         test_failed_internal "Found $actual (expected $expected) entries in $socket"
     fi
