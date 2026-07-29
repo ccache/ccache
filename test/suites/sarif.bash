44552e51a62ee184cc4f2ad9593a1b6d92a024ec
Karl Fessel <karl.fessel@ml-pa.com>
2026-07-29 22:21:29 +0200

    f: rejoin test

--- test/suites/sarif.bash
+++ test/suites/sarif.bash
@@ -13,7 +13,61 @@ SUITE_sarif_SETUP() {
 
 SUITE_sarif() {
     # -------------------------------------------------------------------------
-    TEST "Sarif diagnostics"
+    TEST "Sarif default location -fdiagnostics-format fallback"
+
+    # -fdiagnostics-format=sarif-file is not supported due to version dependent default file location
+    $CCACHE_COMPILE -x c -fdiagnostics-format=sarif-file -c src/input.a.b -o obj/output.x.y
+    expect_stat unsupported_compiler_option 1
+    expect_stat preprocessed_cache_hit 0
+    expect_stat cache_miss 0
+    expect_stat files_in_cache 0
+    # check the two default locations
+    if [ -e output.x.b.sarif ]; then
+        expect_exist output.x.b.sarif
+    else
+        expect_exist obj/output.x.b.sarif
+    fi
+
+    # -------------------------------------------------------------------------
+    TEST "Sarif default location -fdiagnostics-set-output=sarif fallback"
+
+    # -fdiagnostics-set-output=sarif is not supported due to version dependent default file location
+    $CCACHE_COMPILE -x c -fdiagnostics-set-output=sarif -c src/input.a.b -o obj/output.x.y
+    expect_stat unsupported_compiler_option 1
+    expect_stat preprocessed_cache_hit 0
+    expect_stat cache_miss 0
+    expect_stat files_in_cache 0
+    # check the two default locations
+    if [ -e output.x.b.sarif ]; then
+        expect_exist output.x.b.sarif
+    else
+        expect_exist obj/output.x.b.sarif
+    fi
+
+    # -------------------------------------------------------------------------
+    TEST "Sarif diagnostics multi set fallback"
+
+    # multiple set is not supported due to documentation implementation mismatch
+    $CCACHE_COMPILE -x c -fdiagnostics-set-output=sarif:file=output.x.b.sarif -fdiagnostics-set-output=text -c src/input.a.b -o obj/output.x.y
+    expect_stat unsupported_compiler_option 1
+    expect_stat preprocessed_cache_hit 0
+    expect_stat cache_miss 0
+    expect_stat files_in_cache 0
+    expect_exists output.x.b.sarif
+
+    # -------------------------------------------------------------------------
+    TEST "Sarif diagnostics add set fallback"
+
+    # multiple add+set is not supported due to documentation implementation mismatch
+    $CCACHE_COMPILE -x c -fdiagnostics-add-output=sarif:file=output.x.b.sarif -fdiagnostics-set-output=text -c src/input.a.b -o obj/output.x.y
+    expect_stat unsupported_compiler_option 1
+    expect_stat preprocessed_cache_hit 0
+    expect_stat cache_miss 0
+    expect_stat files_in_cache 0
+    expect_exists output.x.b.sarif
+
+    # -------------------------------------------------------------------------
+    TEST "Sarif multi add diagnostics shall cache"
 
     # multiple add is supported
     $CCACHE_COMPILE -x c -fdiagnostics-add-output=sarif:file=output.x.b.sarif -fdiagnostics-add-output=text -c src/input.a.b -o obj/output.x.y
@@ -26,6 +80,26 @@ SUITE_sarif() {
     rm -f output.x.b.sarif
     # finally test if cache is working
     $CCACHE_COMPILE -x c -fdiagnostics-add-output=sarif:file=output.x.b.sarif -fdiagnostics-add-output=text -c src/input.a.b -o obj/output.x.y
+    expect_stat unsupported_compiler_option 0
+    expect_stat preprocessed_cache_hit 1
+    expect_stat cache_miss 1
+    expect_stat files_in_cache 1
+    expect_exists output.x.b.sarif
+
+    # -------------------------------------------------------------------------
+    TEST "Sarif set diagnostics shall cache"
+
+    # multiple add is supported
+    $CCACHE_COMPILE -x c -fdiagnostics-set-output=sarif:file=output.x.b.sarif -c src/input.a.b -o obj/output.x.y
+    expect_stat unsupported_compiler_option 0
+    expect_stat preprocessed_cache_hit 0
+    expect_stat cache_miss 1
+    expect_stat files_in_cache 1
+    expect_exists output.x.b.sarif
+
+    rm -f output.x.b.sarif
+    # finally test if cache is working
+    $CCACHE_COMPILE -x c -fdiagnostics-set-output=sarif:file=output.x.b.sarif -c src/input.a.b -o obj/output.x.y
     expect_stat unsupported_compiler_option 0
     expect_stat preprocessed_cache_hit 1
     expect_stat cache_miss 1
